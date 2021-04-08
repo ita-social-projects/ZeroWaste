9430e8f652b786f9ab1c45b409727547d4008398
Oleh <oleh.chupa@gmail.com>
2021-04-08 18:23:37 +0300

    Add migration createFields

---
+++ db/migrate/20210408145903_create_fields.rb
@@ -0,0 +1,20 @@
+class CreateFields < ActiveRecord::Migration[6.1]
+  def change
+    create_table :fields do |t|
+      t.uuid :uuid, null: false
+      t.references :calculator, null: false
+      t.string :selector, null: false
+      t.string :type, null: false
+      t.string :label, null: false
+      t.string :name
+      t.string :value
+      t.string :unit
+      t.integer :from
+      t.integer :to
+      t.integer :kind, null: false
+
+      t.timestamps
+    end
+    add_index :fields, :uuid, unique: true
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_08_145903) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "fields", force: :cascade do |t|
+    t.uuid "uuid", null: false
+    t.bigint "calculator_id", null: false
+    t.string "selector", null: false
+    t.string "type", null: false
+    t.string "label", null: false
+    t.string "name"
+    t.string "value"
+    t.string "unit"
+    t.integer "from"
+    t.integer "to"
+    t.integer "kind", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["calculator_id"], name: "index_fields_on_calculator_id"
+    t.index ["uuid"], name: "index_fields_on_uuid", unique: true
+  end
+
+end
