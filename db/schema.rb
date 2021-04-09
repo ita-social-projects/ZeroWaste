7b1a4d6a55e9cdb4fe3e6dfb7ebca3b2df43f486
PavloMS <pavlolv7@gmail.com>
2021-04-09 16:23:49 +0300

    ZER-11 Add migration createCalculators

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_145903) do
+ActiveRecord::Schema.define(version: 2021_04_09_131859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calculators", force: :cascade do |t|
+    t.uuid "uuid", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["uuid"], name: "index_calculators_on_uuid", unique: true
+  end
 
   create_table "fields", force: :cascade do |t|
     t.uuid "uuid", null: false
