a9cd4786b722b63cb2191e9a4b79892723fd4874
Oleh <oleh.chupa@gmail.com>
2021-04-13 23:38:10 +0300

    ZER-27 Add migrations to change uuid default to gen_random_uuid()

---
+++ db/migrate/20210413203204_change_uuid_default_in_fields.rb
@@ -0,0 +1,6 @@
+class ChangeUuidDefaultInFields < ActiveRecord::Migration[6.1]
+  enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+  def change
+    change_column :fields, :uuid, :uuid, default: 'gen_random_uuid()'
+  end
+end
