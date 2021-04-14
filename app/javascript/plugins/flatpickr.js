0fc8b236059741ac6d864057839d32e6818b4a1a
Oleksandr Kharchenko <oleksandrkharchenko7@gmail.com>
2021-04-15 00:28:40 +0300

    faild with calendar icon

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,15 +2,10 @@ require('flatpickr/dist/themes/material_green.css');
 const flatpickr = require('flatpickr');
 
 $(function () {
-  $('#datepicker').flatpickr({
+  $('.datepick').flatpickr({
     altInput: true,
     altFormat: 'F j, Y',
     dateFormat: 'Y-m-d',
     maxDate: 'today',
-  });
-  $('#mybtn').on('click', function () {
-    let choosenDate = new Date (document.getElementById('datepicker').value);
-    let differentsOfDays = Math.floor((Date.now() - choosenDate.getTime()) / 1000 / 60 / 60 / 24);
-    $('#days').html(differentsOfDays);
   });
 });
