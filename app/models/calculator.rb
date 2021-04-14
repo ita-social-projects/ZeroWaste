697fd8d9c6ea29c9a0d1c85278fb14a16f0e0191
Viktoriia Bryzghalova <vicabr2000@gmail.com>
2021-04-14 13:04:25 +0300

    ZER-15 Change test and format hashes

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,12 +1,7 @@
 class Calculator < ApplicationRecord
   has_many :fields
-  before_validation :set_uuid
-  validates :name, :presence => true
-  validates :name, :format => { :with => /\A[a-zA-Z]+\z/,
-  :message => "Only letters allowed" }
-  validates :name, :length => { :minimum => 2 }
-  private
-  def set_uuid
-    self.uuid = SecureRandom.uuid if self.uuid.nil?
-  end
+  validates :name, presence: true
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+  message: "Only letters allowed" }
+  validates :name, length: { minimum: 2 }
 end
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -2,15 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Calculator, type: :model do
   before { build(:calculator) }
-  describe 'associations' do
-    it { should have_many(:fields)}
-  end
-
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_length_of(:name).
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).
               is_at_least(2).
               on(:create) }
-    it { should_not allow_value("Hh34").for(:name) }
+    it { is_expected.not_to allow_value("Hh34").for(:name) }
   end
 end
