e3c4762a6131440182234cec3dba9dae2b704d24
aboriiisova <aborisova971@gmail.com>
2021-04-14 17:24:43 +0300

    Create model ProductType

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,5 +1,6 @@
 class ProductType < ApplicationRecord
   has_many :products
+  
   validates :title, presence: true
   validates :title, length: { minimum: 3 }
   validates :title, format: { with: /[a-zA-Z0-9]/ }
