91db1350cd60a308bfcdcd4e84d2d715f3ddb1b9
Viktoriia Bryzghalova <vicabr2000@gmail.com>
2021-04-15 20:58:23 +0300

    ZER-25 Create Calculation model

--- spec/factories/calculations.rb
+++ spec/factories/calculations.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :calculation, parent: :field, class: Calculation do
+  factory :calculation do
     value { 'F1 * P2 / P5' }
     type { 'Calculation' }
     label { 'Label' }
