ec4856dac01cb757df6ddb83aca017ffc84a5064
haliapats <halynapa99@gmail.com>
2021-04-21 01:41:27 +0300

    ZER-36 Localize RangeField model

--- spec/models/range_field_spec.rb
+++ spec/models/range_field_spec.rb
@@ -5,14 +5,26 @@ require 'rails_helper'
 RSpec.describe RangeField, type: :model do
   subject { create(:range_field) }
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:from) }
-    it { is_expected.to validate_presence_of(:to) }
-    it { is_expected.to validate_presence_of(:value) }
-    it { is_expected.to validate_numericality_of(:from).only_integer }
-    it { is_expected.to validate_numericality_of(:to).only_integer }
+    it { is_expected.to validate_presence_of(:from)
+    .with_message(I18n.t('activerecord.errors.models.range_field.attributes.from.blank')) 
+      }
+    it { is_expected.to validate_presence_of(:to)
+    .with_message(I18n.t('activerecord.errors.models.range_field.attributes.to.blank')) 
+      }
+    it { is_expected.to validate_presence_of(:value)
+        .with_message(I18n.t('activerecord.errors.models.range_field.attributes.value.blank')) 
+      }
+    it { is_expected.to validate_numericality_of(:from).only_integer
+    #.with_message(I18n.t('activerecord.errors.models.range_field.attributes.from.not_an_integer'))
+      }
+    it { is_expected.to validate_numericality_of(:to).only_integer 
+    #.with_message(I18n.t('activerecord.errors.models.range_field.attributes.to.not_an_integer'))
+      }
     it {
       is_expected.to validate_length_of(:value)
         .is_at_least(1)
-    }
+        .with_message(I18n.t('activerecord.errors.models.range_field.attributes.value.too_short')) 
+      }
+  
   end
 end
