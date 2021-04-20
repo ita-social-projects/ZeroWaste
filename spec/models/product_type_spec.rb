c202f669e24fc574ec2f4cbfe3358d7c6bba095d
aboriiisova <aborisova971@gmail.com>
2021-04-20 20:03:54 +0300

    ZER-30 Localize ProductType model

--- spec/models/product_type_spec.rb
+++ spec/models/product_type_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe ProductType, type: :model do
     }
     it { is_expected.to allow_value('Ab2').for(:title) }
     it {
-      is_expected.to validate_length_of(:title).is_at_least(3).with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.too_short'))
+      is_expected.to validate_length_of(:title).is_at_least(3)
+                                               .with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.too_short'))
     }
     it {
       is_expected.not_to allow_value('.*+/+/').for(:title).with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.invalid'))
