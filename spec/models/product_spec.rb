dfe4301d7c3c773c09612fd25fbf27398b86def0
aboriiisova <aborisova971@gmail.com>
2021-04-21 00:06:58 +0300

    ZER-29 Localize Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe Product, type: :model do
   subject { build(:product) }
 
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_length_of(:title).is_at_least(2).is_at_most(50) }
+    it {
+      is_expected.to validate_presence_of(:title)
+        .with_message(I18n.t('activerecord.errors.models.product.attributes.title.blank'))
+    }
+    it {
+      is_expected.to validate_length_of(:title).is_at_least(2)
+                                               .with_message(I18n.t('activerecord.errors.models.product.attributes.title.too_short'))
+    }
+    it {
+      is_expected.to validate_length_of(:title).is_at_most(50)
+                                               .with_message(I18n.t('activerecord.errors.models.product.attributes.title.too_long'))
+    }
   end
 end
