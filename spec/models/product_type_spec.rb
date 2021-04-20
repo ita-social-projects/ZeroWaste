ff6d2bbb2df4252a813d3a35c145041c5f5c595e
aboriiisova <aborisova971@gmail.com>
2021-04-20 20:09:16 +0300

    ZER-30 Localize ProductType model

--- spec/models/product_type_spec.rb
+++ spec/models/product_type_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe ProductType, type: :model do
   subject { build(:product_type) }
   describe 'validations' do
     it {
-      is_expected.to validate_presence_of(:title).with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.blank'))
+      is_expected.to validate_presence_of(:title)
+        .with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.blank'))
     }
     it { is_expected.to allow_value('Ab2').for(:title) }
     it {
@@ -14,7 +15,8 @@ RSpec.describe ProductType, type: :model do
                                                .with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.too_short'))
     }
     it {
-      is_expected.not_to allow_value('.*+/+/').for(:title).with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.invalid'))
+      is_expected.not_to allow_value('.*+/+/').for(:title)
+                                              .with_message(I18n.t('activerecord.errors.models.product_type.attributes.title.invalid'))
     }
   end
 end
