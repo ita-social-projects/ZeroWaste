963d2d63fb32e7ce56ba5e725a4f2efc62e3f457
Oleksandr Kharchenko <oleksandrkharchenko7@gmail.com>
2021-04-22 17:31:17 +0300

    ZER-34 Localize Select model

--- spec/models/select_spec.rb
+++ spec/models/select_spec.rb
@@ -5,17 +5,17 @@ require 'rails_helper'
 RSpec.describe Select, type: :model do
   subject { create(:select) }
   let(:localization_prefix) do
-    'activerecord.errors.models.select.attributes'
+    'activerecord.errors.models.select.attributes.value'
   end
   describe 'validations' do
     it { is_expected.to be_valid }
     it {
       is_expected.to validate_presence_of(:value)
-        .with_message(I18n.t("#{localization_prefix}.value.blank"))
+        .with_message(I18n.t("#{localization_prefix}.blank"))
     }
     it {
       is_expected.to validate_length_of(:value).is_at_least(2)
-        .with_message(I18n.t("#{localization_prefix}.value.too_short"))
+        .with_message(I18n.t("#{localization_prefix}.too_short"))
     }
   end
 end
