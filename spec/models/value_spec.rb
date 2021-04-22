64adc61a927fe95650390b0d353f664da32782e0
PavloMS <78100331+PavloMS@users.noreply.github.com>
2021-04-22 17:51:41 +0300

    ZER-35 Localize Value model (#35)
    * ZER-35 Localize Value model
    
    * ZER-35 Localize Value model

--- spec/models/value_spec.rb
+++ spec/models/value_spec.rb
@@ -4,10 +4,21 @@ require 'rails_helper'
 
 RSpec.describe Value, type: :model do
   subject { create(:value) }
+  LOCALIZATION_PREFIX = 'activerecord.errors.models.value.attributes'.freeze
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to be_valid }
+    it {
+      is_expected.to validate_presence_of(:value)
+        .with_message(I18n.t("#{LOCALIZATION_PREFIX}.value.blank"))
+    }
     it { is_expected.to allow_value('Value').for(:value) }
-    it { is_expected.to validate_length_of(:value).is_at_least(1) }
-    it { is_expected.not_to allow_value('.*+/+/').for(:value) }
+    it {
+      is_expected.to validate_length_of(:value).is_at_least(1)
+                                               .with_message(I18n.t("#{LOCALIZATION_PREFIX}.value.too_short"))
+    }
+    it {
+      is_expected.not_to allow_value('.*+/+/').for(:value)
+                                              .with_message(I18n.t("#{LOCALIZATION_PREFIX}.value.invalid"))
+    }
   end
 end
