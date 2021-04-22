941a896a79142df90c499d7fff7b82ac2007a66d
PavloMS <pavlolv7@gmail.com>
2021-04-22 17:48:15 +0300

    ZER-35 Localize Value model

--- spec/models/value_spec.rb
+++ spec/models/value_spec.rb
@@ -4,23 +4,21 @@ require 'rails_helper'
 
 RSpec.describe Value, type: :model do
   subject { create(:value) }
-  let(:localization_prefix) do
-    'activerecord.errors.models.value.attributes'
-  end
+  LOCALIZATION_PREFIX = 'activerecord.errors.models.value.attributes'.freeze
   describe 'validations' do
     it { is_expected.to be_valid }
     it {
       is_expected.to validate_presence_of(:value)
-        .with_message(I18n.t("#{localization_prefix}.value.blank"))
+        .with_message(I18n.t("#{LOCALIZATION_PREFIX}.value.blank"))
     }
     it { is_expected.to allow_value('Value').for(:value) }
     it {
       is_expected.to validate_length_of(:value).is_at_least(1)
-                                               .with_message(I18n.t("#{localization_prefix}.value.too_short"))
+                                               .with_message(I18n.t("#{LOCALIZATION_PREFIX}.value.too_short"))
     }
     it {
       is_expected.not_to allow_value('.*+/+/').for(:value)
-                                              .with_message(I18n.t("#{localization_prefix}.value.invalid"))
+                                              .with_message(I18n.t("#{LOCALIZATION_PREFIX}.value.invalid"))
     }
   end
 end
