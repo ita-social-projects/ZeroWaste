3b5ac679b8c636cb6cfc3db416286a6705227539
PavloMS <pavlolv7@gmail.com>
2021-04-27 16:09:53 +0300

    ZER-17 Implement simple formula field evaluating

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -3,4 +3,14 @@
 class Calculation < Field
   validates_presence_of :value
   validates :value, length: { minimum: 2 }
+
+  def result(value, parameters = {})
+    hash_of_parameters = parameters
+    formula = value
+    calculator = Dentaku::Calculator.new
+    calculator.evaluate(formula, hash_of_parameters)
+  end
 end
+
+calc = Calculation.new
+calc.result('P1 + 2', { P1: params[:P1], P2: params[:P2] })
