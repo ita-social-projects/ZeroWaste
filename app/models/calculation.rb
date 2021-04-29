0de347ed67e80122163ff8a6646bdd660ec91e41
PavloMS <78100331+PavloMS@users.noreply.github.com>
2021-04-29 13:49:01 +0300

    ZER-17 Implement simple formula field evaluating (#47)
    * ZER-17 Implement simple formula field evaluating
    
    * ZER-17 Implement simple formula field evaluating
    
    * ZER-17 Implement simple formula field evaluating
    
    * ZER-17 Implement simple formula field evaluating
    
    * ZER-17 Implement simple formula field evaluating
    
    * ZER-17 Implement simple formula field evaluating
    
    * ZER-17 Implement simple formula field evaluating

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -3,4 +3,9 @@
 class Calculation < Field
   validates_presence_of :value
   validates :value, length: { minimum: 2 }
+
+  def result(parameters)
+    calculator = Dentaku::Calculator.new
+    calculator.evaluate(value, parameters)
+  end
 end
