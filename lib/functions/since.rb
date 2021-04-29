a2bb07a43f7c0c5e2bff72672b8966e5d00dfe99
Oleh <oleh.chupa@gmail.com>
2021-04-29 17:05:58 +0300

    Add additional test cases

--- lib/functions/since.rb
+++ lib/functions/since.rb
@@ -2,16 +2,26 @@
 
 class Since
   def self.calculate_units
-    proc { |from, to, unit| get_diff_of_date(from, to, unit) }
+    ->(from, to, unit) { get_diff_of_date(from, to, unit) }
   end
 
   def self.get_diff_of_date(from, to, unit)
-    unless 1.respond_to?(unit)
-      raise ArgumentError, "#{unit.inspect} is not supported as unit"
-    end
-
+    valid_date_format?(from, to)
+    valid_unit?(unit)
     diff = to - from
     distance = diff / 1.send(unit)
     distance.abs.round
+  end
+
+  def self.valid_date_format?(from, to)
+    return if from.is_a?(Time) && to.is_a?(Time)
+
+    raise ArgumentError, 'invalid date format'
+  end
+
+  def self.valid_unit?(unit)
+    return if 1.respond_to?(unit)
+
+    raise ArgumentError, "#{unit.inspect} is not supported as unit"
   end
 end
--- spec/functions/since_spec.rb
+++ spec/functions/since_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Since, type: :function do
     let(:calculate_units) { Since.calculate_units }
     let(:from) { Time.new(2001,01,01) }
     let(:to) { Time.new(2021,01,01) }
+    let(:invalid_date_format) { Date.new(2001,01,01) }
 
     it {
       expect(calculate_units).to be_a(Proc)
@@ -32,6 +33,24 @@ RSpec.describe Since, type: :function do
     context "when unit is `year`" do
       it {
       expect(calculate_units.call(from, to, 'year')).to be(20)
+      }
+    end
+
+    context "when `unit` is invalid" do
+      it {
+      expect { calculate_units.call(from, to, 'century') }.to raise_error("\"century\" is not supported as unit")
+      }
+    end
+
+    context "when `from` format is invalid" do
+      it {
+      expect { calculate_units.call(invalid_date_format, to, 'day') }.to raise_error("invalid date format")
+      }
+    end
+
+    context "when `to` format is invalid" do
+      it {
+      expect { calculate_units.call(from, invalid_date_format, 'day') }.to raise_error("invalid date format")
       }
     end
   end
