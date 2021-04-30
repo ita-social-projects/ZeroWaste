797b1d3debb306345e026be8c328f9e7b4d778ee
Oleh <oleh.chupa@gmail.com>
2021-04-30 14:00:21 +0300

    Change method names

--- lib/functions/since.rb
+++ lib/functions/since.rb
@@ -6,20 +6,20 @@ class Since
   end
 
   def self.get_diff_of_date(from, to, unit)
-    valid_date_format!(from, to)
-    valid_unit!(unit)
+    validate_date_format!(from, to)
+    validate_unit!(unit)
     diff = to - from
     distance = diff / 1.send(unit)
     distance.abs.round
   end
 
-  def self.valid_date_format!(from, to)
+  def self.validate_date_format!(from, to)
     return if from.is_a?(Time) && to.is_a?(Time)
 
-    raise ArgumentError, 'invalid date format'
+    raise ArgumentError, 'invalid time format'
   end
 
-  def self.valid_unit!(unit)
+  def self.validate_unit!(unit)
     return if 1.respond_to?(unit)
 
     raise ArgumentError, "#{unit.inspect} is not supported as unit"
--- spec/functions/since_spec.rb
+++ spec/functions/since_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Since, type: :function do
 
   describe "#calculate_units" do
     let(:calculate_units) { Since.calculate_units }
-    let(:from) { Time.new(2021,01,01) }
+    let(:from) { Time.new(2020,01,01) }
     let(:to) { Time.new(2021,01,31) }
     let(:invalid_date_format) { Date.new(2001,01,01) }
 
@@ -20,37 +20,37 @@ RSpec.describe Since, type: :function do
 
     context "when unit is `day`" do
       it {
-      expect(calculate_units.call(from, to, 'day')).to be(30)
+      expect(calculate_units.call(from, to, 'day')).to be(396)
       }
     end
 
     context "when unit is `month`" do
       it {
-      expect(calculate_units.call(from, to, 'month')).to be(1)
+      expect(calculate_units.call(from, to, 'month')).to be(13)
       }
     end
 
     context "when unit is `year`" do
       it {
-      expect(calculate_units.call(from, to, 'year')).to be(0)
+      expect(calculate_units.call(from, to, 'year')).to be(1)
       }
     end
 
     context "when `unit` is invalid" do
       it {
-      expect { calculate_units.call(from, to, 'century') }.to raise_error("\"century\" is not supported as unit")
+      expect { calculate_units.call(from, to, 'century') }.to raise_error(ArgumentError)
       }
     end
 
     context "when `from` format is invalid" do
       it {
-      expect { calculate_units.call(invalid_date_format, to, 'day') }.to raise_error("invalid date format")
+      expect { calculate_units.call(invalid_date_format, to, 'day') }.to raise_error(ArgumentError)
       }
     end
 
     context "when `to` format is invalid" do
       it {
-      expect { calculate_units.call(from, invalid_date_format, 'day') }.to raise_error("invalid date format")
+      expect { calculate_units.call(from, invalid_date_format, 'day') }.to raise_error(ArgumentError)
       }
     end
   end
