8262926e9ea8f80088e5ee3b586adab621092e51
Viktoriia Bryzghalova <vicabr2000@gmail.com>
2021-05-05 20:15:57 +0300

    ZER-56 Session expiration functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable
