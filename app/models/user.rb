9b6e7553284642a9976d6ade2f6d7a501a130cb2
Viktoriia Bryzghalova <vicabr2000@gmail.com>
2021-05-05 20:24:41 +0300

    ZER-56 Modified user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :lockable
+         :lockable, :timeoutable
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { minimum: 6, maximum: 100 },
