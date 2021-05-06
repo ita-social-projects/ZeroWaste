7037cbb24453d88a2c7794df1303083a595227ce
aboriiisova <68248705+aboriiisova@users.noreply.github.com>
2021-05-06 15:22:04 +0300

    ZER-44 Create 'IN' external function (#52)
    * ZER-44 Create 'IN' external function
    
    * ZER-44 Create 'IN' external function
    
    * ZER-44 Create IN external function
    
    * ZER-44 Create IN external function(fix naming)
    
    * ZER-44 Create IN external function(add test)
    
    * ZER-44 Create IN external function(add test)
    
    * ZER-44 Create IN external function(add test)

--- spec/models/range_field_spec.rb
+++ spec/models/range_field_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'functions/from_list'
 LOCAL_PREFIX_RANGE_FIELD = 'activerecord.errors.models.range_field.attributes'
 
 RSpec.describe RangeField, type: :model do
@@ -33,5 +34,19 @@ RSpec.describe RangeField, type: :model do
       is_expected.to validate_presence_of(:value)
         .with_message(I18n.t("#{LOCAL_PREFIX_RANGE_FIELD}.value.blank"))
     }
+    context 'when pass value with `from_list` formula' do
+      subject { RangeField }
+      let(:get_params) { FromList.to_hash }
+      let(:calculator) { build(:calculator) }
+      let(:range1) do
+        create(:range_field, from: 5, type: 'Calculation', label: 'label',
+                             kind: 'form', calculator: calculator, to: 19, value: '56')
+      end
+      let(:ranges) { [range1, range2, range3] }
+      let(:value) { "FROM_LIST(#{ranges})" }
+      it {
+        expect(get_params.call([range1])).to eq({ range1 => { range1.from..range1.to => range1.value } })
+      }
+    end
   end
 end
