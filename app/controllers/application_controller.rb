471fe3d5198a8e8039fc4c91d7ad61023da5e0a7
IvanShvets42 <ivanshvets42@gmail.com>
2021-05-07 03:36:15 +0300

    ZER-61 Sign Out link is not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :store_user_location!, if: :storable_location?
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? &&
+      !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def after_sign_out_path_for(*)
+    request.referer
+  end
 end
