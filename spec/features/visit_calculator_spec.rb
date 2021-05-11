0153defe7b1635a8675be5773eba605698c3dd3e
Viktoriia Bryzghalova <vicabr2000@gmail.com>
2021-05-11 21:03:00 +0300

    ZER-53.1 Create tests for login page

--- spec/features/visit_calculator_spec.rb
+++ spec/features/visit_calculator_spec.rb
@@ -3,9 +3,26 @@
 require 'rails_helper'
 
 describe 'visit Calculator page', js: true do
+  let(:user) { create(:user) }
   before { create(:calculator) }
+
   it 'visits calculator page' do
     visit '/calculators/diapers-calculator'
     expect(page).to have_content 'receive email messages'
+  end
+
+  it "visits calculator page and open log_in page" do
+    visit '/calculators/diapers-calculator' 
+    find('#log_in').click
+    expect(page).to have_content 'Log in'
+  end
+
+  it "when sign in with correct login and password" do
+    allow_any_instance_of(ApplicationController).to receive(:after_sign_in_path_for).and_return('/calculators/diapers-calculator')
+    visit '/users/sign_in' 
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_selector("a[href='/users/sign_out']")
   end
 end
