4fac1abd612d7b6b29afacb53c637c1ee1d28fb9
Viktoriia Bryzghalova <vicabr2000@gmail.com>
2021-05-12 19:27:04 +0300

    ZER-53.1 Create separate test for login page

--- spec/features/visit_calculator_spec.rb
+++ spec/features/visit_calculator_spec.rb
@@ -4,25 +4,16 @@ require 'rails_helper'
 
 describe 'visit Calculator page', js: true do
   let(:user) { create(:user) }
-  before { create(:calculator) }
+  let(:calculator) { create(:calculator) }
 
   it 'visits calculator page' do
-    visit '/calculators/diapers-calculator'
+    visit "/calculators/#{calculator.slug}"
     expect(page).to have_content 'receive email messages'
   end
 
   it "visits calculator page and open log_in page" do
-    visit '/calculators/diapers-calculator' 
-    find('#log_in').click
-    expect(page).to have_content 'Log in'
-  end
-
-  it "when sign in with correct login and password" do
-    allow_any_instance_of(ApplicationController).to receive(:after_sign_in_path_for).and_return('/calculators/diapers-calculator')
-    visit '/users/sign_in' 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    expect(page).to have_selector("a[href='/users/sign_out']")
+    visit "/calculators/#{calculator.slug}"
+    click_link ('Log In')
+    expect(page).to have_content 'Forgot your password'
   end
 end
