f92624803ddcb9c169bf27a3f4354bdc3945c687
Oleh <oleh.chupa@gmail.com>
2021-05-13 12:45:45 +0300

    Make changes requested by reviewer

--- db/migrate/20210510205349_add_last_visit_to_users.rb
+++ db/migrate/20210510205349_add_last_visit_to_users.rb
@@ -1,9 +1,11 @@
 class AddLastVisitToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :sign_in_count, :integer, default: 0, null: false
-    add_column :users, :current_sign_in_at, :datetime
-    add_column :users, :last_sign_in_at, :datetime
-    add_column :users, :current_sign_in_ip, :string
-    add_column :users, :last_sign_in_ip, :string
+    change_table :users do |t|
+      t.integer :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
+    end
   end
 end
