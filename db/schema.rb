831f04170fdd9416272e183305f341a16aad3153
haliapats <halynapa99@gmail.com>
2021-05-18 01:12:05 +0300

    Updated tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_05_13_165126) do
-=======
-
-ActiveRecord::Schema.define(version: 2021_05_10_205349) do
->>>>>>> master
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
