efff6ec1aa7b8cbe284e7b8f8a30f97d545eb136
Oleh <oleh.chupa@gmail.com>
2021-05-18 09:45:52 +0300

    ZER-64 Extend CalculatorResolver to get nested dependencies

--- app/models/calculator_resolver.rb
+++ app/models/calculator_resolver.rb
@@ -7,15 +7,45 @@ class CalculatorResolver
         hash[field] = []
         next
       end
-      hash[field] = get_dependencies(field[:value])
+      values_list = dependencies_list(field[:value])
+      hash[field] = get_dependencies(values_list)
     end
   end
 
-  def self.get_dependencies(value)
+  def self.get_dependencies(values_list)
+    dependencies_objects = []
+    if get_dependencies_values(values_list).present?
+      get_dependencies_values(values_list).each do |value|
+        dependencies_objects << value
+      end
+    end
+
+    unless get_dependencies_calculations(values_list).present?
+      return dependencies_objects
+    end
+
+    get_dependencies_calculations(values_list).each do |nested_calculation|
+      nested_values_list = dependencies_list(nested_calculation[:value])
+      dependencies_objects += get_dependencies(nested_values_list)
+    end
+
+    dependencies_objects
+  end
+
+  def self.dependencies_list(value)
     calculator = Dentaku::Calculator.new
     selectors = calculator.dependencies(value)
     selectors.map!(&:upcase)
+  end
+
+  def self.get_dependencies_values(value)
+    selectors = dependencies_list(value)
     Value.where(selector: selectors)
+  end
+
+  def self.get_dependencies_calculations(value)
+    selectors = dependencies_list(value)
+    Calculation.where(selector: selectors)
   end
 
   def self.get_fields(calculator)
@@ -23,6 +53,8 @@ class CalculatorResolver
   end
 
   class << self
-    private :get_dependencies, :get_fields
+    private :get_dependencies, :get_dependencies_values,
+            :get_dependencies_calculations, :dependencies_list,
+            :get_fields
   end
 end
--- spec/models/calculator_resolver_spec.rb
+++ spec/models/calculator_resolver_spec.rb
@@ -5,21 +5,27 @@ require 'rails_helper'
 RSpec.describe CalculatorResolver, type: :model do
   subject { described_class }
   let(:calculator) { build(:calculator) }
-  let!(:calculation) { create(:calculation, value: 'P1 * P2 / P3', type: 'Calculation', label: 'one', kind: 'result', calculator: calculator) }
+  let!(:calculation1) { create(:calculation, value: 'P1 * P2 / P3', type: 'Calculation', label: 'one', kind: 'result', calculator: calculator) }
+  let!(:calculation2) { create(:calculation, value: '10 * P4', type: 'Calculation', label: 'one', kind: 'result', calculator: calculator) }
   let!(:value1) { create(:value, value: 'Value', type: 'Value', label: 'two', kind: 'result', calculator: calculator) }
   let!(:value2) { create(:value, value: 'Value', type: 'Value', label: 'three', kind: 'parameter', calculator: calculator) }
   let!(:value3) { create(:value, value: 'Value', type: 'Value', label: 'four', kind: 'parameter', calculator: calculator) }
   let!(:value4) { create(:value, value: 'Value', type: 'Value', label: 'five', kind: 'parameter', calculator: calculator) }
+  let!(:calculation3) { create(:value, value: 'P1 * P2', type: 'Calculation', label: 'four', kind: 'parameter', calculator: calculator) }
 
   describe "#call" do
     it { expect(CalculatorResolver.call(calculator)).to be_kind_of(Hash) }
 
     context "when 'field' is 'Calculation'" do
-      it { expect(CalculatorResolver.call(calculator)[calculation]).to match_array([value2, value3, value4]) }
+      it { expect(CalculatorResolver.call(calculator)[calculation1]).to match_array([value2, value3, value4]) }
     end
 
     context "when 'field' is not 'Calculation'" do
       it { expect(CalculatorResolver.call(calculator)[value1]).to be_empty }
+    end
+
+    context "when 'Calculation' contains nested dependecies" do
+      it { expect(CalculatorResolver.call(calculator)[calculation2]).to match_array([value2, value3]) }
     end
   end
 end
