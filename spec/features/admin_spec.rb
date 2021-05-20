e227b43e5e68fe0cb11a1b3e8886b4fd2d4a8f9e
haliapats <halynapa99@gmail.com>
2021-05-20 11:48:53 +0300

    Returned the previous version

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'visit admin page', js: true do
   let(:time_login) { Time.new(2020,01,01).utc }
-  let!(:user1) { create(:user, email: 'test1@gmail.com', password: '123458789', last_sign_in_at: time_login) }
+  let!(:user1) { create(:user, email: 'test1@gmail.com', password: '12345878', last_sign_in_at: time_login) }
 
   it 'visits admin page' do
       visit '/admin'
