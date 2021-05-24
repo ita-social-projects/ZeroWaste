2d2c77184d1082cba145426adb5911fa7fb15724
IvanShvets42 <ivanshvets42@gmail.com>
2021-05-24 13:49:55 +0300

    ZER-68 Create a migration to create diapers calculator

---
+++ db/migrate/20210524103702_change_calculators.rb
@@ -0,0 +1,9 @@
+class ChangeCalculators < ActiveRecord::Migration[6.1]
+  def up
+    Calculator.create(name: 'Diapers Calculator')
+  end
+
+  def down
+    Calculator.destroy_by(name: 'Diapers Calculator')
+  end
+end
