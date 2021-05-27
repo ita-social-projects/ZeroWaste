f4b1273f140b02c1552d2aea0acb2fbde1ad02f1
aboriiisova <aborisova971@gmail.com>
2021-05-27 14:50:11 +0300

    ZER-58 Add validation and error messaging to the registration page

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,35 +1,38 @@
 # frozen_string_literal: true
 
-class Admins::SessionsController < Devise::SessionsController
-  layout 'admin'
-  before_action :check_user
+module Admins
+  class SessionsController < Devise::SessionsController
+    layout 'admin'
+    before_action :check_user
 
-  protected
-  def check_user
-    if current_admin
-      flash.clear
-      redirect_to('/admin') and return
+    protected
+
+    def check_user
+      if current_admin
+        flash.clear
+        redirect_to('/admin') and return
+      end
     end
+    # GET /resource/sign_in
+    # def new
+    #   super
+    # end
+
+    # POST /resource/sign_in
+    # def create
+    #  super
+    # end
+
+    # DELETE /resource/sign_out
+    # def destroy
+    #   super
+    # end
+
+    # protected
+
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_in_params
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    # end
   end
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  #def create
-  #  super
-  #end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module User
+  class RegistrationsController < Devise::RegistrationsController
+    # Override the action you want here.
+    protect_from_forgery with: :exception
+    before_action :sign_up_params
+
+    private
+
+    def sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up,
+                                        keys: %i[first_name last_name country])
+    end
+  end
+end
