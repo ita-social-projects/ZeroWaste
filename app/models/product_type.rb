916170b5276f45e184083018d4165779d4c84448
olenochka123 <53562002+olenochka123@users.noreply.github.com>
2021-08-12 13:37:03 +0300

    Delete product_type.rb

--- app/models/product_type.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ProductType < ApplicationRecord
-  has_many :products, dependent: :destroy
-
-  validates :title, presence: true
-  validates :title, length: { minimum: 3 }
-  validates :title, format: { with: /[a-zA-Z0-9]/ }
-end
