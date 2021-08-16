de2d0ce9b4c10103549193364a350d243e8530da
halyna-m <galya.mik16@gmail.com>
2021-08-16 11:49:33 +0200

    Created constant for path instead of code duplication

--- spec/features/reset_password_spec.rb
+++ spec/features/reset_password_spec.rb
@@ -5,12 +5,13 @@ require 'rails_helper'
 describe 'Password Reset Page', js: true do
   let(:user) { create(:user) }
   let(:calculator) { create(:calculator) }
+  PASSWORD_RESET_PATH = '/users/password/new'
 
   context "when user clicks button Send me reset password instructions" do
     it "shows message that user will receive reset password instructions" do
       allow_any_instance_of(ApplicationController).to receive(:after_sign_in_path_for).and_return("/calculators/#{calculator.slug}")
       allow(Devise::Mailer).to receive(:reset_password_instructions).and_return(double(deliver: true))
-      visit '/users/password/new' 
+      visit PASSWORD_RESET_PATH
       fill_in 'Email', with: user.email
       click_button 'Send me reset password instructions'
       expect(page).to have_content('If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes.')
@@ -19,7 +20,7 @@ describe 'Password Reset Page', js: true do
 
   context "when user clicks button Log in" do
     it "redirect to sign in page" do
-      visit '/users/password/new' 
+      visit PASSWORD_RESET_PATH
       click_link 'Log in'
       expect(page).to have_current_path("/users/sign_in")
     end
@@ -27,7 +28,7 @@ describe 'Password Reset Page', js: true do
 
   context "when user clicks button Sign up" do
     it "redirect to sign up page" do
-      visit '/users/password/new' 
+      visit PASSWORD_RESET_PATH
       click_link 'Sign up'
       expect(page).to have_current_path("/users/sign_up")
     end
