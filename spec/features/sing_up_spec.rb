b60c9a0c5444c701a29a6fa79f5877c39ca95f47
andriibureviy <andrii.yazvinskyi@gmail.com>
2021-08-16 11:46:26 +0300

    Add Capybara test for Sign up page

--- spec/features/sing_up_spec.rb
+++ spec/features/sing_up_spec.rb
@@ -1,10 +1,9 @@
-#Add Capybara test for Sign Up page
-#ZER-46
 require 'rails_helper'
 
 describe "User Sign Up",  js: true do
   let(:calculator) { create(:calculator) }
-    it "Right registers a new user" do
+  context "when sign up with correct password and email  " do
+    it "shows a message that the user will receive a confirmation link in the mail" do
       allow_any_instance_of(ApplicationController).to receive(:after_sign_in_path_for).and_return("/calculators/#{calculator.slug}")
       allow(Devise::Mailer).to receive(:confirmation_instructions).and_return(double(deliver: true))
       visit '/users/sign_up'
@@ -18,32 +17,19 @@ describe "User Sign Up",  js: true do
       click_button 'Sing Up'
       expect(page).to have_content 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
     end
-
-    it "Wrong registers a new user" do
+  end
+  context "when sign up with incorrect password or email" do
+    it "redirect to sign up page" do
       visit '/users/sign_up'
-
-      # click_link "Sign Up"
-      fill_in 'Email', with: 'wrong email.com'
-      fill_in 'Password', with: '1'
-      fill_in 'Password Confirmation', with: '11'
+      fill_in 'Email', with: ' '
+      fill_in 'Password', with: ' '
+      fill_in 'Password Confirmation', with: ' '
       fill_in 'First Name', with: ' '
       fill_in 'Last Name', with: ' '
       select 'Albania', from: 'user_country'
       click_button 'Sing Up'
-      expect(page).to have_text 'Password (6 Characters Minimum)'# different between have_text and have_content
+      expect(page).to have_content 'Password (6 Characters Minimum)'
     end
-
-    it "No right password" do
-      visit '/users/sign_up'
-
-      fill_in 'Email', with: 'simple@email.com'
-      fill_in 'Password', with: ' '
-      fill_in 'Password Confirmation', with: '1111111'
-      fill_in 'First Name', with: 'User'
-      fill_in 'Last Name', with: 'User'
-      select 'Albania', from: 'user_country'
-      click_button 'Sing Up'
-      expect(page).to have_content 'Email'
-    end
+  end
 end
 
