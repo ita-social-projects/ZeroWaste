9e788885e8ae9756fb3605e1fffd2f073f275994
andriibureviy <andrii.yazvinskyi@gmail.com>
2021-09-01 15:15:58 +0300

    Move API

--- app/controllers/api/v2/calculators_controller.rb
+++ app/controllers/api/v2/calculators_controller.rb
@@ -2,11 +2,10 @@
 
 module Api
   module V2
-      class CalculatorsController < ApplicationController
-        def compute
-          @fields = Calculator.find_by(slug: params['id']).fields.result
-          render json: @fields, root: 'result', adapter: :json
-        end
+    class CalculatorsController < ApplicationController
+      def compute
+        @fields = Calculator.find_by(slug: params['id']).fields.result
+        render json: @fields, root: 'result', adapter: :json
       end
     end
   end
