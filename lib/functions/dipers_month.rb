1cf10797203e23893f14e6942c70ec69d5fe6c55
Nestor Khomytskiy <nestor.homitskiy@gmail.com>
2021-09-14 08:13:01 +0300

    refactoring code according to the condition of the task

--- lib/functions/dipers_month.rb
+++ lib/functions/dipers_month.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class Dipers
-  def dipers_per_month(x, data = {})
+  def dipers_per_month(x, data)
     sum = 0
-    data.each do |key, value|
-      sum += value if key <= x
+    (x + 1).times do |i|
+      set = data.find { |months, _| months.include?(i) }
+      sum += set[1] if set
     end
-    p sum
+    sum
   end
 end
+
+# calc = Dipers.new
+# p calc.dipers_per_month(0, {0..2 => 30, 3..5 => 70})
--- spec/functions/dipers_month_rspec.rb
+++ spec/functions/dipers_month_rspec.rb
@@ -9,26 +9,24 @@ describe Dipers do
   context 'calculate' do
     it do
       expect(dipers.dipers_per_month(2,
-                                     { 0 => 30, 1 => 40, 2 => 70 })).to eq(140)
+                                     { 0..2 => 30, 3..5 => 70 })).to eq(90)
     end
   end
   context 'calculate' do
     it do
       expect(dipers.dipers_per_month(5,
-                                     { 0 => 30, 1 => 30, 2 => 30, 3 => 70,
-                                       4 => 70, 5 => 70 })).to eq(300)
+                                     { 0..2 => 30, 3..5 => 70 })).to eq(300)
     end
   end
   context 'calculate' do
     it do
-      expect(dipers.dipers_per_month(5,
-                                     { 0 => 40, 1 => 40, 2 => 70, 3 => 90,
-                                       4 => 100, 5 => 100 })).to eq(440)
+      expect(dipers.dipers_per_month(4,
+                                     { 0..2 => 30, 3..5 => 70 })).to eq(230)
     end
   end
   context 'calculate' do
     it do
-      expect(dipers.dipers_per_month(2, { 0 => 40, 1 => 40 })).to eq(80)
+      expect(dipers.dipers_per_month(1, { 0..2 => 30, 3..5 => 70 })).to eq(60)
     end
   end
 end
