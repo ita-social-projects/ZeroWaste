525f1bb1339aadfdbf9208993b9017b46933ccc8
halyna-m <galya.mik16@gmail.com>
2021-09-16 11:46:13 +0200

    Created before blocks in capybara tests

--- spec/features/admin/create_calculator_spec.rb
+++ spec/features/admin/create_calculator_spec.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-CREATE_CALCULATOR_PATH = '/admins/calculators/new'
 CREATE_CALCULATOR_BUTTON = 'Create calculator'
 
 describe 'Create Calculator Page', js: true do
   let(:calculator) { create(:calculator) }
+  before do 
+    visit '/admins/calculators/new'
+  end
 
   context 'when user clicks button Create calculator' do
     it 'shows message that calculator has been successfully created' do
-      visit CREATE_CALCULATOR_PATH
       fill_in 'Name', with: 'Calculator1'
       click_button CREATE_CALCULATOR_BUTTON
       expect(page).to have_content('Calculator has been successfully created')
@@ -18,7 +19,6 @@ describe 'Create Calculator Page', js: true do
 
   context 'when user clicks button Create calculator' do
     it 'redirects to Edit calculator page' do
-      visit CREATE_CALCULATOR_PATH
       fill_in 'Name', with: 'Calculator2'
       click_button CREATE_CALCULATOR_BUTTON
       expect(page).to have_current_path("/admins/calculators/1/edit")
@@ -27,7 +27,6 @@ describe 'Create Calculator Page', js: true do
 
   context 'when user fill in the Name field with name that already exist' do
     it 'shows message that name is already has been taken' do
-      visit CREATE_CALCULATOR_PATH
       fill_in 'Name', with: calculator.name
       click_button CREATE_CALCULATOR_BUTTON
       expect(page).to have_content('Name has already been taken')
@@ -36,7 +35,6 @@ describe 'Create Calculator Page', js: true do
 
   context 'when user fill in the Name field with name shorter than 2 symbols' do
     it 'shows message that name is too short' do
-      visit CREATE_CALCULATOR_PATH
       fill_in 'Name', with: 'i'
       click_button CREATE_CALCULATOR_BUTTON
       expect(page).to have_content("The field 'Name' is too short.")
@@ -45,7 +43,6 @@ describe 'Create Calculator Page', js: true do
 
   context 'when user fill in the Name field with symbols' do
     it 'shows message that name is invalid' do
-      visit CREATE_CALCULATOR_PATH
       fill_in 'Name', with: 'i[]p'
       click_button CREATE_CALCULATOR_BUTTON
       expect(page).to have_content("The field 'Name' is invalid")
@@ -54,7 +51,6 @@ describe 'Create Calculator Page', js: true do
 
   context 'when user left the Name field blank' do
     it 'shows message that name can\'t be blank' do
-      visit CREATE_CALCULATOR_PATH
       fill_in 'Name', with: ''
       click_button CREATE_CALCULATOR_BUTTON
       expect(page).to have_content("The field 'Name' can\'t be blank.")
--- spec/features/admin/update_calculator_spec.rb
+++ spec/features/admin/update_calculator_spec.rb
@@ -5,10 +5,12 @@ UPDATE_CALCULATOR_BUTTON = 'Update calculator'
 
 describe 'Update Calculator Page', js: true do
   let(:calculator) { create(:calculator) }
-  
+  before do 
+    visit "/admins/calculators/#{calculator.id}/edit"
+  end
+
   context 'when user clicks button Update calculator' do
     it 'shows message that calculator has been successfully updated' do
-      visit "/admins/calculators/#{calculator.id}/edit"
       fill_in 'Name', with: 'Calculator2'
       click_button UPDATE_CALCULATOR_BUTTON
       expect(page).to have_content('Calculator has been successfully updated')
@@ -17,7 +19,6 @@ describe 'Update Calculator Page', js: true do
 
   context 'when user fill in the Name field with name shorter than 2 symbols' do
     it 'shows message that name is too short' do
-      visit "/admins/calculators/#{calculator.id}/edit"
       fill_in 'Name', with: 'o'
       click_button UPDATE_CALCULATOR_BUTTON
       expect(page).to have_content("The field 'Name' is too short.")
@@ -26,7 +27,6 @@ describe 'Update Calculator Page', js: true do
 
   context 'when user fill in the Name field with symbols' do
     it 'shows message that name is invalid' do
-      visit "/admins/calculators/#{calculator.id}/edit"
       fill_in 'Name', with: '\[d]]p'
       click_button UPDATE_CALCULATOR_BUTTON
       expect(page).to have_content("The field 'Name' is invalid")
@@ -35,7 +35,6 @@ describe 'Update Calculator Page', js: true do
 
   context 'when user left the Name field blank' do
     it 'shows message that name can\'t be blank' do
-      visit "/admins/calculators/#{calculator.id}/edit"
       fill_in 'Name', with: ''
       click_button UPDATE_CALCULATOR_BUTTON
       expect(page).to have_content("The field 'Name' can\'t be blank.")
