a28c9df72c9e39c6305b64ff098314f41292c3fe
Maksym Stukalo <mstuk@softserveinc.com>
2021-09-21 13:18:24 +0300

    another try to fix test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   config.cache_classes = false
   config.action_view.cache_template_loading = true
-
+  config.assets.compile = true
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
