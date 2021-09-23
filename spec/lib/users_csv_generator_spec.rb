04f8a10834f673633ce286c7784f6279615c4a8a
andriibureviy <andrii.yazvinskyi@gmail.com>
2021-09-23 18:20:31 +0300

    Add Rspec test for UserCsvGenerator

--- spec/lib/users_csv_generator_spec.rb
+++ spec/lib/users_csv_generator_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-
-RSpec.describe UsersCsvGenerator ,js: true do
+RSpec.describe UsersCsvGenerator, js: true do
   let(:time_login) { Time.new(2020, 0o1, 0o1).utc }
-  let!(:users) { [create(:user, email: 'test@gmail.com', last_sign_in_at: time_login)] }
+  let!(:users) do
+    [create(:user, email: 'test@gmail.com', last_sign_in_at: time_login)]
+  end
 
-  context '' do
-    it 'test' do
-      expect(UsersCsvGenerator.call(users,
-                                    fields: %w[email last_sign_in_at])).to eq("email,last_sign_in_at\ntest@gmail.com,#{time_login}\n")
-    end
+  it 'when export user' do
+    expect(UsersCsvGenerator.call(users,
+                                  fields: %w[email last_sign_in_at])).to
+    eq("email,last_sign_in_at\ntest@gmail.com,#{time_login}\n")
   end
 end
