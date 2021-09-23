94435789dcb11552f48f70df5bc5ec1b3e540777
andriibureviy <andrii.yazvinskyi@gmail.com>
2021-09-23 19:07:10 +0300

    Fix Rspec test

--- spec/lib/users_csv_generator_spec.rb
+++ spec/lib/users_csv_generator_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-RSpec.describe UsersCsvGenerator, js: true do
+RSpec.describe UsersCsvGenerator, type: :lib do
   let(:time_login) { Time.new(2020, 0o1, 0o1).utc }
-  let!(:users) do
+  let(:users) do
     [create(:user, email: 'test@gmail.com', last_sign_in_at: time_login)]
   end
 
-  it 'when export user' do
+  it 'export users' do
     expect(UsersCsvGenerator.call(users,
                                   fields: %w[email
                                              last_sign_in_at]))
