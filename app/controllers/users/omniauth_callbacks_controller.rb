3ba3b8b6a078abb2f39be737ea0be36c51cc7bcb
halyna-m <galya.mik16@gmail.com>
2021-09-27 07:02:01 +0200

    Added Google sign in

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication 
+        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      else
+        session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except("extra")
+        redirect_to new_user_registration_url
+      end
+    end
+  
+    def failure
+      redirect_to root_path
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :lockable, :timeoutable, :trackable, :confirmable
+         :lockable, :timeoutable, :trackable, :confirmable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { minimum: 6, maximum: 100 },
@@ -17,4 +18,20 @@ class User < ApplicationRecord
                                      length: { minimum: 2 },
                                      on: :create,
                                      format: { with: /[a-zA-Zа-їА-ЯЄІЇ]+-?'?`?/ }
+                                     
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name   
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.google_oauth2"] && session["devise.google_oauth2_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
