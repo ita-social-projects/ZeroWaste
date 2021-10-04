588b8ed97ecf780963ecb2a8414d988736ee0c03
olenochka123 <53562002+olenochka123@users.noreply.github.com>
2021-10-04 13:24:59 +0300

    adding v1 route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :calculators, only: [] do
-        get :compute
+        post :compute
       end
     end
     namespace :v2 do
