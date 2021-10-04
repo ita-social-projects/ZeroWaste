b49609f17ebe14991369dcd9e886fa9a257a8a55
olenochka123 <53562002+olenochka123@users.noreply.github.com>
2021-10-04 11:19:15 +0300

    Create Calculator V1 compute endpoint

--- app/controllers/api/v1/calculators_controller.rb
+++ app/controllers/api/v1/calculators_controller.rb
@@ -3,6 +3,10 @@
 module Api
   module V1
     class CalculatorsController < ApplicationController
+      VALUES = {bought_diapers:8956,money_spent:7841,garbage_created:342}
+      def compute
+        render json:VALUES
+      end
     end
   end
 end
