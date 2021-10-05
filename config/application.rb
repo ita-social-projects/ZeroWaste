9364aadcb7e7bff67b0f7c263e0a283aaf842fa8
hacker2008 <nazarik9532@gmail.com>
2021-10-05 13:04:25 +0300

    Fix

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +12,7 @@ Bundler.require(*Rails.groups)
 
 module ZeroWaste
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
