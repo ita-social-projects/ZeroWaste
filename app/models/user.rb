af8552541cf38dc5ffba714e01eea1f84921ddcb
hacker2008 <nazarik9532@gmail.com>
2021-10-05 20:57:15 +0300

    Final fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :lockable, :timeoutable, :trackable, :confirmable,
          :omniauthable, omniauth_providers: %i[google_oauth2 facebook]
-  # devise :omniauthable, omniauth_providers: %i[facebook]
+
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { minimum: 6, maximum: 100 },
