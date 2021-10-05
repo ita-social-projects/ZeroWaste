d14b8097c35bf5aacad82937630fa3d9ea07e9d8
hacker2008 <nazarik9532@gmail.com>
2021-10-05 12:53:26 +0300

    Fix

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'csv'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module ZeroWaste
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
