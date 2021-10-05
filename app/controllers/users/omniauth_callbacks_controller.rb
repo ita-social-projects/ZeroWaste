9a70d6bf9fd328f916411440ba4e1946478438ac
hacker2008 <nazarik9532@gmail.com>
2021-10-05 16:33:01 +0300

    Fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,8 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    OMNIAUTH = 'omniauth.auth'
+
     include OmniConcern
     %w[facebook twitter gplus linkedin].each do |meth|
       define_method(meth) do
@@ -10,19 +12,22 @@ module Users
     end
 
     def facebook
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+      @user = User.from_omniauth(request.env[OMNIAUTH])
 
       if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        sign_in_and_redirect @user, event: :authentication
+        if is_navigational_format?
+          set_flash_message(:notice, :success,
+                            kind: 'Facebook')
+        end
       else
-        session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+        session['devise.facebook_data'] = request.env[OMNIAUTH].except(:extra)
         redirect_to new_user_registration_url
       end
     end
 
     def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+      @user = User.from_omniauth(request.env[OMNIAUTH])
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
         if is_navigational_format?
@@ -31,7 +36,7 @@ module Users
         end
       else
         session['devise.google_oauth2_data'] =
-          request.env['omniauth.auth'].except('extra')
+          request.env[OMNIAUTH].except('extra')
         redirect_to new_user_registration_url
       end
     end
