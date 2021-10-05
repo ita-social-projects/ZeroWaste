99282d7aa2a5ef57c2a8b417024a43caf270eca3
olenochka123 <53562002+olenochka123@users.noreply.github.com>
2021-10-05 18:21:06 +0300

    Update calculators_controller.rb

--- app/controllers/api/v1/calculators_controller.rb
+++ app/controllers/api/v1/calculators_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class CalculatorsController < ApplicationController
       VALUES = { bought_diapers: 8956, money_spent: 7841, garbage_created: 342 }
+      
       def compute
         render json: VALUES
       end
