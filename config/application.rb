eee9cbc267b27ecd7bc03e62320b26bf54e714c4
hacker2008 <nazarik9532@gmail.com>
2021-10-05 13:21:44 +0300

    Fix

--- config/application.rb
+++ config/application.rb
@@ -3,11 +3,6 @@
 
 require_relative 'boot'
 
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-
 require 'rails/all'
 require 'csv'
 
