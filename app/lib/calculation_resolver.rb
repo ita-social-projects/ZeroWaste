4dcf5f13b1f5c79bfa1e58e55ae0463942339e8b
Maksym Stukalo <mstuk@softserveinc.com>
2021-10-25 18:16:32 +0300

    more fixes

--- app/lib/calculation_resolver.rb
+++ app/lib/calculation_resolver.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require 'functions/from_list'
+require 'functions/diapers_per_month'
+require 'functions/since'
 
 class CalculationResolver
   def self.result(parameters, value)
     calculator = Dentaku::Calculator.new
     calculator.add_function(:since, :numeric, Since.calculate_units)
     calculator.add_function(:from_list, :numeric, FromList.to_hash)
-    calculator.add_function(:dipers_per_month, :numeric, Dipers.dipers_per_month)
+    calculator.add_function(:dipers_per_month, :numeric, DiapersPerMonth.call)
     calculator.evaluate(value, parameters)
   end
 end
