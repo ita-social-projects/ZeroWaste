983a02bd08a3cafbd2175e859b9789b4627403c7
AnastasiiaBilynska <nk015132@gmail.com>
2021-12-03 15:14:58 +0200

    first commit

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -17,7 +17,26 @@ module Admins
       end
     end
 
+    def edit
+      @user = find_user
+    end
+
+    def update
+      @user = find_user
+      if @user.update(user_params)
+        flash[:success] = 'User`s profile has been updated!'
+        redirect_to admins_user_path(@user)
+      else
+        flash[:error] = 'User`s profile hasn`t been updated!'
+        render 'edit'
+      end
+    end
+
     private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :country)
+    end
 
     def find_user
       @user = User.find(params[:id])
