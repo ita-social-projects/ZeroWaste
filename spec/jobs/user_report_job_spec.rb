5d4d1f41a3d0de7ba5f6b93a4764c3c6ec017329
AnastasiiaBilynska <nk015132@gmail.com>
2021-12-17 20:50:18 +0200

    corrected tests

--- spec/jobs/user_report_job_spec.rb
+++ spec/jobs/user_report_job_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 
 RSpec.describe UserReportJob, type: :job do
-  describe "#perform_later" do
-  let(:job_report) { UserReportJob.perform_later('report') }
-    it "uploads a report" do 
-     expect { :job_report }.to have_enqueued_job
+  describe '#perform_later' do
+    let(:job_report) { UserReportJob.perform_later('report') }
+    let(:job_with_date) do 
+      UserReportJob
+        .set(wait_until: Date.tomorrow.noon, queue: 'low')
+        .perform_later('report') 
     end
-  let(:job_with_date) { UserReportJob.set(wait_until: Date.tomorrow.noon, queue: "low").perform_later('report') }
-    it "uploads a report" do
-      expect{ :job_with_date }.to have_enqueued_job.with('report').on_queue("low").at(Date.tomorrow.noon)
+
+    it 'uploads a report' do 
+     expect { job_report }.to have_enqueued_job
+    end
+
+    it 'uploads a report' do
+      expect{ job_with_date }.to have_enqueued_job
+        .with('report').on_queue('low').at(Date.tomorrow.noon)
     end
   end
 end
