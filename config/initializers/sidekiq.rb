8442b2a0c60f66e3eb0c9dfe99484e9c76ba8d22
AnastasiiaBilynska <nk015132@gmail.com>
2021-12-17 20:30:50 +0200

    corrected hash

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size: 1, :url: ENV['REDIS_URL'], :namespace: ENV["SIDEKIQ_NAMESPACE"] }
+  config.redis = { size: 1, url: ENV['REDIS_URL'], namespace: ENV['SIDEKIQ_NAMESPACE'] }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :size: 7, :url: ENV['REDIS_URL'], :namespace: ENV["SIDEKIQ_NAMESPACE"] }
+  config.redis = { size: 7, url: ENV['REDIS_URL'], namespace: ENV['SIDEKIQ_NAMESPACE'] }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,6 +76,4 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
-
-
 end
