aa3d6e32242725ec01def5c9970fa75daea2b4e9
Olexandra Orlova <alexandra.volosko@gmail.com>
2021-12-22 14:03:39 +0200

    Fix calendar input on page load

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,7 @@
 require('flatpickr/dist/themes/material_green.css');
 const flatpickr = require('flatpickr');
 
-$(function () {
+$(document).on('turbolinks:load', () => {
   $('.datepick').flatpickr({
     altInput: true,
     altFormat: 'F j, Y',
