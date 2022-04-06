902f12a3625e76bc0a702cc24548d0f0d6872e7f
Dmytro Losiev <junglebrandlviv@gmail.com>
2022-04-06 11:10:54 +0300

    modified result_button.js

--- app/javascript/ajax/result_button.js
+++ app/javascript/ajax/result_button.js
@@ -15,7 +15,27 @@ $(document).on('turbolinks:load', function() {
       url: "/api/v1/calculators/diapers-calculator/compute",
       type: "POST",
       data: formData,
-      dataType: "json"
+      dataType: "json",
+      success: function(data) {
+                  
+
+                  const boughtDiapers = data.result.find(function(post, index) {
+                    if(post.name == 'bought_diapers')
+                      return true;
+                  });
+                  const moneySpent = data.result.find(function(post, index) {
+                    if(post.name == 'money_spent')
+                      return true;
+                  });  
+                  const garbageCreated = data.result.find(function(post, index) {
+                    if(post.name == 'garbage_created')
+                      return true;
+                  });  
+
+        $('[data-type="bought_diapers"]').text(boughtDiapers.result);
+        $('[data-type="money_spent"]').text(moneySpent.result);
+        $('[data-type="garbage_created"]').text(garbageCreated.result);
+      }
     })
   });
 })
