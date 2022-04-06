f180f06301e5a090fc58fe6a9de9f2b4c586dbc7
Dmytro Losiev <junglebrandlviv@gmail.com>
2022-04-06 11:10:54 +0300

    Add calculator logic and modified controller

--- app/controllers/api/v1/diaper_calculators_controller.rb
+++ app/controllers/api/v1/diaper_calculators_controller.rb
@@ -9,7 +9,20 @@ module Api
       ].freeze
 
       def create
-        render(json: { result: VALUES, date: params[:childs_birthday] })
+        result = diapers_service_handler(childs_birthday).calculate!
+        VALUES.first[:result] = result.used_diapers_price
+        VALUES.last[:result] = result.to_be_used_diapers_price
+        render(json: { result: VALUES, date: childs_birthday })
+      end
+
+      private
+
+      def diapers_service_handler(age)
+        @diapers_service_handler ||= Calculators::DiapersService.new(age)
+      end
+
+      def childs_birthday
+        params[:childs_birthday]
       end
     end
   end
