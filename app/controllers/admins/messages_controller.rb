0a0ec920a5f15d86332aa9d0f38bc3c81ee8db77
Oleh Kovalchuk <rod07@ukr.net>
2022-04-26 16:50:16 +0300

    Fix tests/ add test

--- app/controllers/admins/messages_controller.rb
+++ app/controllers/admins/messages_controller.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 module Admins
   class MessagesController < BaseController
-      
     def index
       @message = Message.order(created_at: :desc)
     end
 
     def show
-      @message ||= Message.find(params[:id])
+      @message = Message.find(params[:id])
     end
-    
   end
 end
