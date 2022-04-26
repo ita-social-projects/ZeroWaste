b5f9fa0c1115aa9605e4359c19373ac5eef164dc
Oleh Kovalchuk <rod07@ukr.net>
2022-04-26 14:20:37 +0300

    sending contact_us message

--- app/controllers/admins/messages_controller.rb
+++ app/controllers/admins/messages_controller.rb
@@ -1,19 +1,13 @@
 module Admins
   class MessagesController < BaseController
+      
+    def index
+      @message = Message.order(created_at: :desc)
+    end
+
+    def show
+      @message ||= Message.find(params[:id])
+    end
     
-  def index
-    @message = Message.order(created_at: :desc)
-  end
-
-  def show
-    @message ||= Message.find(params[:id])
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:title, :message, :email)
-  end
-
   end
 end
