0b1f2797f1720860cb0c7ce1cff19295c2ba2333
BeforeBlackout <77724437+BeforeBlackout@users.noreply.github.com>
2022-05-03 10:47:58 +0300

    ZER 127 (#183)
    * ZER 127
    
    fixed login with Google
    updated user.rb
    refactoring/rubocop
    Fixed rubocop lint errors
    
    * added env variables

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,23 +2,23 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        if is_navigational_format?
-          set_flash_message(:notice, :success,
-                            kind: 'Google')
-        end
-      else
-        session['devise.google_oauth2_data'] =
-          request.env['omniauth.auth'].except('extra')
-        redirect_to new_user_registration_url
-      end
-    end
-
     def failure
       redirect_to root_path
+    end
+
+    def google_oauth2
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+
+      if @user.persisted?
+        flash[:notice] =
+          I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
+      else
+        session['devise.google_data'] =
+          request.env['omniauth.auth'].except('extra')
+        redirect_to new_user_registration_url,
+                    alert: @user.errors.full_messages.join("\n")
+      end
     end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,11 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  google_client_id = Rails.application.credentials.google_oauth2[:client_id]
-  google_client_secret = Rails.application.credentials.google_oauth2[:client_secret]
-  config.omniauth :google_oauth2, google_client_id, google_client_secret,
-                  scope: 'userinfo.email,userinfo.profile', skip_jwt: true
-
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
