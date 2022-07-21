4c9113b832a3392a699b3f1d7986600f54af66cd
Vladyslav Denysiuk <vl.denysiuk@gmail.com>
2022-07-21 12:22:44 +0300

    Fix bug when it shows 0-11 months instead of 0-6 when selecting 2 years

--- app/javascript/ajax/calculate_result_button.js
+++ app/javascript/ajax/calculate_result_button.js
@@ -2,47 +2,24 @@ $(document).on('turbolinks:load', function() {
   const form = document.getElementById('form');
   const childs_years = document.getElementById('childs_years');
   const childs_months = document.getElementById('childs_months');
-  const two_years_childs_months = document.getElementById('two_years_childs_months');
-  
   if (!form) {
     return
   }
-
-  two_years_childs_months.classList.add('hidden')
-  two_years_childs_months.required = false
-  
-  childs_years.addEventListener('input', () => {
-    if (childs_years.value == 2) {
-      childs_months.classList.add('hidden')
-      childs_months.required = false
-
-      two_years_childs_months.classList.remove('hidden')
-      two_years_childs_months.required = true
+  childs_years.addEventListener('click',() => {
+    if(childs_years.value < 2) {
+      childs_months.classList.remove('last-year');
     } else {
-      two_years_childs_months.classList.add('hidden')
-      two_years_childs_months.required = false
-
-      childs_months.classList.remove('hidden')
-      childs_months.required = true
+      childs_months.selectedIndex = 0;
+      childs_months.classList.add('last-year');
     }
   });
-
   form.addEventListener('submit', function(e) {
 
     e.preventDefault();
 
-    let months = 0;
-
-    if (childs_years.value == 2) {
-      months = (+$("#two_years_childs_months").val());
-    } else {
-      months = (+$("#childs_months").val())
-    }
-
     const formData = {
-      childs_age: $("#childs_years").val() * 12 + months
+      childs_age: $("#childs_years").val() * 12 + (+$("#childs_months").val())
     }
-
     $.ajax({
       url: "/api/v1/diaper_calculators",
       type: "POST",
