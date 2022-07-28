4e5426cb44ecf7b75631e6362a5b48ce90f7b70f
Владислав Малко <93429979+PrincTwilig@users.noreply.github.com>
2022-07-28 18:43:13 +0200

    FirstCommit

---
+++ app/models/product_price.rb
@@ -0,0 +1,10 @@
+class ProductPrice < ApplicationRecord
+  belongs_to :product
+  enum category: {
+    LOW: 0,
+    MEDIUM: 1,
+    HIGH: 2
+  }
+  
+  validates :category, :price, presence: true
+end
