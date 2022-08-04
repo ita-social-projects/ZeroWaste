f8a9d7b7b36fb5a3e71037f99ee859a8daede2a3
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2022-08-04 23:27:02 +0300

    added rspec tests for controller

---
+++ spec/controllers/diaper_calculators_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::DiaperCalculatorsController do
+  describe "#create" do
+
+    let (:values) {}
+
+    context "підгузків" do
+
+      let (:values){[
+        { name: 'money_spent', result: 0 },
+        { name: 'money_will_be_spent', result: 0 },
+        { name: 'used_diapers_amount', result: 0 },
+        { name: 'to_be_used_diapers_amount', result: 0 }
+      ]}
+
+      it "renders" do
+        @expected = {
+        :result  =>  values,
+        :date    => 0,
+        :word_form   => 'підгузків'
+          }.to_json
+        get :create
+        response.body.should == @expected
+      end
+    end
+  end
+end
