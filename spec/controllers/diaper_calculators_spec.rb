72051ab73dde4279312cdca163938a1c196fa360
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2022-08-09 23:17:50 +0300

    corrected controller test according to comments

--- spec/controllers/diaper_calculators_spec.rb
+++ spec/controllers/diaper_calculators_spec.rb
@@ -3,27 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::DiaperCalculatorsController do
-  describe "#create" do
-
-    let (:values) {}
-
-    context "default values" do
-
-      let (:values){[
+  before do
+    get :create
+  end
+  describe '#create' do
+    let(:values) do
+      [
         { name: 'money_spent', result: 0 },
         { name: 'money_will_be_spent', result: 0 },
         { name: 'used_diapers_amount', result: 0 },
         { name: 'to_be_used_diapers_amount', result: 0 }
-      ]}
-
-      it "renders" do
-        @expected = {
-        :result  =>  values,
-        :date    => 0,
-        :word_form   => 'підгузків'
-          }.to_json
-        get :create
-        response.body.should == @expected
+      ]
+    end
+    let(:expected) do
+      { result: values,
+        date: 0,
+        word_form: 'підгузків' }.to_json
+    end
+    context 'when default values' do
+      it 'renders expected result' do
+        expect(response.body).to eq(expected)
       end
     end
   end
