3dc9ff9a68692348eaf82725e2301c3d97de6ac8
Iryna Koval <iryna.koval2003@gmail.com>
2022-08-16 09:55:31 +0300

    Code fixed with rubocop

--- app/models/feature_flag.rb
+++ app/models/feature_flag.rb
@@ -4,27 +4,26 @@ class FeatureFlag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def add_new_feature
-    FeatureFlag.create(name: self.name, enabled: false)
+    FeatureFlag.create(name: name, enabled: false)
   end
-  
+
   def feature_added?
-    FeatureFlag.exists?(:name=>self.name)
+    FeatureFlag.exists?(name: name)
   end
 
   def activate
-    feature_flag = self.feature_added? ? self : self.add_new_feature
+    feature_flag = feature_added? ? self : add_new_feature
     feature_flag.update(enabled: true)
     feature_flag
   end
 
   def deactivate
-    feature_flag = self.feature_added? ? self : self.add_new_feature
+    feature_flag = feature_added? ? self : add_new_feature
     feature_flag.update(enabled: false)
     feature_flag
   end
 
   def active?
-    self.feature_added? ? self.enabled : false
+    feature_added? ? enabled : false
   end
-
 end
