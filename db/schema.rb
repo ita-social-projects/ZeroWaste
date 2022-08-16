1c79bbd092af5cd352f97c5d9dd346adb16569cf
Iryna Koval <iryna.koval2003@gmail.com>
2022-08-16 10:27:28 +0300

    Added schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_162718) do
+ActiveRecord::Schema.define(version: 2022_08_08_110227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 2022_07_28_162718) do
     t.boolean "preferable"
     t.index ["slug"], name: "index_calculators_on_slug", unique: true
     t.index ["uuid"], name: "index_calculators_on_uuid", unique: true
+  end
+
+  create_table "feature_flags", force: :cascade do |t|
+    t.string "name"
+    t.boolean "enabled"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "fields", force: :cascade do |t|
