7c2cd8ae115a7f1712ea61d1f26717d7a28242c0
Владислав Малко <93429979+PrincTwilig@users.noreply.github.com>
2022-08-18 11:36:23 +0200

    change by comments

--- spec/models/product_price_spec.rb
+++ spec/models/product_price_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-LOCAL_PREFIX_PRODUCT_PRICE = "activerecord.errors.models.product_price.attributes"
 
 RSpec.describe ProductPrice, type: :model do
   subject { build(:product_price) }
   describe "validations" do
-    describe "price" do
+    context "price" do
       it {
         should validate_presence_of(:price)
       }
@@ -14,27 +13,25 @@ RSpec.describe ProductPrice, type: :model do
         expect(subject.price).to be_a(Float)
       }
     end
-    describe "category" do
+    context "category" do
       it {
         should validate_presence_of(:category)
       }
       it {
         should validate_inclusion_of(:category).in?([0, 1, 2])
       }
-      describe "enum_names" do
-        it {
-          subject.category = 0
-          expect(subject.category).to eq("LOW")
-        }
-        it {
-          subject.category = 1
-          expect(subject.category).to eq("MEDIUM")
-        }
-        it {
-          subject.category = 2
-          expect(subject.category).to eq("HIGH")
-        }
-      end
+      it {
+        subject.category = 0
+        expect(subject.category).to eq("LOW")
+      }
+      it {
+        subject.category = 1
+        expect(subject.category).to eq("MEDIUM")
+      }
+      it {
+        subject.category = 2
+        expect(subject.category).to eq("HIGH")
+      }
     end
   end
 end
