a7da4f781b884485e110a23e97eda860c7b47d5b
Olexiy Sivachenko <oleksii.r.sivachenko@gmail.com>
2022-08-19 11:36:37 +0300

    RspecSignUp - edited

--- spec/features/sing_up_spec.rb
+++ spec/features/sing_up_spec.rb
@@ -19,18 +19,31 @@ describe 'User Sign Up', js: true do
     end
   end
 
-  # TODO: Fix me
-  # context 'when sign up with incorrect password or email' do
-  #   it 'redirect to sign up page' do
-  #     visit '/users/sign_up'
-  #     fill_in 'Email', with: ' '
-  #     fill_in 'Password', with: ' '
-  #     fill_in 'Password Confirmation', with: ' '
-  #     fill_in 'First Name', with: ' '
-  #     fill_in 'Last Name', with: ' '
-  #     select 'Albania', from: 'user_country'
-  #     click_button 'Sing Up'
-  #     expect(page).to have_content 'Password (6 Characters Minimum)'
-  #   end
-  # end
+  context 'when sign up with incorrect password' do
+    it 'redirect to sign up page' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: ' '
+      fill_in 'Password', with: ' '
+      fill_in 'Re-password', with: ' '
+      fill_in 'First name', with: ' '
+      fill_in 'Last name', with: ' '
+      select 'Albania', from: 'user_country'
+      click_button 'Sing Up'
+      expect(page).to have_content 'Password is invalid'
+    end
+  end
+
+  context 'when sign up with incorrect email' do
+    it 'redirect to sign up page' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: ' '
+      fill_in 'Password', with: ' '
+      fill_in 'Re-password', with: ' '
+      fill_in 'First name', with: ' '
+      fill_in 'Last name', with: ' '
+      select 'Albania', from: 'user_country'
+      click_button 'Sing Up'
+      expect(page).to have_content 'Email is invalid'
+    end
+  end
 end
