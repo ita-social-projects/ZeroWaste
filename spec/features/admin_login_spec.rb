f2bde7c3d634b9bb05a16fad559260a2ef035f98
Владислав Малко <93429979+PrincTwilig@users.noreply.github.com>
2022-08-22 17:53:28 +0200

    Get started

--- spec/features/admin_login_spec.rb
+++ spec/features/admin_login_spec.rb
@@ -2,25 +2,44 @@
 
 require 'rails_helper'
 
-describe 'visit admin page', js: true do
-  let(:admin) { create(:admin) }
-  # TODO: Fix me
-  # context "when sign in with correct login and password" do
-  #   it "redirect to admin page" do
-  #     visit '/admins/sign_in'
-  #     fill_in 'Email', with: admin.email
-  #     fill_in 'Password', with: admin.password
-  #     click_button 'Log in'
-  #     expect(page).to have_content 'Dashboard'
-  #   end
-  # end
-  # context "when sign in with wrong login and password" do
-  #   it "redirect to admin login page" do
-  #     visit '/admins/sign_in'
-  #     fill_in 'Email', with: 'wrong@email.com'
-  #     fill_in 'Password', with: admin.password
-  #     click_button 'Log in'
-  #     expect(page).to have_content 'Log in'
-  #   end
-  # end
+RSpec.feature "AdminLogins", type: :feature do
+  describe 'sign in admin page', js: true do
+    subject { create(:user) }
+    context "when sign in with correct login and password" do
+      it "redirect to admin page" do
+        visit '/users/sign_in'
+        fill_in 'Email', with: subject.email
+        fill_in 'Password', with: subject.password
+        click_button 'Log in'
+        expect(page).to have_content 'Signed in successfully.'
+      end
+    end
+    context "when sign in with wrong login and password" do
+      it "redirect to admin login page" do
+        visit '/users/sign_in'
+        fill_in 'Email', with: 'wrong@email.com'
+        fill_in 'Password', with: 'wrong password'
+        click_button 'Log in'
+        expect(page).to have_content 'Log in'
+      end
+    end
+    context "when sign in with wrong password" do
+      it "redirect to admin login page" do
+        visit '/users/sign_in'
+        fill_in 'Email', with: subject.email
+        fill_in 'Password', with: 'wrong password'
+        click_button 'Log in'
+        expect(page).to have_content 'Log in'
+      end
+    end
+    context "when sign in with wrong login" do 
+      it "redirect to admin login page" do
+        visit '/users/sign_in'
+        fill_in 'Email', with: 'wrong@email.com'
+        fill_in 'Password', with: subject.password
+        click_button 'Log in'
+        expect(page).to have_content 'Log in'
+      end
+    end
+  end
 end
