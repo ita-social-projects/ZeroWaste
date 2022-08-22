040337a00bad1f58450904526d5b3f776fc663cd
KostiaFed <kostiantyn.fed@gmail.com>
2022-08-22 10:17:31 +0300

    Template spec backup

--- spec/controllers/diaper_calculators_spec.rb
+++ spec/controllers/diaper_calculators_spec.rb
@@ -27,14 +27,4 @@ RSpec.describe Api::V1::DiaperCalculatorsController do
     end
   end
 
-  describe '#product_price' do
-    context 'when default value' do
-      it 'diaper price category selected' do
-        allow(Api::V1::DiaperCalculatorsController).to receive(params[:price]) {1}
-
-        expect(dcc.product_price).to eq(ProductPrice.first)
-      end
-    end
-  end
-
 end
