f1f28e882555435b7a9f6ec07ab1934e8c9abb48
Iryna Koval <iryna.koval2003@gmail.com>
2022-08-24 21:01:21 +0300

    Delete 'let' and unnecessary files

--- spec/models/feature_flag_spec.rb
+++ spec/models/feature_flag_spec.rb
@@ -3,8 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe FeatureFlag, type: :model do
-  let(:new_flag) { build(:feature_flag) }
-  subject(:feature) { described_class.new(new_flag.attributes)}
+  subject{ FactoryBot.build(:feature_flag, enabled: false) }
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
@@ -13,44 +12,46 @@ RSpec.describe FeatureFlag, type: :model do
 
   describe '#feature_exist?' do
     it 'reaturn true if feature is added to table' do
-      expect(feature.create.feature_exist?).to be_truthy
+      expect(subject.create.feature_exist?).to be_truthy
     end
   end
 
   describe '#activate' do
     context 'when feature is not added in table' do
-      let(:flag_name){ new_flag.name }
-      it { expect(new_flag.activate.reload.enabled).to be_truthy }
+      it { expect(subject.activate.reload.enabled).to be_truthy }
     end
     context 'when feature is added in table' do
-      it { expect(feature.create.activate.enabled).to be_truthy }
+      it { expect(subject.create.activate.enabled).to be_truthy }
     end
   end
 
   describe '#deactivate' do
     context 'when feature is not added in table' do
-      it { expect(new_flag.deactivate.reload.enabled).to be_falsey }
+      it { expect(subject.deactivate.reload.enabled).to be_falsey }
     end
     context 'when feature is added in table' do
-      it { expect(feature.create.deactivate.enabled).to be_falsey }
+      it { expect(subject.create.deactivate.enabled).to be_falsey }
     end
   end
 
   describe '#active?' do
     context 'when feature is not added in table' do
       it 'return false' do
-        expect(new_flag.active?).to be_falsey
+        expect(subject.active?).to be_falsey
       end
     end
-
     context 'when feature is added in table' do
-      let(:activated_feature) { feature.create.activate }
-      it 'return true if feature has active status' do
-        expect(activated_feature.active?).to be_truthy
+      context 'when feature is activated' do
+        subject{ FactoryBot.create(:feature_flag, enabled: true) }
+        it 'return true' do
+          expect(subject.active?).to be_truthy
+        end
       end
-      let(:deactivated_feature) { feature.create.deactivate }
-      it 'return false if feature is inactive status' do
-        expect(deactivated_feature.active?).to be_falsey
+      context 'when feature is deactivated' do
+        subject{ FactoryBot.create(:feature_flag, enabled: false) }
+        it 'return false' do
+          expect(subject.active?).to be_falsey
+        end
       end
     end
   end
