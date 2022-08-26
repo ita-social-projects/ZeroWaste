f7f8498a8a201b71720824444bbeb1f13ec2eb43
Olexiy Sivachenko <oleksii.r.sivachenko@gmail.com>
2022-08-26 20:44:46 +0300

    Resolved remark

--- spec/features/admin/navigator_spec.rb
+++ spec/features/admin/navigator_spec.rb
@@ -1,29 +1,21 @@
 require 'rails_helper'
 
-  RSpec.feature 'Сontacts css', type: :feature do
-    describe 'contacts css on entry page', js: true do
-      context 'when entry page' do
+  RSpec.feature 'Link Contacts', type: :feature do
+    describe 'contacts on entry page', js: true do
+      context 'when follow the link' do
         before do
           visit root_path
         end
-        it 'css' do
+
+        it 'is not valid without a css style' do
           expect(page).to have_css('.page-header')
           expect(page).to have_css('.tabs')
         end
-      end
-    end
-  end
 
-  RSpec.feature 'Сontacts', type: :feature do
-    describe 'contacts on entry page to link', js: true do
-      context 'when redirect to link' do
-        before do
-          visit root_path
-        end
-        it 'when redirect to link' do
+        it 'is not valid without a link' do
           expect(page).to have_link('contacts')
           click_link('contacts', href: 'https://zerowastelviv.org.ua/en/contacts/')
-        end
       end
+    end
     end
   end
