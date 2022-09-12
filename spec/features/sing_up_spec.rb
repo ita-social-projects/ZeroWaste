05e70a5989c3f4b18d33d0646f1b2f259080d0bd
Olexiy Sivachenko <oleksii.r.sivachenko@gmail.com>
2022-09-12 21:22:21 +0300

    Added tests

--- spec/features/sing_up_spec.rb
+++ spec/features/sing_up_spec.rb
@@ -22,14 +22,29 @@ describe 'User Sign Up', js: true do
   context 'when sign up with incorrect password' do
     it 'redirect to sign up page' do
       visit '/users/sign_up'
-      fill_in 'Email', with: ' '
+      fill_in 'Email', with: 'user@gmail.com '
       fill_in 'Password', with: ' '
       fill_in 'Re-password', with: ' '
-      fill_in 'First name', with: ' '
-      fill_in 'Last name', with: ' '
+      fill_in 'First name', with: 'First'
+      fill_in 'Last name', with: 'Last'
       select 'Albania', from: 'user_country'
       click_button 'Sing Up'
       expect(page).to have_content 'Password is invalid'
+    end
+  end
+
+  context 'when sign up with password is too short' do
+    it 'redirect to sign up page' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'user@gmail.com'
+      fill_in 'Password', with: 'n'
+      fill_in 'Re-password', with: 'n'
+      fill_in 'First name', with: 'First'
+      fill_in 'Last name', with: 'Last'
+      select 'Albania', from: 'user_country'
+      click_button 'Sing Up'
+      expect(page).to have_content 'Password is invalid'
+      expect(page).to have_content 'Password is too short'
     end
   end
 
@@ -37,13 +52,58 @@ describe 'User Sign Up', js: true do
     it 'redirect to sign up page' do
       visit '/users/sign_up'
       fill_in 'Email', with: ' '
-      fill_in 'Password', with: ' '
-      fill_in 'Re-password', with: ' '
-      fill_in 'First name', with: ' '
-      fill_in 'Last name', with: ' '
+      fill_in 'Password', with: '111111111'
+      fill_in 'Re-password', with: '111111111'
+      fill_in 'First name', with: 'First'
+      fill_in 'Last name', with: 'Last'
       select 'Albania', from: 'user_country'
       click_button 'Sing Up'
       expect(page).to have_content 'Email is invalid'
     end
+  end
+
+  context 'when sign up with incorrect email' do
+    it 'redirect to sign up page' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'user@gmail.com'
+      fill_in 'Password', with: '111111111'
+      fill_in 'Re-password', with: '111111111'
+      fill_in 'First name', with: '123'
+      fill_in 'Last name', with: ' '
+      select 'Albania', from: 'user_country'
+      click_button 'Sing Up'
+      expect(page).to have_content 'First name is invalid'
+      expect(page).to have_content 'minimum is 2 characters'
+    end
+  end
+end
+
+
+describe 'user views sign up', :type => :feature do
+  context 'when the user has set their locale to :en' do
+    let(:locale) { :en }
+
+    it 'displays a translated welcome message to the user', :js => true do
+      visit '/users/sign_up'
+      expect(page).to have_content I18n.t('devise.registrations.new.sing_up_header')
+    end
+  end
+
+  context 'when sign up with correct' do
+    it 'redirect to sign up page' do
+    flash_message_disappear_time = 20
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'user@gmail.com'
+    fill_in 'Password', with: '111111111'
+    fill_in 'Re-password', with: '111111111'
+    fill_in 'First name', with: 'Name'
+    fill_in 'Last name', with: 'Sivachenko'
+    select 'Albania', from: 'user_country'
+    click_button 'Sing Up'
+
+    Capybara.using_wait_time flash_message_disappear_time do
+      expect(page).to have_content ('A message with a confirmation link has been sent to your email address.')
+    end
+  end
   end
 end
