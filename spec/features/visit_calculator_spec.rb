e7ca7a30b5fdd9aa141af674ec9d8ae3b029fe49
kromch <94765048+kromch@users.noreply.github.com>
2022-09-13 00:55:40 +0300

    Add files via upload

---
+++ spec/features/visit_calculator_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'visit Calculator page', js: true do
+  let(:user) { create(:user) }
+  let(:calculator) { create(:calculator) }
+
+  it 'visits calculator page' do
+    visit "/calculators/#{calculator.slug}"
+    expect(page).to have_content 'receive email messages'
+  end
+
+
+  xit "visits calculator page and open log_in page" do
+    visit "/calculators/#{calculator.slug}"
+    click_link ('Log In')
+    expect(page).to have_content 'Forgot your password'
+  end
+end
