a9c9190067fa4ef1ed335ff843bc2342e7d13786
kromch <94765048+kromch@users.noreply.github.com>
2022-09-14 00:53:23 +0300

    Add files via upload

---
+++ spec/features/visit_calculator_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'visit Calculator page', js: true do
+  let(:user) { create(:user) }
+  let(:calculator) { create(:calculator) }
+
+  it 'visits calculator page ENG localization' do
+    visit "/calculators/#{calculator.slug}"
+    expect(page).to have_content 'Yes, I want to receive email messages'
+  end
+
+  it 'visits calculator page UKR localization' do
+    visit "/calculators/#{calculator.slug}"
+    expect(page).to have_content
+                      'Так, Я бажаю отримувати повідомлення електронною поштою'
+  end
+
+  xit "visits calculator page and open log_in page" do
+    visit "/calculators/#{calculator.slug}"
+    click_link ('Log In')
+    expect(page).to have_content 'Forgot your password'
+  end
+end
