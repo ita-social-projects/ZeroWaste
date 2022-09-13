573f6435c00fb4b4861b86c5b8a0aea41b2c960c
Владислав Малко <93429979+PrincTwilig@users.noreply.github.com>
2022-09-14 01:06:20 +0200

    rubocoped

--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -2,7 +2,7 @@
 
 class ProductPrice < ApplicationRecord
   belongs_to :product
-  enum category: [ :BUDGETARY, :MEDIUM, :PREMIUM ]
+  enum category: %i(BUDGETARY MEDIUM PREMIUM)
   
   validates :category, :price, presence: true
 end
