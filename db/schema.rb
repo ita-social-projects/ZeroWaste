132f55cc4ad594ca4435a51676d832e07fedf5f4
Olexiy Sivachenko <oleksii.r.sivachenko@gmail.com>
2022-09-15 03:40:56 +0300

    Edited schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_04_192036) do
+ActiveRecord::Schema.define(version: 2022_08_08_110227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
