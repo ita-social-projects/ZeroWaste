492c82681a9230061cad8a78dc230ad227d892a4
Olexiy Sivachenko <oleksii.r.sivachenko@gmail.com>
2022-09-15 04:59:54 +0300

    Edited schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,30 @@ ActiveRecord::Schema.define(version: 2022_08_08_110227) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "app_configs", force: :cascade do |t|
     t.jsonb "diapers_calculator", default: {}
     t.datetime "created_at", precision: 6, null: false
@@ -109,10 +133,6 @@ ActiveRecord::Schema.define(version: 2022_08_08_110227) do
     t.integer "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "period"
-    t.float "budgetary"
-    t.float "medium"
-    t.float "premium"
     t.index ["product_id"], name: "index_product_prices_on_product_id"
     t.index ["uuid"], name: "index_product_prices_on_uuid", unique: true
   end
@@ -173,7 +193,6 @@ ActiveRecord::Schema.define(version: 2022_08_08_110227) do
     t.string "whodunnit"
     t.text "object"
     t.datetime "created_at"
-    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
