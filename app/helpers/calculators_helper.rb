eb423108794768235e2390b55d966357ed7dc891
popovycj <marian.popovych.ua@gmail.com>
2022-11-01 09:47:23 -0700

    remove code duplication

--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -12,7 +12,7 @@ module CalculatorsHelper
     arr << (simple_format '',
                           { type: 'checkbox', name: 'email_receiver',
                             id: 'checkbox_submit' }, wrapper_tag: 'input')
-    arr << (simple_format t('calculators.calculator.email_receiver_checkbox'),
+    arr << (simple_format t('calculators.calculator.want_recomentation_label'),
                           { for: 'checkbox_submit' }, wrapper_tag: 'label')
     content_tag(:div, class: 'flex-item') do
       arr.join(' ').html_safe
