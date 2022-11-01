704d69cea86f57f7ea853f0c0470dd80c5dd0902
popovycj <marian.popovych.ua@gmail.com>
2022-11-01 04:16:04 -0700

    add ukr localization to email_receiver_checkbox

--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -12,7 +12,7 @@ module CalculatorsHelper
     arr << (simple_format '',
                           { type: 'checkbox', name: 'email_receiver',
                             id: 'checkbox_submit' }, wrapper_tag: 'input')
-    arr << (simple_format 'Yes, I want to receive email messages',
+    arr << (simple_format t('calculators.calculator.email_receiver_checkbox'),
                           { for: 'checkbox_submit' }, wrapper_tag: 'label')
     content_tag(:div, class: 'flex-item') do
       arr.join(' ').html_safe
