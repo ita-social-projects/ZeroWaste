9bd75d9d54c3aaab0e21ef59e7c4181dfafade38
Paul Ruby <kolodiy6767@gmail.com>
2022-11-04 12:57:18 +0200

    refactor(rubocop)

--- config/initializers/any_login.rb
+++ config/initializers/any_login.rb
@@ -1,54 +1,56 @@
 # frozen_string_literal: true
 
-AnyLogin.setup do |config|
-  config.collection_method = :grouped_collection_by_role
-  # # provider (:devise, :authlogic, sorcery, clearance). \
-  # Provider can be identified automatically based on your Gemfile
-  # config.provider = nil
+if Rails.env.development?
+  AnyLogin.setup do |config|
+    config.collection_method = :grouped_collection_by_role
+    # # provider (:devise, :authlogic, sorcery, clearance). \
+    # Provider can be identified automatically based on your Gemfile
+    # config.provider = nil
 
-  # # enabled or not
-  # config.enabled = Rails.env.development?
+    # # enabled or not
+    # config.enabled = Rails.env.development?
 
-  # # Account, User, Person, etc
-  # config.klass_name = 'User'
+    # # Account, User, Person, etc
+    # config.klass_name = 'User'
 
-  #  # .all, .active, .admins, .groped_collection, etc ... \
-  # need to return an array (or hash with arrays) of users
-  # config.collection_method = :all
+    #  # .all, .active, .admins, .groped_collection, etc ... \
+    # need to return an array (or hash with arrays) of users
+    # config.collection_method = :all
 
-  # # to format user name in dropdown list
-  # config.name_method = proc { |e| [e.email, e.id] }
+    # # to format user name in dropdown list
+    # config.name_method = proc { |e| [e.email, e.id] }
 
-  # # after logging in redirect user to path
-  # config.redirect_path_after_login = :root_path
+    # # after logging in redirect user to path
+    # config.redirect_path_after_login = :root_path
 
-  # # login on select change event OR click on button, or BOTH
-  # config.login_on = :both
+    # # login on select change event OR click on button, or BOTH
+    # config.login_on = :both
 
-  # # position of any_login box top_left, top_right, bottom_left, bottom_right
-  # config.position = :bottom_left
+    # # position of any_login box top_left, top_right, bottom_left, bottom_right
+    # config.position = :bottom_left
 
-  # # label on Login button
-  # config.login_button_label = 'Login'
+    # # label on Login button
+    # config.login_button_label = 'Login'
 
-  # # prompt message in select
-  # config.select_prompt = "Select #{AnyLogin.klass_name}"
+    # # prompt message in select
+    # config.select_prompt = "Select #{AnyLogin.klass_name}"
 
-  # # show any_login box by default
-  # config.auto_show = false
+    # # show any_login box by default
+    # config.auto_show = false
 
-  # # limit, integer or :none
-  # config.limit = 10
+    # # limit, integer or :none
+    # config.limit = 10
 
-  # # Enable http basic authentication
-  # config.http_basic_authentication_enabled = false
+    # # Enable http basic authentication
+    # config.http_basic_authentication_enabled = false
 
-  # # Enable http basic authentication
-  # config.http_basic_authentication_user_name = 'any_login'
+    # # Enable http basic authentication
+    # config.http_basic_authentication_user_name = 'any_login'
 
-  # # Enable http basic authentication
-  # config.http_basic_authentication_password = 'password'
+    # # Enable http basic authentication
+    # config.http_basic_authentication_password = 'password'
 
-  # # Use controller proc condition
-  # config.verify_access_proc = proc { |controller| true }
-end if Rails.env.development?
+    # # Use controller proc condition
+    # config.verify_access_proc = proc { |controller| true }
+  end
+end
