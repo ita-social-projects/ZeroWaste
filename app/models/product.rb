03ed3b9ade369ffa0677b224572a2bc0447dc9b9
Paul Ruby <kolodiy6767@gmail.com>
2022-11-09 16:19:51 +0200

    add annotate to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id              :bigint           not null, primary key
+#  title           :string
+#  uuid            :uuid             not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  product_type_id :bigint
+#
+# Indexes
+#
+#  index_products_on_product_type_id  (product_type_id)
+#  index_products_on_uuid             (uuid) UNIQUE
+#
 class Product < ApplicationRecord
   belongs_to :product_type
 
