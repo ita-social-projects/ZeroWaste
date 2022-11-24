2b6ce24d56f568debaf6317f16a6593c0841d00b
popovycj <marian.popovych.ua@gmail.com>
2022-11-24 08:12:16 -0800

    change spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,8 +20,8 @@
 require 'simplecov'
 SimpleCov.start 'rails'
 
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+# require 'codecov'
+# SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
