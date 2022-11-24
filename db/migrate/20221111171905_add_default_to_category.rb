3055eac46e19d82258fad5438fcf0302dc9a3c2b
Paul Ruby <kolodiy6767@gmail.com>
2022-11-24 18:51:07 +0200

    refactor migrations

--- db/migrate/20221111171905_add_default_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToCategory < ActiveRecord::Migration[6.1]
-  def change
-    change_column :product_prices, :category, :string,null: false, default: 'medium'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_164401) do
+ActiveRecord::Schema.define(version: 2022_11_24_165030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
