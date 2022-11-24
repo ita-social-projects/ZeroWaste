009dcd23c70c8cb51fa55b1cf28ed382a5dbabc5
Paul Ruby <kolodiy6767@gmail.com>
2022-11-24 16:47:29 +0200

    refactor migrations

--- db/migrate/20221122164401_change_price_from_float_to_decimal.rb
+++ db/migrate/20221122164401_change_price_from_float_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceFromFloatToDecimal < ActiveRecord::Migration[6.1]
   def change
-    change_column :product_prices, :price, :decimal,precision: 8, scale: 2
+    change_column :product_prices, :price, :decimal, precision: 8, scale: 2
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_171905) do
+ActiveRecord::Schema.define(version: 2022_11_22_164401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
