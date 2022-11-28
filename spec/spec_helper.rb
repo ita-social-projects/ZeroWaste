909a42e43d485257ec7f994bcac13190c80eca51
popovycj <marian.popovych.ua@gmail.com>
2022-11-29 00:30:18 +0200

    add codecov uploader to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,8 +18,12 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'simplecov'
-
 SimpleCov.start 'rails'
+
+if ENV['CI']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
