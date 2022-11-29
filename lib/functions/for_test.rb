b312c9da1edfc541083f4439e45a5b6f24f61d9b
popovycj <marian.popovych.ua@gmail.com>
2022-11-29 01:59:07 -0800

    add additional class for test

--- lib/functions/for_test.rb
+++ lib/functions/for_test.rb
@@ -20,3 +20,24 @@ class Human
     puts "Age: #{'year'.pluralize(count: @age)}; Gender: #{@gender}"
   end
 end
+
+class Animal
+  attr_accessor :age, :gender
+
+  def initialize(age, gender)
+    @age = age
+    @gender = gender
+  end
+
+  def breath
+    puts 'Breathing'
+  end
+
+  def eat
+    puts 'Eating'
+  end
+
+  def to_s
+    puts "Age: #{'year'.pluralize(count: @age)}; Gender: #{@gender}"
+  end
+end
