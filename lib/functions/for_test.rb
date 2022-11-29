1fc79a893ff230904faacfee334c63e977d144e8
popovycj <marian.popovych.ua@gmail.com>
2022-11-29 00:38:46 -0800

    add class for test code coverage

---
+++ lib/functions/for_test.rb
@@ -0,0 +1,20 @@
+class Human
+  attr_accessor :age, :gender
+
+  def initialize(age, gender)
+    @age = age
+    @gender = gender
+  end
+
+  def breath
+    puts 'Breathing'
+  end
+
+  def eat
+    puts 'Eating'
+  end
+
+  def to_s
+    puts "Age: #{'year'.pluralize(count: @age)}; Gender: #{@gender}"
+  end
+end
