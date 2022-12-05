13a6a198075d026432b5f2f00806a40b32d3cfdd
Iryna Omeliukh <iryna.omeliukh@gmail.com>
2022-12-05 18:32:45 +0200

    change sign_up rspec

--- spec/features/sing_up_spec.rb
+++ spec/features/sing_up_spec.rb
@@ -1,81 +1,102 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-def fill_field_sign_up (email, password, re_password, first_name, last_name)
-  visit '/users/sign_up'
-  @email = email
-  @password = password
-  @re_password = re_password
-  @first_name = first_name
-  @last_name = last_name
-  fill_in 'Email', with: @email
-  fill_in 'Password', with: @password
-  fill_in 'Re-password', with: @re_password
-  fill_in 'First name', with: @first_name
-  fill_in 'Last name', with: @last_name
-  select 'Bahamas', from: 'user_country'
-  click_button 'Sing Up'
-end
 
 describe 'User Sign Up', js: true do
   context 'when sign up with correct password and email' do
     it 'shows a message about a confirmation link in the mail' do
       receive(:confirmation_instructions)
         .and_return(double(deliver: true))
-      fill_field_sign_up 'users@gmail.com', 'password1', 'password1', 'Margaret', 'Thatcher'
+      visit new_user_registration_path
+
+      fill_in 'Email', with: 'simple@email.com'
+      fill_in 'Password', with: '111111111'
+      fill_in 'Re-password', with: '111111111'
+      fill_in 'First name', with: 'User'
+      fill_in 'Last name', with: 'Users'
+      select 'Albania', from: 'user_country'
+
+      click_button 'Sign Up'
+
       expect(page).to have_content 'A message with a confirmation link has '
     end
   end
 
   context 'when sign up with incorrect password' do
-    it 'to signUp' do
-      fill_field_sign_up 'users@gmail.com', ' ', ' ', 'Margaret', 'Thatcher'
+    it 'shows a message about incorrect password' do
+      visit new_user_registration_path
+
+      fill_in 'Email', with: 'users@gmail.com'
+      fill_in 'Password', with: ' '
+      fill_in 'Re-password', with: ' '
+      fill_in 'First name', with: 'Margaret'
+      fill_in 'Last name', with: 'Thatcher'
+      select 'Albania', from: 'user_country'
+
+      click_button 'Sign Up'
+
       expect(page).to have_content 'Password is invalid'
     end
   end
 
   context 'when sign up with password is too short' do
-    it 'redirect to signUp' do
-      fill_field_sign_up 'users@gmail.com', 'n', 'n', 'Margaret', 'Thatcher'
+    it 'shows a message that password is too short' do
+      visit new_user_registration_path
+
+      fill_in 'Email', with: 'users@gmail.com'
+      fill_in 'Password', with: 'n'
+      fill_in 'Re-password', with: 'n'
+      fill_in 'First name', with: 'Margaret'
+      fill_in 'Last name', with: 'Thatcher'
+      select 'Albania', from: 'user_country'
+
+      click_button 'Sign Up'
+
       expect(page).to have_content 'Password is too short'
     end
   end
 
   context 'when sign up with incorrect email' do
-    it 'to sign up' do
-      visit '/users/sign_up'
-      fill_field_sign_up ' ', 'Password1', 'Password1', 'Margaret', 'Thatcher'
+    it 'shows a message that Email is invalid' do
+      visit new_user_registration_path
+
+      fill_in 'Email', with: ' '
+      fill_in 'Password', with: 'Password1'
+      fill_in 'Re-password', with: 'Password1'
+      fill_in 'First name', with: 'Margaret'
+      fill_in 'Last name', with: 'Thatcher'
+      select 'Albania', from: 'user_country'
+
+      click_button 'Sign Up'
+
       expect(page).to have_content 'Email is invalid'
     end
   end
 
-  context 'when sign up with incorrect email' do
-    it 'redirect to sign up page' do
-      fill_field_sign_up 'users@gmail.com', 'Password1', 'Password1', '123', ' '
+  context 'when sign up with incorrect first and last name' do
+    it 'shows a message that first and last name is invalid' do
+      visit new_user_registration_path
+
+      fill_in 'Email', with: 'users@gmail.com'
+      fill_in 'Password', with: 'Password1'
+      fill_in 'Re-password', with: 'Password1'
+      fill_in 'First name', with: '123'
+      fill_in 'Last name', with: ' '
+      select 'Albania', from: 'user_country'
+
+      click_button 'Sign Up'
+
       expect(page).to have_content 'First name is invalid'
       expect(page).to have_content 'minimum is 2 characters'
     end
   end
-end
 
+  # context 'when the user has set their locale to :uk' do
+  #   it 'displays a translated welcome message to the user' do
+  #     visit new_user_registration_path(locale: :uk)
 
-describe 'user views sign up', :type => :feature do
-  context 'when the user has set their locale to :en' do
-    let(:locale) { :en }
-
-    it 'displays a translated welcome message to the user', :js => true do
-      visit '/users/sign_up'
-      expect(page).to have_content ('Sing Up')
-    end
-  end
-
-  context 'when sign up with correct' do
-    it 'to sign up page' do
-    flash_message_disappear_time = 20
-    fill_field_sign_up 'users@gmail.com', 'Password1', 'Password1', 'Margaret', 'Thatcher'
-    Capybara.using_wait_time flash_message_disappear_time do
-      expect(page).to have_content ('A message with a confirmation link has been sent to your email address.')
-    end
-  end
-  end
+  #     expect(new_user_registration_path).to eq('/uk')
+  #     expect(page).to have_content ('Зареєструватися')
+  #   end
+  # end
 end
