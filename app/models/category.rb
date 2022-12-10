81f89967d6f2c772cbeeb6ea449f9148f56321aa
Paul Ruby <kolodiy6767@gmail.com>
2022-12-10 14:18:35 +0200

    feat(add additionals validates to models)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :category_categoryables, dependent: :restrict_with_exception
   has_many :categoryables, through: :category_categoryables
+
+  validates :name, presence: true, unique: true
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,4 +1,7 @@
 class Price < ApplicationRecord
   belongs_to :priceable, polymorphic: true
   belongs_to :category, optional: true
+
+  validates :price, presence: true
+  validates :priceable, uniqueness: { scope: :category_id }
 end
