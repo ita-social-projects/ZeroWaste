603ec57d32086931d20d05353e40f1be33195082
KERRAT <v.vlad.demid@gmail.com>
2022-12-15 11:27:46 +0200

    drop hello

--- app/javascript/controllers/hello_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  connect() {
-    console.log("Hello world from Stimulus");
-    console.log(this.element);
-  }
-}
