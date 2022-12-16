910373a3260fa85399a492a19afa40641a9a9684
KERRAT <v.vlad.demid@gmail.com>
2022-12-16 18:53:58 +0200

    diapers locale

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -6,6 +6,8 @@ export default class extends Controller {
     "diapersToBeUsed",
     "moneySpent",
     "moneyWillBeSpent",
+    "localUkToBeUsedDiapersAmount",
+    "localUkUsedDiapersAmount",
   ];
 
   showResults(data) {
@@ -14,5 +16,10 @@ export default class extends Controller {
     this.diapersUsedTarget.innerHTML = data.result.used_diapers_amount;
     this.diapersToBeUsedTarget.innerHTML =
       data.result.to_be_used_diapers_amount;
+
+    this.localUkToBeUsedDiapersAmountTarget.innerHTML =
+      data.word_form_to_be_used + " ви ще використаєте";
+    this.localUkUsedDiapersAmountTarget.innerHTML =
+      data.word_form_used + " ви вже використали";
   }
 }
