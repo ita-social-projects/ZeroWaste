7578c4ca9f3764f500a7bf144b574f93ecbe7afd
Iryna Omeliukh <iryna.omeliukh@gmail.com>
2022-12-20 17:09:06 +0200

    change namespace from admin to account

---
+++ app/controllers/account/base_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Account::BaseController < ApplicationController
+  layout 'account'
+
+  before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: exception.message }
+    end
+  end
+end
