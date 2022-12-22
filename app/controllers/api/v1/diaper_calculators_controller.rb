924b8314b0d2a96d878023ffde7ae46a19ee1a73
KERRAT <v.vlad.demid@gmail.com>
2022-12-22 23:46:02 +0200

    rubocop -A

--- app/controllers/api/v1/diaper_calculators_controller.rb
+++ app/controllers/api/v1/diaper_calculators_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::DiaperCalculatorsController < ApplicationController
   def calculate
     if !(params[:childs_years].nil? || params[:childs_months].nil?)
-      childs_age = params[:childs_years] + params[:childs_months]
+      childs_age      = params[:childs_years] + params[:childs_months]
       result          = Calculators::DiapersService.new(childs_age).calculate!
       diapers_be_used = t("calculators.calculator.diaper").pluralize(
         count: result.to_be_used_diapers_amount,
@@ -26,30 +26,29 @@ class Api::V1::DiaperCalculatorsController < ApplicationController
           date: params[:childs_age].to_i,
           word_form_to_be_used: diapers_be_used,
           word_form_used: diapers_used
-        }, status: 200
+        }, status: :ok
       )
     else
       render(
-        json:{
-          errors: check_data_present()
-        }, status: 422
+        json: {
+          errors: check_data_present
+        }, status: :unprocessable_entity
       )
     end
   end
 
   private
 
-  def check_data_present()
+  def check_data_present
     errors = []
     if params[:childs_years].nil? && params[:childs_months].nil?
-      errors << t('calculators.calculator.year_and_month_error_msg')
+      errors << t("calculators.calculator.year_and_month_error_msg")
     elsif params[:childs_years].nil?
-      errors << t('calculators.calculator.year_error_msg')
+      errors << t("calculators.calculator.year_error_msg")
     elsif params[:childs_months].nil?
-      errors << t('calculators.calculator.month_error_msg')
+      errors << t("calculators.calculator.month_error_msg")
     end
 
     errors
   end
-
 end
