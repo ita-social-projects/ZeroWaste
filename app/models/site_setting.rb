98b82a01de1aa1acddd8542c9c2c91b51d73cee6
popovycj <marian.popovych.ua@gmail.com>
2022-12-28 00:42:50 +0200

    refactor site setting model

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -1,31 +1,41 @@
 class SiteSetting < ApplicationRecord
-  default_scope { order("created_at DESC") }
+  @@__active = nil
+
+  default_scope { order(created_at: :desc) }
 
   has_one_attached :favicon, dependent: :destroy
 
-  validates :title, presence: true
-  validates :favicon, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :favicon, attached: true,
+                      content_type: [:png, :jpg, :jpeg, :ico],
+                      size: { less_than: 500.kilobytes,
+                              message: I18n.t("account.site_settings.validations.size") }
 
-  before_save :disable_all_except_current, if: -> { enabled }
-  before_destroy :restrict_enabled_destroy, if: -> { enabled }
+  before_save :ensure_one_active!
+  before_destroy :restrict_destruction_of_active
 
-  def disable_all_except_current
-    SiteSetting.where.not(id: id).find_each { |s| s.update(enabled: false) }
+  def self.active
+    @@__active = find_by(enabled: true) if @@__active.nil?
+
+    @@__active
   end
 
-  def restrict_enabled_destroy
-    raise ActiveRecord::RecordNotDestroyed, "Enabled record cannot be destroyed"
+  def self.active=(site_setting)
+    @@__active = site_setting if site_setting.is_a?(self) && site_setting.enabled
   end
 
-  def favicon_with_proper_size
-    favicon.variant(resize_to_limit: [100, 100]).processed
+  private
+
+  def ensure_one_active!
+    return unless enabled_changed?(to: true)
+
+    SiteSetting.where(enabled: true).where.not(id: id)
+               .find_each { |s| s.update(enabled: false) }
+
+    SiteSetting.active = self
   end
 
-  def self.get_enabled
-    find_by(enabled: true)
-  end
-
-  def enable
-    update(enabled: true)
+  def restrict_destruction_of_active
+    raise ActiveRecord::RecordNotDestroyed if enabled
   end
 end
