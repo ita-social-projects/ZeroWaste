27c56933e4c6f959e182ec15a32cd283e927d271
popovycj <marian.popovych.ua@gmail.com>
2023-01-12 19:00:48 +0200

    move default site settings initialization to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,20 @@
 
 module ApplicationHelper
   def current_site_setting
-    SiteSetting.instance
+    site_setting = SiteSetting.instance
+
+    if site_setting.invalid?
+      site_setting.title = "ZeroWaste"
+      site_setting.favicon.attach(
+        io: File.open("app/assets/images/logo_zerowaste.png"),
+        filename: "logo_zerowaste.png",
+        content_type: "image/png"
+      )
+
+      site_setting.save!
+    end
+
+    site_setting
   end
 
   def flash_messages
