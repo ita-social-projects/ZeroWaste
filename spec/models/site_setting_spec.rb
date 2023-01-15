13bd460a887f5a86d28cdb8f0e3f05994786c969
popovycj <marian.popovych.ua@gmail.com>
2023-01-15 14:52:04 +0200

    add tests for validation of site setting model

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -5,8 +5,60 @@ RSpec.describe SiteSetting, type: :model do
     it "should not allow using new method" do
       expect { SiteSetting.new }.to raise_error NoMethodError
     end
+
     it "should have instance method" do
       expect(SiteSetting.instance).to be_an SiteSetting
+    end
+  end
+
+  context "validates properly" do
+    before do
+      @file = Tempfile.new(["test_image", ".jpg"], content_type: "image/jpg")
+      @file.write("Test file contents")
+      @file.rewind
+
+      @blob = ActiveStorage::Blob.create_and_upload!(io: @file, filename: "test_image.jpg")
+
+      @original_site_setting = SiteSetting.instance
+      @site_setting          = SiteSetting.instance
+    end
+
+    after do
+      @file.close
+      @file.unlink
+
+      SiteSetting.instance.update(@original_site_setting.attributes)
+    end
+
+    it "has a valid factory" do
+      @site_setting.update(title: "Test Title", favicon: @blob)
+
+      expect(@site_setting).to be_valid
+    end
+
+    it "is valid with valid attributes" do
+      @site_setting.update(title: "Test Title", favicon: @blob)
+
+      expect(@site_setting.valid?).to be_truthy
+    end
+
+    it "is not valid without a title" do
+      @site_setting.update(favicon: @blob)
+
+      expect(@site_setting.valid?).to be_falsey
+    end
+
+    it "is not valid with a title longer than 20 characters" do
+      @site_setting.update(title: "a" * 21, favicon: @blob)
+
+      expect(@site_setting.valid?).to be_falsey
+    end
+
+    it "is not valid without a favicon" do
+      @site_setting.update(title: "Test Title")
+      @site_setting.favicon.purge
+
+      expect(@site_setting.valid?).to be_falsey
     end
   end
 end
