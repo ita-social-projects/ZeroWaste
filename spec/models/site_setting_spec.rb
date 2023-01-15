4b839bac910116cf0b512abeea80d86ff8472334
popovycj <marian.popovych.ua@gmail.com>
2023-01-15 21:26:31 +0200

    add new specs for site setting model

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -1,5 +1,41 @@
 require "rails_helper"
 
+def create_valid_file
+  file = Tempfile.new(["valid_file", ".png"], content_type: "image/png")
+  file.truncate(500.kilobytes - 1)
+
+  blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: "valid_file.png")
+
+  file.close
+  file.unlink
+
+  blob
+end
+
+def create_invalid_file
+  file = Tempfile.new(["invalid_file", ".pdf"], content_type: "application/pdf")
+  file.truncate(500.kilobytes - 1)
+
+  blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: "invalid_file.pdf")
+
+  file.close
+  file.unlink
+
+  blob
+end
+
+def create_big_file
+  file = Tempfile.new(["big_file", ".jpg"], content_type: "image/jpg")
+  file.truncate(500.kilobytes + 1)
+
+  blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: "big_file.jpg")
+
+  file.close
+  file.unlink
+
+  blob
+end
+
 RSpec.describe SiteSetting, type: :model do
   context "is a singleton model" do
     it "should not allow using new method" do
@@ -13,43 +49,34 @@ RSpec.describe SiteSetting, type: :model do
 
   context "validates properly" do
     before do
-      @file = Tempfile.new(["test_image", ".jpg"], content_type: "image/jpg")
-      @file.write("Test file contents")
-      @file.rewind
-
-      @blob = ActiveStorage::Blob.create_and_upload!(io: @file, filename: "test_image.jpg")
-
       @original_site_setting = SiteSetting.instance
       @site_setting          = SiteSetting.instance
     end
 
     after do
-      @file.close
-      @file.unlink
-
       SiteSetting.instance.update(@original_site_setting.attributes)
     end
 
     it "has a valid factory" do
-      @site_setting.update(title: "Test Title", favicon: @blob)
+      @site_setting.update(title: "Test Title", favicon: create_valid_file)
 
       expect(@site_setting).to be_valid
     end
 
     it "is valid with valid attributes" do
-      @site_setting.update(title: "Test Title", favicon: @blob)
+      @site_setting.update(title: "Test Title", favicon: create_valid_file)
 
       expect(@site_setting.valid?).to be_truthy
     end
 
     it "is not valid without a title" do
-      @site_setting.update(favicon: @blob)
+      @site_setting.update(favicon: create_valid_file)
 
       expect(@site_setting.valid?).to be_falsey
     end
 
     it "is not valid with a title longer than 20 characters" do
-      @site_setting.update(title: "a" * 21, favicon: @blob)
+      @site_setting.update(title: "a" * 21, favicon: create_valid_file)
 
       expect(@site_setting.valid?).to be_falsey
     end
@@ -59,6 +86,25 @@ RSpec.describe SiteSetting, type: :model do
       @site_setting.favicon.purge
 
       expect(@site_setting.valid?).to be_falsey
+    end
+
+    it "is not valid with a favicon of invalid type" do
+      @site_setting.update(title: "Test Title", favicon: create_invalid_file)
+
+      expect(@site_setting.valid?).to be_falsey
+    end
+
+    it "is valid with a favicon of valid type" do
+      @site_setting.update(title: "Test Title", favicon: create_valid_file)
+
+      expect(@site_setting.valid?).to be_truthy
+    end
+
+    it "is not valid with a favicon larger than 500 KB" do
+      @site_setting.update(title: "Test Title", favicon: create_big_file)
+
+      expect(@site_setting.valid?).to be_falsey
+      expect(@site_setting.errors.messages[:favicon]).to include(I18n.t("account.site_settings.validations.size"))
     end
   end
 end
