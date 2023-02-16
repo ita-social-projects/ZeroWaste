1f46fac65b01874701f71f86b1caba2a38942ff8
kerrat <v.vlad.demid@gmail.com>
2023-02-16 19:18:33 +0200

    Revert "refactor fields_controller"
    This reverts commit 686103e41359c4bd12986223952e17ec6a400693.

--- app/controllers/account/calculators/fields_controller.rb
+++ app/controllers/account/calculators/fields_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Account::Calculators::FieldsController < Account::BaseController
+  before_action :calculator, only: :new
+
   def new
     @field      = Field.new(field_params)
     @calculator = Calculator.new(fields: [@field])
@@ -11,6 +13,10 @@ class Account::Calculators::FieldsController < Account::BaseController
   end
 
   private
+
+  def calculator
+    @calculator = Calculator.friendly.find(params[:calculator_slug])
+  end
 
   def field_params
     params.require(:field).permit(:kind, :type)
