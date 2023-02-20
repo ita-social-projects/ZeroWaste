964937df89550d75f082a2d867505836a2f863d1
Anna Kynutsya <kunits.anya@gmail.com>
2023-02-20 21:01:34 +0200

    Used session_context for dashboard request test

--- spec/requests/dashboard_request_spec.rb
+++ spec/requests/dashboard_request_spec.rb
@@ -1,21 +1,25 @@
 require "rails_helper"
 
 RSpec.describe Account::DashboardController, type: :request do
-  describe "GET :index" do
-    context "when the user is signed in" do
-      include_context :signed_in_user
+  context "when the user is signed in" do
+    describe "GET :index" do
+      include_context :authorize_admin
 
       it "returns a success response" do
+        get account_root_path
         expect(response).to be_successful
       end
 
       it "renders the dashboard template" do
+        get account_root_path
         expect(response).to render_template(:index)
-        expect(response.body).to include("hello #{user.full_name}")
+        expect(response.body).to include("hello #{current_user.full_name}")
       end
     end
+  end
 
-    context "when the user is not signed in" do
+  context "when the user is not signed in" do
+    describe "GET :index" do
       it "redirects to the sign-in page" do
         get account_root_path
         expect(response).to redirect_to(new_user_session_path)
--- spec/support/shared/context/dashboard_context.rb
+++
@@ -1,8 +0,0 @@
-RSpec.shared_context :signed_in_user do
-  let(:user) { create(:user) }
-
-  before do
-    sign_in user
-    get account_root_path
-  end
-end
