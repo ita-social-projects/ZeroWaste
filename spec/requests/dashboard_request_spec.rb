2684d3717532ba494945e1a31e918832b5ec904c
Anna Kynutsya <kunits.anya@gmail.com>
2023-02-20 17:04:52 +0200

    Replace single-quotes with double-quotes in dashboard_request_spec.rb

--- spec/requests/dashboard_request_spec.rb
+++ spec/requests/dashboard_request_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Account::DashboardController, type: :request do
-  describe 'GET :index' do
-    context 'when the user is signed in' do
+  describe "GET :index" do
+    context "when the user is signed in" do
       let(:user) { create(:user) }
 
       before do
@@ -10,18 +10,18 @@ RSpec.describe Account::DashboardController, type: :request do
         get account_root_path
       end
 
-      it 'returns a success response' do
+      it "returns a success response" do
         expect(response).to be_successful
       end
 
-      it 'renders the dashboard template' do
+      it "renders the dashboard template" do
         expect(response).to render_template(:index)
-        expect(response.body).to include(user.full_name)
+        expect(response.body).to include("hello #{user.full_name}")
       end
     end
 
-    context 'when the user is not signed in' do
-      it 'redirects to the sign-in page' do
+    context "when the user is not signed in" do
+      it "redirects to the sign-in page" do
         get account_root_path
         expect(response).to redirect_to(new_user_session_path)
       end
