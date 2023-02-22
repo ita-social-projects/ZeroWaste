ad5ba5f19f2af0d4cf9619937ffd50f5f9babe41
Anna Kynutsya <kunits.anya@gmail.com>
2023-02-22 16:06:14 +0200

    Added configs

---
+++ config/initializers/factory_bot.rb
@@ -0,0 +1,11 @@
+if defined?(FactoryBot)
+  module FactoryBotCharacters
+    def clear_special_characters(string)
+      string.gsub(/[^0-9A-Za-zа-яА-Я ]/, '')
+    end
+  end
+
+  class FactoryBot::SyntaxRunner
+    include FactoryBotCharacters
+  end
+end
