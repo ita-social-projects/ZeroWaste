a50dd14340aa7da19a1eb1caf68bcfc3f394429b
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-02-24 20:35:26 +0200

    Added initializer_factory_bot/second try

--- config/initializers/factory_bot.rb
+++ config/initializers/factory_bot.rb
@@ -9,7 +9,7 @@ end
 if defined?(FactoryBot)
   module FactoryBotCharacters
     def clear_special_characters(string)
-      string.gsub(/[^0-9A-Za-zа-яА-Я ]/, '')
+      string.gsub(/[^0-9A-Za-zа-яА-Я ]/, "")
     end
   end
 
