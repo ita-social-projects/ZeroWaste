6eb5ecb4f69ab15112a1a4510a0afe5bbc16a021
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-02-24 20:51:17 +0200

    Added init third try

--- config/initializers/factory_bot.rb
+++ config/initializers/factory_bot.rb
@@ -1,15 +1,7 @@
-sequence :first_name do |num|
-  if Rails.env.test?
-    "#{clear_special_characters(Faker::Name.first_name.strip)} #{num}"
-  else
-    Faker::Name.first_name
-  end
-end
-
 if defined?(FactoryBot)
   module FactoryBotCharacters
     def clear_special_characters(string)
-      string.gsub(/[^0-9A-Za-zа-яА-Я ]/, '')
+      string.gsub(/[^0-9A-Za-zа-яА-Я ]/, "")
     end
   end
 
