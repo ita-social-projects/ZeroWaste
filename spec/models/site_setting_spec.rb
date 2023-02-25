106b29b4843b489ca3658f087b316fe2cdd767d9
Anna Kynutsya <kunits.anya@gmail.com>
2023-02-25 20:07:19 +0200

    fixed quotes

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe SiteSetting, type: :model do
     include_context :update_site_setting
 
     let!(:site_setting) { SiteSetting.instance }
-    let(:site_setting_params) { FactoryBot.attributes_for(:site_setting, :with_favicon ) }
+    let(:site_setting_params) { FactoryBot.attributes_for(:site_setting, :with_favicon) }
 
     it "has a valid factory" do
       expect(site_setting).to be_valid
@@ -40,17 +40,17 @@ RSpec.describe SiteSetting, type: :model do
       expect(site_setting).not_to be_valid
     end
 
-    it 'is not valid with a favicon of invalid type' do
+    it "is not valid with a favicon of invalid type" do
       site_setting.favicon.content_type = "application/pdf"
       expect(site_setting).not_to be_valid
     end
 
-    it 'is valid with a favicon of valid type' do
+    it "is valid with a favicon of valid type" do
       site_setting.favicon.content_type = "image/png"
       expect(site_setting).to be_valid
     end
 
-    it 'is not valid with a favicon larger than 500 KB' do
+    it "is not valid with a favicon larger than 500 KB" do
       site_setting.favicon.byte_size = 600.kilobytes
       expect(site_setting).not_to be_valid
       expect(site_setting.errors.messages[:favicon]).to include(I18n.t("account.site_settings.validations.size"))
