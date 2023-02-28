87db1b2189e0758ba2c69d44869544b98de37c44
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-02-28 16:23:49 +0200

    Added validations for priority,test and uniqueness

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,8 @@ class Category < ApplicationRecord
   has_many :category_categoryables, dependent: :restrict_with_exception
   has_many :categoryables, through: :category_categoryables
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :priority, numericality: { greater_than_or_equal_to: 0 }
 
   scope :ordered_by_priority, -> { order(priority: :asc) }
 end
