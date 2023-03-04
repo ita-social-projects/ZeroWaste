ff6a5a9d26a3fbc4cf65274a5ff6c553cde95597
Anna Kynutsya <kunits.anya@gmail.com>
2023-03-04 17:43:09 +0200

    removed few tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,31 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from '@rails/ujs';
-import Turbolinks from 'turbolinks';
-import * as ActiveStorage from '@rails/activestorage';
-import 'channels';
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "@rails/request.js";
 
-require('jquery');
+require("jquery");
 require("@nathanvda/cocoon");
 
 // const jQuery = require('jquery');
 // global.$ = global.jQuery = jQuery;
 // window.$ = window.jQuery = jQuery;
 
-import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/all';
-import '../stylesheets/application';
+import "@fortawesome/fontawesome-free/js/all";
+
 // import 'ajax/result_button';
-import 'ajax/checkbox';
-import 'plugins/flatpickr';
-import "@fortawesome/fontawesome-free/css/all"
+import "ajax/checkbox";
+import "plugins/flatpickr";
 
-import "../js/calculators/edit"
-import "../js/account/handle_browser_tab"
-import "../ajax/calculate_result_button"
+import "../js/calculators/edit";
+import "../controllers/stimulus_application";
 
-const images = require.context('../images', true)
+global.toastr = require("toastr");
 
 Rails.start();
 Turbolinks.start();
@@ -38,8 +36,8 @@ ActiveStorage.start();
 //   $('[data-toggle="popover"]').popover();
 // });
 
-$('document').ready(function() {
-    setTimeout(function() {
-        $('.alert').slideUp();
-    }, 10000);
+$("document").ready(function () {
+  setTimeout(function () {
+    $(".alert").slideUp();
+  }, 10000);
 });
