9cce1dceceb277f9dd3d26c0c3604d7304893cd1
Anna Kynutsya <kunits.anya@gmail.com>
2023-03-08 10:47:37 +0200

    removed flipper from schema

--- db/schema.rb
+++ db/schema.rb
@@ -106,22 +106,6 @@ ActiveRecord::Schema.define(version: 2023_03_07_170020) do
     t.index ["uuid"], name: "index_fields_on_uuid", unique: true
   end
 
-  create_table "flipper_features", force: :cascade do |t|
-    t.string "key", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["key"], name: "index_flipper_features_on_key", unique: true
-  end
-
-  create_table "flipper_gates", force: :cascade do |t|
-    t.string "feature_key", null: false
-    t.string "key", null: false
-    t.string "value"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
-  end
-
   create_table "messages", force: :cascade do |t|
     t.string "title", null: false
     t.string "message", null: false
