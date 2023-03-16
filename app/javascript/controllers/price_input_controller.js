9ba49a6b80f3fa97f2c301a9c010ff79ae36d77a
heroisaprinciple <valcat552@gmail.com>
2023-03-16 14:08:05 +0000

    OMG PARAMS ARE OKAY AHAHAHHAHAHAHAHHAHA

--- app/javascript/controllers/price_input_controller.js
+++ app/javascript/controllers/price_input_controller.js
@@ -3,7 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     let prices = document.querySelectorAll(".hidden-sum")
-    let checkboxes = document.querySelectorAll('#checkbox-cat')
+    let checkboxes = Array.from(document.querySelectorAll('input')).filter((el) => el.type == 'checkbox')
+    let submit = document.querySelector('#submit')
+
+    submit.addEventListener('click', (e) => {
+      //debugger
+      let filtered_prices = Array.from(prices).filter(function (price) {
+        return price.style.display == 'none';
+      })
+      filtered_prices.forEach(el => {el.remove()})
+    })
 
     prices.forEach((price) => price.style.display = 'none')
 
@@ -16,9 +25,9 @@ export default class extends Controller {
     }
 
     for (const el of checkboxesPrices) {
+      //debugger
       el.checkbox.addEventListener("click", (event) => {
         el.price.style.display = el.checkbox.checked ? 'block' : 'none'
-        el.price.querySelector('input').value = ''
       })
     }
   }
