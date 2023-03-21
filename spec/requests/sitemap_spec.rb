d55b71de1a15fc6db540c994aacd9b1dbb91c73f
Jinny <you@example.com>
2023-03-21 14:26:44 +0200

    changed according to cheks

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Sitemaps", type: :request do
   describe "GET /index" do
@@ -7,5 +7,4 @@ RSpec.describe "Sitemaps", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
