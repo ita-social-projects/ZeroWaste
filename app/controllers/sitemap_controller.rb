33b132515847fab654e4214ed8317f1d9476fb10
Jinny <you@example.com>
2023-03-24 08:01:50 +0200

    some changed

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,5 +1,4 @@
 class SitemapController < ApplicationController
   def index
-    # render sitemap.slim.html
   end
 end
--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -2,9 +2,29 @@ require "rails_helper"
 
 RSpec.describe "Sitemaps", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    it "is successful" do
       get sitemap_path
-      expect(response).to have_http_status(:success)
+
+      expect(sitemap_path).to eq("/en/sitemap")
+      expect(response.body).to include("Sitemap")
+      expect(response).to render_template(:index)
+      expect(response).to be_successful
+    end
+
+    context "when locale switching" do
+      it "is successful" do
+        get sitemap_path(locale: :en)
+
+        expect(root_path).to eq("/en")
+        expect(response.body).to include("Sitemap")
+
+        get sitemap_path(locale: :uk)
+
+        expect(sitemap_path).to eq("/uk/sitemap")
+        expect(response.body).to include("Мапа сайту")
+        expect(response).to render_template(:index)
+        expect(response).to be_successful
+      end
     end
   end
 end
