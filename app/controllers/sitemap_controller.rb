c94dc2cb144b47f2a42ddfacb8c6c24337a1be66
Jinny <you@example.com>
2023-03-25 18:08:40 +0200

    more changes

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,4 +1,5 @@
 class SitemapController < ApplicationController
+  # an action that shows a page with links to other pages of the site - a sitemap
   def index
   end
 end
--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -5,25 +5,24 @@ RSpec.describe "Sitemaps", type: :request do
     it "is successful" do
       get sitemap_path
 
-      expect(sitemap_path).to eq("/en/sitemap")
+      expect(response).to be_successful
       expect(response.body).to include("Sitemap")
       expect(response).to render_template(:index)
-      expect(response).to be_successful
     end
 
     context "when locale switching" do
       it "is successful" do
         get sitemap_path(locale: :en)
 
-        expect(root_path).to eq("/en")
+        expect(sitemap_path).to eq("/en/sitemap")
         expect(response.body).to include("Sitemap")
 
         get sitemap_path(locale: :uk)
 
+        expect(response).to be_successful
         expect(sitemap_path).to eq("/uk/sitemap")
         expect(response.body).to include("Мапа сайту")
         expect(response).to render_template(:index)
-        expect(response).to be_successful
       end
     end
   end
