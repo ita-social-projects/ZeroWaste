9b799200ebe3647c33fee2f0a08cc0bcc11813d9
Jinny <you@example.com>
2023-03-26 08:33:32 +0300

    one more changes

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -9,21 +9,5 @@ RSpec.describe "Sitemaps", type: :request do
       expect(response.body).to include("Sitemap")
       expect(response).to render_template(:index)
     end
-
-    context "when locale switching" do
-      it "is successful" do
-        get sitemap_path(locale: :en)
-
-        expect(sitemap_path).to eq("/en/sitemap")
-        expect(response.body).to include("Sitemap")
-
-        get sitemap_path(locale: :uk)
-
-        expect(response).to be_successful
-        expect(sitemap_path).to eq("/uk/sitemap")
-        expect(response.body).to include("Мапа сайту")
-        expect(response).to render_template(:index)
-      end
-    end
   end
 end
