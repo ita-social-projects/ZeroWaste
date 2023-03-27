452214289fd6a35a09c1492bbb7d04188a924a76
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-03-27 16:54:19 +0300

    Added changes 3.5

--- spec/requests/sitemap.spec.rb
+++ spec/requests/sitemap.spec.rb
@@ -5,7 +5,6 @@ RSpec.describe SitemapController, type: :request do
     it "renders the sitemap" do
       get sitemap_path(format: :xml)
 
-      expect(response.content_type).to eq "application/xml; charset=utf-8"
       expect(response).to render_template(:show)
       expect(response).to be_successful
     end
