73e8a2f3454a0867ef1b2dbb6a99deb7b35f7388
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-03-29 16:17:31 +0300

    Last change

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe SitemapController, type: :request do
-  describe "GET /index" do
+  describe "GET #index" do
     it "is successful" do
       get sitemap_path
 
