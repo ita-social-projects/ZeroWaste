1fc22e60fe93ff2576901427f0eb63144aa24d72
Paul Industrious <106867723+Userlastname@users.noreply.github.com>
2023-03-30 14:24:43 +0300

    Create sitemap.xml (#396)
    * Created uk/en sitemap.xml and comperad with sitemap.html

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,5 +1,9 @@
 class SitemapController < ApplicationController
   # an action that shows a page with links to other pages of the site - a sitemap
   def index
+    respond_to do |format|
+      format.html
+      format.xml
+    end
   end
 end
