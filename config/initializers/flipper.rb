0deaf26f4b44de967101b8530c228899f6d34f63
annakynutsya <kunits.anya@gmail.com>
2023-04-10 14:28:16 +0300

    edited flipper.rb

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -9,25 +9,3 @@ end
 Flipper.features.each do |feature|
   Flipper.enable(feature.name) if Rails.env.development? # || Rails.env.staging?
 end
-
-class Flipper::Feature
-  alias_method :name, :key
-
-  def description
-    @description ||= feature_record.description
-  end
-
-  def description=(value)
-    feature_record.update(description: value)
-
-    @description = value
-  end
-
-  private
-
-  def feature_record
-    @feature_record ||= Flipper::Adapters::ActiveRecord::Feature.find_or_create_by(key: key)
-  end
-end
-
-Flipper[:access_admin_menu].description = "This feature flag is responsible for visibility of 'Admin Menu' button on main page"
