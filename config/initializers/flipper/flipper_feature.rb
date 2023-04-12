bbcd73bb1aa47a235b85527dd466a428e43c8e35
annakynutsya <kunits.anya@gmail.com>
2023-04-13 01:57:00 +0300

    added translate for description of feature

--- config/initializers/flipper/flipper_feature.rb
+++ config/initializers/flipper/flipper_feature.rb
@@ -2,13 +2,23 @@ class Flipper::Feature
   alias_method :name, :key
 
   def description
-    @description ||= feature_record.description
+    send("description_#{I18n.locale}") || description_en
   end
 
-  def description=(value)
-    feature_record.update(description: value)
+  def description_en
+    feature_record.description_en
+  end
 
-    @description = value
+  def description_en=(value)
+    feature_record.update(description_en: value)
+  end
+
+  def description_uk
+    feature_record.description_uk
+  end
+
+  def description_uk=(value)
+    feature_record.update(description_uk: value)
   end
 
   private
