87688e22ca48f2d5bc857f0de810cec86773102a
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-04-15 21:19:31 +0300

    Last changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_14_085536) do
+ActiveRecord::Schema.define(version: 2023_03_21_185919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
