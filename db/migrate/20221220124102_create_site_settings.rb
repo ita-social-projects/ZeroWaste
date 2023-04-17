cb746cb72614cc6cc8da612cd30eda779ae346c4
annakynutsya <kunits.anya@gmail.com>
2023-04-18 01:56:26 +0300

    replace adding default value to another migration

--- db/migrate/20221220124102_create_site_settings.rb
+++ db/migrate/20221220124102_create_site_settings.rb
@@ -1,7 +1,7 @@
 class CreateSiteSettings < ActiveRecord::Migration[6.1]
   def change
     create_table :site_settings do |t|
-      t.string :title
+      t.string :title, default: "ZeroWaste", null: false
 
       t.timestamps
     end
