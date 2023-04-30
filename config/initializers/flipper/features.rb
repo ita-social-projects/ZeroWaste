c2b900de1d1eb9e20c23b0e6fd05efc11b376727
BarryLev <Yaroslavik123@i.ua>
2023-04-30 21:01:18 +0300

    add flipper feature for old and new calculator design

--- config/initializers/flipper/features.rb
+++ config/initializers/flipper/features.rb
@@ -2,3 +2,5 @@ require_relative "flipper_feature"
 
 Flipper[:access_admin_menu].en_description = "This feature flag is responsible for visibility of 'Admin Menu' button on main page"
 Flipper[:access_admin_menu].uk_description = "Відкриває можливість заходити на сторінку адмінки авторизованим адмінам через верхнє меню посилань"
+Flipper[:old_calculator_design].en_description = "This feature flag is responsible for new calculator design"
+Flipper[:old_calculator_design].uk_description = "Відкриває можливість використовувати новий дизайн калькулятора"
