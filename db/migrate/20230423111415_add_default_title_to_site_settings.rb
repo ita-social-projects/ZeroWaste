7471e62a6457f9199eb8f54005a1e743401c027c
annakynutsya <kunits.anya@gmail.com>
2023-05-11 19:18:16 +0300

    fixed rubocop error in migration

--- db/migrate/20230423111415_add_default_title_to_site_settings.rb
+++ db/migrate/20230423111415_add_default_title_to_site_settings.rb
@@ -1,5 +1,16 @@
 class AddDefaultTitleToSiteSettings < ActiveRecord::Migration[6.1]
   def change
-    change_column :site_settings, :title, :string, null: false, default: "ZeroWaste"
+    change_column_default :site_settings, :title, from: nil, to: "ZeroWaste"
+    change_column_null :site_settings, :title, false
+  end
+
+  def up
+    change_column_default :site_settings, :title, from: nil, to: "ZeroWaste"
+    change_column_null :site_settings, :title, false
+  end
+
+  def down
+    change_column_default :site_settings, :title, from: "ZeroWaste", to: nil
+    change_column_null :site_settings, :title, true
   end
 end
