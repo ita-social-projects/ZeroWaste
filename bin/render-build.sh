666a37422d93831fe01b3a67cd336cb31632ac36
Ivan Marynych <49816584+loqimean@users.noreply.github.com>
2023-05-25 11:00:27 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 bundle install
+bundle exec rake db:migrate
+bundle exec rake db:seed
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
