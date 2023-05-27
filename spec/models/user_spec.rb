902e8dae30d27033dc269d82c6486b7bafc35d9b
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-05-27 21:16:18 +0300

    fix multiline in user's model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,32 +41,14 @@ RSpec.describe User, type: :model do
   let!(:user) { build(:user) }
 
   describe "validations" do
-    it {
-      is_expected.to validate_presence_of(:email)
-    }
-    it {
-      is_expected.to validate_uniqueness_of(:email).case_insensitive
-    }
-    it {
-      is_expected.to validate_length_of(:email).is_at_least(6)
-    }
-    it {
-      is_expected.to validate_length_of(:email).is_at_most(100)
-    }
-    it {
-      is_expected.to allow_value("email@gmail.com").for(:email)
-    }
-    it {
-      is_expected.not_to allow_value("email.factory-com").for(:email)
-    }
-    it {
-      is_expected.to allow_value("ddc5+/8/555/dd").for(:password)
-    }
-    it {
-      is_expected.not_to allow_value("/asd").for(:password)
-    }
-    it {
-      is_expected.to validate_length_of(:password).is_at_least(8)
-    }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_length_of(:email).is_at_least(6) }
+    it { is_expected.to validate_length_of(:email).is_at_most(100) }
+    it { is_expected.to allow_value("email@gmail.com").for(:email) }
+    it { is_expected.not_to allow_value("email.factory-com").for(:email) }
+    it { is_expected.to allow_value("ddc5+/8/555/dd").for(:password) }
+    it { is_expected.not_to allow_value("/asd").for(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
   end
 end
