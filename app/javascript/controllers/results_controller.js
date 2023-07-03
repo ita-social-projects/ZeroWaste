c2ea4cf1db895155c58b61cfab4a0b0582cb0110
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-03 23:42:22 +0300

    fix new results design for new code

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -13,19 +13,12 @@ export default class extends Controller {
   showResults(data) {
     let result = data.result;
 
-    this.moneySpentTarget.innerHTML = result.money_spent;
-    this.moneyWillBeSpentTarget.innerHTML = result.money_will_be_spent;
-    this.diapersUsedTarget.innerHTML = Math.round(result.used_diapers_amount);
-    this.diapersToBeUsedTarget.innerHTML = Math.round(result.to_be_used_diapers_amount);
+    this.moneySpentTarget.innerHTML = Math.ceil(result.money_spent);
+    this.moneyWillBeSpentTarget.innerHTML = Math.ceil(result.money_will_be_spent);
+    this.diapersUsedTarget.innerHTML = Math.ceil(result.used_diapers_amount);
+    this.diapersToBeUsedTarget.innerHTML = Math.ceil(result.to_be_used_diapers_amount);
 
     this.usedDiapersAmountTarget.innerHTML = data.text_products_used;
     this.toBeUsedDiapersAmountTarget.innerHTML = data.text_products_to_be_used;
-
-    console.log(result.money_spent)
-    console.log(result.money_will_be_spent)
-    console.log(result.used_diapers_amount)
-    console.log(result.to_be_used_diapers_amount)
-    console.log(data.text_products_used)
-    console.log(data.text_products_to_be_used)
   }
 }
