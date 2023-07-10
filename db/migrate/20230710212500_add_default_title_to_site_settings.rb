dae7524e8f2d32efe0790cac6dc53f3b4c87dc89
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-10 21:29:38 +0300

    removed useless change method in migration

--- db/migrate/20230710212500_add_default_title_to_site_settings.rb
+++ db/migrate/20230710212500_add_default_title_to_site_settings.rb
@@ -1,9 +1,4 @@
 class AddDefaultTitleToSiteSettings < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :site_settings, :title, from: nil, to: "ZeroWaste"
-    change_column_null :site_settings, :title, false
-  end
-
   def up
     change_column_default :site_settings, :title, from: nil, to: "ZeroWaste"
     change_column_null :site_settings, :title, false
