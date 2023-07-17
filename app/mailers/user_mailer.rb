a65f7a60a2fab260cffb101b6045a1495f11c926
HoshkoS <sofochka9500ho@gmail.com>
2023-07-17 20:22:01 +0300

    Add user_mailer tests

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,16 +3,9 @@
 class UserMailer < ApplicationMailer
   default from: "zerowastemailer@gmail.com"
 
-  def test_email(email, message = nil)
-    @email   = email
-    @message = message
-
-    mail(to: @email, subject: I18n.t("mailer.test_mail.subject"))
-  end
-
-  def welcome_email
-    @user = params[:user]
-    @url  = "https://zerowastelviv.org.ua"
+  def welcome_email(user)
+    @user = user
+    @url  = "#{default_url_options[:host]}:#{default_url_options[:port]}"
     mail(to: @user.email, subject: I18n.t("mailer.welcome_mail.subject"))
   end
 end
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  describe "welcome_email" do
+    let!(:user) { create(:user) }
+    let!(:mail) { UserMailer.welcome_email(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq(I18n.t("mailer.welcome_mail.subject"))
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["zerowastemailer@gmail.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body).to include(I18n.t("user_mailer.welcome_email.greeting", name: user.first_name))
+      expect(mail.body).to include(I18n.t("user_mailer.welcome_email.email", email: user.email))
+      expect(mail.body).to include(I18n.t("user_mailer.welcome_email.password", password: user.password))
+      expect(mail.body).to include(I18n.t("user_mailer.welcome_email.to_login"))
+      expect(mail.body).to include("ZeroWaste")
+      expect(mail.body).to include(I18n.t("user_mailer.welcome_email.thanks"))
+    end
+  end
+end
