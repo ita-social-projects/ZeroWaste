b2529e0f50bda8c63f3b9494d32e64381117f836
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-23 15:24:44 +0300

    fix request/feature_flags_spec and add new test to it

--- spec/requests/feature_flags_spec.rb
+++ spec/requests/feature_flags_spec.rb
@@ -3,13 +3,30 @@ require "rails_helper"
 RSpec.describe Account::FeatureFlagsController, type: :request do
   include_context :authorize_admin
 
-  xdescribe "PATCH /account/feature_flags" do
-    it "updates feature flags and redirects to edit_account_site_setting_path" do
+  describe "PATCH /account/feature_flags" do
+    it "updates access_admin_menu flag and redirects to edit_account_site_setting_path" do
       feature = Flipper[:access_admin_menu]
       feature.disable
 
-      patch account_features_flags_path, params: {
-        access_admin_menu_enabled: 1
+      patch account_features_flags_path, params: { 
+        feature_flags: {
+          access_admin_menu_enabled: 1
+        }
+      }
+
+      expect(feature.enabled?).to eq(true)
+      expect(response).to redirect_to(edit_account_site_setting_path)
+      expect(flash[:notice]).to eq(I18n.t("notifications.feature_flags_updated"))
+    end
+
+    it "updates feature flags and redirects to edit_account_site_setting_path" do
+      feature = Flipper[:new_calculator_design]
+      feature.disable
+
+      patch account_features_flags_path, params: { 
+        feature_flags: {
+          new_calculator_design_enabled: 1
+        }
       }
 
       expect(feature.enabled?).to eq(true)
