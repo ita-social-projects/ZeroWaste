2efd71125732f683b07242842c994a5aedb1a529
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-24 22:25:27 +0300

    update annotations of all models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  priority   :integer          default(0), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Category < ApplicationRecord
   has_one :price, dependent: :destroy
 
