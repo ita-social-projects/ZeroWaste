2224a7f2ab32216e875a61521f25952d27fa4f24
HoshkoS <sofochka9500ho@gmail.com>
2023-07-26 20:49:58 +0300

    Add hooks to tests

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -14,7 +14,8 @@ RSpec.describe SiteSetting, type: :model do
   context "validates properly" do
     before { subject.update(site_setting_params) }
 
-    let!(:subject) { SiteSetting.current }
+    subject { described_class.current }
+
     let(:site_setting_params) { FactoryBot.attributes_for(:site_setting, :with_valid_site_setting) }
 
     it "has a valid factory" do
@@ -35,23 +36,45 @@ RSpec.describe SiteSetting, type: :model do
 
     it "is not valid without a favicon" do
       subject.favicon.purge
+
       is_expected.not_to be_valid
     end
 
-    it "is not valid with a favicon of invalid type" do
-      subject.favicon.content_type = "application/pdf"
-      is_expected.not_to be_valid
+    context "with an invalid favicon type" do
+      let(:invalid_favicon_format) { "application/pdf" }
+
+      before do
+        subject.favicon.content_type = invalid_favicon_format
+      end
+
+      it "is not valid" do
+        is_expected.not_to be_valid
+      end
     end
 
-    it "is valid with a favicon of valid type" do
-      subject.favicon.content_type = "image/png"
-      is_expected.to be_valid
+    context "with a valid favicon type" do
+      let(:valid_favicon_format) { "image/png" }
+
+      before do
+        subject.favicon.content_type = valid_favicon_format
+      end
+
+      it "is valid" do
+        is_expected.to be_valid
+      end
     end
 
-    it "is not valid with a favicon larger than 500 KB" do
-      subject.favicon.byte_size = 600.kilobytes
-      is_expected.not_to be_valid
-      expect(subject.errors.messages[:favicon]).to include(I18n.t("account.site_settings.validations.size"))
+    context "with a favicon larger than 500 KB" do
+      let(:invalid_favicon_size) { 600.kilobytes }
+
+      before do
+        subject.favicon.byte_size = invalid_favicon_size
+      end
+
+      it "is not valid" do
+        is_expected.not_to be_valid
+        expect(subject.errors.messages[:favicon]).to include(I18n.t("account.site_settings.validations.size"))
+      end
     end
   end
 end
