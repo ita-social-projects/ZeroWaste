5d287850a4204c7429b1664e83ebef13a788a137
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-28 14:36:43 +0300

    fix feature flags shared context

--- spec/support/shared/context/feature_flags.rb
+++ spec/support/shared/context/feature_flags.rb
@@ -1,27 +1,27 @@
 RSpec.shared_context :enable_admin_menu do
   before do
     FeatureFlag.find_or_create_by!(name: "access_admin_menu")
-    Flipper.public_send(:enable, "access_admin_menu")
+    Flipper.enable :access_admin_menu
   end
 end
 
 RSpec.shared_context :disable_admin_menu do
   before do
     FeatureFlag.find_or_create_by!(name: "access_admin_menu")
-    Flipper.public_send(:disable, "access_admin_menu")
+    Flipper.disable :access_admin_menu
   end
 end
 
 RSpec.shared_context :new_calculator_design do
   before do
     FeatureFlag.find_or_create_by!(name: "new_calculator_design")
-    Flipper.public_send(:enable, "new_calculator_design")
+    Flipper.enable :new_calculator_design
   end
 end
 
 RSpec.shared_context :old_calculator_design do
   before do
     FeatureFlag.find_or_create_by!(name: "new_calculator_design")
-    Flipper.public_send(:disable, "new_calculator_design")
+    Flipper.disable :new_calculator_design
   end
 end
