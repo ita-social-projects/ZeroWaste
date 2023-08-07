029e636995d8098cb94e5ab885045ee4b9149771
kos-nastiia <kostyrka.anastasiia@gmail.com>
2023-08-07 18:46:58 +0300

    fix capybara test

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,8 +1,15 @@
+Capybara.register_driver :chrome do |app|
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 120
+
+  Capybara::Selenium::Driver.new(app, {browser: :chrome, http_client: client})
+end
+
 JS_DRIVER = :selenium_chrome_headless
 
-Capybara.default_driver = :rack_test
+Capybara.default_driver = :chrome
 Capybara.javascript_driver = JS_DRIVER
-Capybara.default_max_wait_time = 2
+Capybara.default_max_wait_time = 200
 
 RSpec.configure do |config|
   config.before do |example|
