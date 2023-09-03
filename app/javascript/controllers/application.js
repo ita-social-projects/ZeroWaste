586cdf96f2bf0ca15073d1bba122f5c2912e6e15
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-09-03 23:32:50 +0300

    fix using toastr in all stimulus controllers

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,6 @@
 import { Application } from "@hotwired/stimulus"
+import toastr from "toastr";
+
 
 const application = Application.start()
 
@@ -7,3 +9,4 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+export { toastr }
