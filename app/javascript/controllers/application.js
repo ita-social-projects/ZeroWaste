f61eb9006f86062d9455195c5ada610ab0abf94d
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-09-12 20:50:42 +0300

    fix toastr

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,7 @@
 import { Application } from "@hotwired/stimulus"
 import toastr from "toastr";
 
+window.toastr = toastr
 
 const application = Application.start()
 
@@ -9,4 +10,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-export { toastr }
--- app/javascript/controllers/input_child_info_controller.js
+++ app/javascript/controllers/input_child_info_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
 import { FetchRequest } from "@rails/request.js";
-import toastr from "toastr";
 
 export default class extends Controller {
   static targets = ["month", "year", "productCategory"];
