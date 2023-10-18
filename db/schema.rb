b42d09722b943ec9f67f63d23c9bbc694843d9ca
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-10-18 14:29:36 +0300

    minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -200,8 +200,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_212500) do
   end
 
   create_table "versions", force: :cascade do |t|
-    t.string "item_type"
-    t.string "{:null=>false}"
+    t.string "item_type", null: false
     t.bigint "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
--- spec/features/account/users_spec.rb
+++ spec/features/account/users_spec.rb
@@ -4,24 +4,27 @@ require "rails_helper"
 
 describe "visit admin page", js: true do
   let(:time_login) { Time.new(2020, 0o1, 0o1).utc }
-  let!(:users) { create_list(:user, 5, last_sign_in_at: time_login) }
+  let!(:another_user) do
+    create(:user, email: "test1@gmail.com", password: "12345878",
+                  last_sign_in_at: time_login)
+  end
 
   include_context :authorize_admin
 
   it "visits admin page" do
     visit account_users_path
-    expect(page).to have_content users.first.email
+    expect(page).to have_content "test1@gmail.com"
     expect(page).to have_content time_login
   end
 
   context "when user clicks show icon" do
     it "redirects to user info page" do
       visit account_users_path
-      within(:css, "#user-info-#{users.first.id}") do
+      within(:css, "#user-info-#{another_user.id}") do
         find(".fa-eye", visible: :all).click
         sleep 3
       end
-      expect(page).to have_current_path(account_user_path(id: users.first.id))
+      expect(page).to have_current_path(account_user_path(id: another_user.id))
       expect(page).to have_content "Email"
       expect(page).to have_content "First name"
       expect(page).to have_content "Last name"
@@ -35,11 +38,11 @@ describe "visit admin page", js: true do
   context "when user clicks edit icon" do
     it "redirects to user edit info page" do
       visit account_users_path
-      within(:css, "#user-info-#{users.first.id}") do
-        click_link(href: edit_account_user_path(id: users.first.id))
+      within(:css, "#user-info-#{another_user.id}") do
+        click_link(href: edit_account_user_path(id: another_user.id))
         sleep 3
       end
-      expect(page).to have_current_path(edit_account_user_path(id: users.first.id))
+      expect(page).to have_current_path(edit_account_user_path(id: another_user.id))
       expect(page).to have_content "First name"
       expect(page).to have_content "Last name"
       expect(page).to have_content "Country"
@@ -50,14 +53,14 @@ describe "visit admin page", js: true do
 
   context "when edit user`s info correctly" do
     it "redirects to user info page" do
-      visit edit_account_user_path(id: users.first.id)
+      visit edit_account_user_path(id: another_user.id)
       find("#user_first_name").set("John")
       find("#user_last_name").set("Doe")
       select "Albania", from: "user_country"
       find("#user_password").set("111111111")
       find("#user_password_confirmation").set("111111111")
       find_button("commit").click
-      expect(page).to have_current_path(account_user_path(id: users.first.id))
+      expect(page).to have_current_path(account_user_path(id: another_user.id))
       expect(page).to have_content "John"
       expect(page).to have_content "Doe"
       expect(page).to have_content "AL"
@@ -66,7 +69,7 @@ describe "visit admin page", js: true do
 
   context "when edit user`s info wrongly" do
     it "show error messages" do
-      visit edit_account_user_path(id: users.first.id)
+      visit edit_account_user_path(id: another_user.id)
       find("#user_first_name").set("J")
       find("#user_last_name").set("D")
       select "Albania", from: "user_country"
