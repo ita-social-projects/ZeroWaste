5de79969f0b5effc5bd10cb9cbd7cb9cc1e73245
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-10-20 20:35:30 +0300

    fix updating methods in migration

--- db/migrate/20231019194527_change_user_attributes_null.rb
+++ db/migrate/20231019194527_change_user_attributes_null.rb
@@ -1,7 +1,14 @@
 class ChangeUserAttributesNull < ActiveRecord::Migration[6.1]
   def up
-    User.where(first_name: nil).update_all(first_name: "Firstname") # rubocop:disable Rails/SkipsModelValidations
-    User.where(last_name: nil).update_all(last_name: "Lastname") # rubocop:disable Rails/SkipsModelValidations
+    User.where(first_name: nil).each do |user|
+      user.update(first_name: "Firstname")
+      user.save(validate: false)
+    end
+
+    User.where(last_name: nil).each do |user|
+      user.update(last_name: "Lastname")
+      user.save(validate: false)
+    end
 
     change_column_null :users, :first_name, false
     change_column_null :users, :last_name, false
