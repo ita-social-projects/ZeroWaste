bc765d7dba254e0d07d4ccb82c97a4c865803beb
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-10-20 21:23:28 +0300

    added test for csv response

--- spec/lib/users_csv_generator_spec.rb
+++ spec/lib/users_csv_generator_spec.rb
@@ -4,13 +4,24 @@ require "rails_helper"
 
 RSpec.describe UsersCsvGenerator do
   let(:time_login) { Time.new(2020, 0o1, 0o1).utc }
+
   let(:users) do
     [create(:user, email: "test@gmail.com", first_name: "Han", last_name: "Solo", last_sign_in_at: time_login)]
+  end
+
+  let(:csv_content) do
+    result = "email,first_name,last_name,last_sign_in_at\n"
+
+    users.each do |user|
+      result += "#{users.first.email},#{users.first.first_name},#{users.first.last_name},#{time_login}\n"
+    end
+
+    result
   end
 
   it "export users" do
     expect(UsersCsvGenerator.call(users,
                                   fields: ["email", "first_name", "last_name", "last_sign_in_at"]))
-      .to eq("email,first_name,last_name,last_sign_in_at\n#{user.email},#{user.first_name},#{user.last_name},#{time_login}\n")
+      .to eq(csv_content)
   end
 end
