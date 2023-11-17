c4f5ed96b451bc30fac0a010f09990af90d2af57
dafeys <dafeys@outlook.com>
2023-11-17 23:53:32 +0200

    edit text of error messages for favicon

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -74,8 +74,7 @@ RSpec.describe SiteSetting, type: :model do
       it "is not valid" do
         is_expected.not_to be_valid
         expect(subject.errors.messages[:favicon]).to include(
-          I18n.t("errors.messages.file_size_out_of_range",
-                 file_size: "#{invalid_favicon_size / 1024} KB")
+          I18n.t("errors.messages.file_size_out_of_range")
         )
       end
     end
