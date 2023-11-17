5f6de313b04ead2a836a9457bcc0b575ff7b9722
dafeys <dafeys@outlook.com>
2023-11-17 15:47:59 +0200

    add validations for favicon

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -15,9 +15,12 @@ class SiteSetting < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 3, maximum: 30 }, if: -> { title.present? }
   validates :favicon, attached: true,
-                      content_type: [:png, :jpg, :jpeg, :ico],
-                      size: { less_than: 500.kilobytes,
-                              message: I18n.t("account.site_settings.validations.size") }
+                      content_type: [:png, :ico],
+                      size: { less_than_or_equal_to: 500.kilobytes,
+                              message: I18n.t("account.site_settings.validations.size") },
+                      dimension: { width: { min: 16, max: 180 },
+                                   height: { min: 16, max: 180 }},
+                      aspect_ratio: :square
 
   after_initialize :set_default_favicon
 
@@ -28,8 +31,8 @@ class SiteSetting < ApplicationRecord
   def set_default_favicon
     if new_record? && !favicon.attached?
       favicon.attach(
-        io: File.open("app/assets/images/logo_zerowaste.png"),
-        filename: "logo_zerowaste.png",
+        io: File.open("app/assets/images/icons/favicon-48x48.png"),
+        filename: "favicon-48x48.png",
         content_type: "image/png"
       )
     end
