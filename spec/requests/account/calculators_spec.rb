91d57e0f1d8f81c404f9d8001dc771e9025b77b9
Iryna Borniak <la.aerea.1@gmail.com>
2023-11-25 20:57:53 +0200

    add test

---
+++ spec/requests/account/calculators_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Account::CalculatorsController", type: :request do
+  include_context :authorize_admin
+  
+  let!(:calculator) { create(:calculator) }
+
+  describe "DELETE #destroy" do
+    it "destroys the calculator and redirects" do
+      expect do
+        delete account_calculator_path(calculator)        
+      end.to change(Calculator, :count).by(-1)      
+
+      expect(response).to redirect_to(account_calculators_path)
+      expect(flash[:notice]).to eq(I18n.t("notifications.calculator_deleted"))
+    end
+  end
+end
