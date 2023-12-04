0d75ddcf1884b31812d977d913d7791d6446e228
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-12-04 22:54:08 +0200

    update UpdateFeatureFlagsService logic

--- app/services/update_feature_flags_service.rb
+++ app/services/update_feature_flags_service.rb
@@ -5,12 +5,18 @@ class UpdateFeatureFlagsService
 
   def call
     Flipper.features.each do |feature|
-      feature_name = feature.name
-      is_enabled   = @feature_params["#{feature_name}_enabled"].to_s == "1"
+      feature_name       = feature.name
+      is_enabled_new     = @feature_params["#{feature_name}_enabled"].to_s == "1"
+      is_enabled_current = Flipper.enabled?(feature_name)
 
-      SandBoxService.enable(is_enabled) if feature_name == "sandbox_mode"
-
-      Flipper.public_send((is_enabled ? "enable" : "disable").to_s, feature_name)
+      if is_enabled_new != is_enabled_current
+        SandBoxService.enable(is_enabled_new) if feature_name == "sandbox_mode"
+        Flipper.public_send((is_enabled_new ? "enable" : "disable").to_s, feature_name)
+      end
     end
+  end
+
+  def feature_enabled?(feature_name)
+    Flipper.enabled?(feature_name)
   end
 end
