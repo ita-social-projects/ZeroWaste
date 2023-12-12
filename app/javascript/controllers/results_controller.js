4faed959465d6371f41a64906366a5bf409a8181
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-12-12 18:46:01 +0200

    Update the pluralization of diapers in the results view.

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -6,14 +6,19 @@ export default class extends Controller {
     "diapersToBeUsed",
     "moneySpent",
     "moneyWillBeSpent",
+    "willBuyDiapersPluralize",
+    "boughtDiapersPluralize",
   ];
 
   showResults(data) {
     let result = data.result;
 
-    this.moneySpentTarget.innerHTML = Math.ceil(result.money_spent);
+    this.moneySpentTarget.innerHTML = Math.ceil(result.money_spent);;
     this.moneyWillBeSpentTarget.innerHTML = Math.ceil(result.money_will_be_spent);
     this.diapersUsedTarget.innerHTML = Math.ceil(result.used_diapers_amount);
     this.diapersToBeUsedTarget.innerHTML = Math.ceil(result.to_be_used_diapers_amount);
+
+    this.willBuyDiapersPluralizeTarget.innerHTML = result.to_be_diapers_amount_pluralize;
+    this.boughtDiapersPluralizeTarget.innerHTML = result.used_diapers_amount_pluralize;
   }
 }
