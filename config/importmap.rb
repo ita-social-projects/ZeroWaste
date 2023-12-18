29d500f5586b8a0e2a551573bd46846df9382a86
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-12-18 19:53:16 +0200

    Remove toastr gem and related code

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/channels", under: "channels"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js", preload: true
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.4.0/js/all.js", preload: true
-pin "toastr", to: "https://ga.jspm.io/npm:toastr@2.1.4/toastr.js", preload: true
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: true
 pin "@rails/actioncable", to: "actioncable.esm.js", preload: true
+pin "toastify-js", to: "https://ga.jspm.io/npm:toastify-js@1.12.0/src/toastify.js", preload: true
