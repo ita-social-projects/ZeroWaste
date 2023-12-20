2e7686eb070d95dd14d2267bdd681d8d017c4fe7
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-12-20 21:23:39 +0200

    add tests

--- spec/services/database_spec.rb
+++ spec/services/database_spec.rb
@@ -3,16 +3,17 @@ require "rails_helper"
 RSpec.describe DatabaseService do
   let(:command_enable) { "rake 'db:dump[zero_waste_sandbox.dump]'" }
   let(:command_disable) { "rake 'db:restore[zero_waste_sandbox.dump]'" }
-  let(:backup_file) { described_class.backup_full_path(described_class::BACKUP_SANDBOX_NAME) }
+  let(:backup_file) { DatabaseService.backup_full_path(DatabaseService::BACKUP_SANDBOX_NAME) }
+  let(:new_file_path) { File.join(DatabaseService::BACKUP_ARCHIVE_DIR, "zero_waste_sandbox.dump") }
+  let(:new_file_path_regex) { Regexp.new("#{Regexp.escape(DatabaseService::BACKUP_ARCHIVE_DIR.to_s)}/\\d{14}_zero_waste_sandbox\\.dump") }
   let(:files) { ["older.dump", "newest.dump"] }
-
   let(:file_name) { "test.dump" }
-  let(:new_file_path) { File.join(DatabaseService::BACKUP_ARCHIVE_DIR, "timestamp_zero_waste_sandbox.dump") }
 
   describe ".sandbox_enable" do
     context "when dump_flag is true" do
       it "runs dump command and returns true" do
         allow(Kernel).to receive(:system).with(command_enable).and_return(true)
+
         expect(described_class.sandbox_enable(true)).to eq(true)
       end
     end
@@ -21,9 +22,10 @@ RSpec.describe DatabaseService do
       it "runs restore command, copies file to archive and returns false" do
         allow(Kernel).to receive(:system).with(command_disable).and_return(true)
         allow(FileUtils).to receive(:cp).with(backup_file, anything)
+
         expect(described_class.sandbox_enable(false)).to eq(false)
       end
-    end 
+    end
   end
 
   describe ".sandbox_enabled?" do
@@ -36,6 +38,7 @@ RSpec.describe DatabaseService do
   describe ".init_backup_directory" do
     it "creates the backup directory if it does not exist" do
       expect(FileUtils).to receive(:mkdir_p).with(DatabaseService::BACKUP_DIR)
+
       DatabaseService.init_backup_directory
     end
 
@@ -54,6 +57,7 @@ RSpec.describe DatabaseService do
     context "when file name is not provided" do
       it "returns the full backup path with a generated file name" do
         allow(DatabaseService).to receive(:generate_backup_filename).and_return("generated_filename.dump")
+
         expect(DatabaseService.backup_full_path).to include("generated_filename.dump")
       end
     end
@@ -67,23 +71,21 @@ RSpec.describe DatabaseService do
     end
 
     it "copies the backup file to the archive directory" do
-      expect(FileUtils).to receive(:cp).with(backup_file, new_file_path)
-      expect(Time.current).to receive(:strftime).with("%Y%m%d%H%M%S").and_return("timestamp")
+      expect(FileUtils).to receive(:cp).with(backup_file, match(new_file_path_regex))
 
- puts backup_file
- puts new_file_path
-      
       DatabaseService.copy_to_archive(backup_file)
     end
 
     it "removes the original backup file" do
       expect(FileUtils).to receive(:rm).with(backup_file)
+
       DatabaseService.copy_to_archive(backup_file)
     end
 
     it "calls prune_old_backups if archive directory exceeds max backups" do
       allow(Dir).to receive(:entries).and_return(Array.new(11, "file.dump"))
       expect(DatabaseService).to receive(:prune_old_backups)
+
       DatabaseService.copy_to_archive(backup_file)
     end
   end
@@ -105,8 +107,8 @@ RSpec.describe DatabaseService do
     end
 
     it "removes the oldest backup file in the archive directory" do
-      allow(File).to receive(:mtime).with(anything).and_return(Time.now)
-      allow(File).to receive(:mtime).with(File.join(DatabaseService::BACKUP_ARCHIVE_DIR, "older.dump")).and_return(Time.now - 60)
+      allow(File).to receive(:mtime).with(anything).and_return(Time.current)
+      allow(File).to receive(:mtime).with(File.join(DatabaseService::BACKUP_ARCHIVE_DIR, "older.dump")).and_return(Time.current - 60)
       expect(FileUtils).to receive(:rm).with(File.join(DatabaseService::BACKUP_ARCHIVE_DIR, "older.dump"))
 
       DatabaseService.send(:prune_old_backups)
