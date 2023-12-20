126532e48b1a41535c514e95751a70534c347745
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-12-20 19:56:17 +0200

    update controller target

--- app/javascript/controllers/burger_controller.js
+++ app/javascript/controllers/burger_controller.js
@@ -1,10 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["iconLines", "iconCross"];
+  static targets = ["navbarNav", "iconLines", "iconCross"];
 
   toggle() {
-    document.getElementById('navbarNav').classList.toggle('hidden');
+    this.navbarNavTarget.classList.toggle('hidden');
     this.iconLinesTarget.classList.toggle('hidden');
     this.iconCrossTarget.classList.toggle('hidden');
   }
