b8edcb4f712352f6a1cba1e6bd15117344c3a172
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-12-26 11:32:46 +0200

    Rename `toast-helper.js` to `toast_helper.js`

--- app/javascript/turbo_streams/toast.js
+++ app/javascript/turbo_streams/toast.js
@@ -1,4 +1,4 @@
-import { showToast } from "helpers/toast-helper";
+import { showToast } from "helpers/toast_helper";
 
 window.Turbo.StreamActions.toast = function() {
   const message = this.getAttribute("message")
