da8cbf56ebe9c501949a266151f8b0ba4c4310ef
obniavko <vobniavko@gmail.com>
2024-01-08 19:31:50 +0200

    test: update tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,6 +10,8 @@
 #
 require "rails_helper"
 
+LOCAL_PREFIX_CATEGORY = "activerecord.errors.models.category.attributes"
+
 RSpec.describe Category, type: :model do
   subject { build(:category) }
 
@@ -21,5 +23,24 @@ RSpec.describe Category, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(30) }
     it { is_expected.to validate_numericality_of(:priority).is_greater_than_or_equal_to(0) }
+  end
+
+  context "validates the name format" do
+    let(:category) { build(:category) }
+
+    it "with valid name" do
+      category.name = "Hedgehog і єнот з'їли 2 аґруси"
+
+      expect(category).to be_valid
+    end
+
+    it "with invalid name" do
+      ["#", "!", "@", "$", "%", "^", "&", "*", "(", ")", "?", "\"", "_"].each do |sym|
+      category.name = "Invalid Name #{sym}"
+
+        expect(category).to_not be_valid
+        expect(category.errors.messages[:name]).to include(I18n.t("#{LOCAL_PREFIX_CATEGORY}.name.invalid"))
+      end
+    end
   end
 end
