a16f7b3312317dc9bb5758f33c821a368a99d5ed
obniavko <vobniavko@gmail.com>
2024-01-16 12:35:30 +0200

    test: add tests for messages

---
+++ spec/requests/account/messages_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe Account::ProductsController, type: :request do
+  let!(:message) { create(:message, title: "Title") }
+
+  include_context :authorize_admin
+
+  describe "GET :index" do
+    it "is successful" do
+      get account_messages_path
+
+      expect(response).to be_successful
+      expect(response).to render_template(:index)
+      expect(response.body).to include(message.title)
+    end
+  end
+
+  describe "GET :show" do
+    it "is successful" do
+      get account_message_path(message)
+
+      expect(response).to be_successful
+      expect(response).to render_template(:show)
+      expect(response.body).to include(message.title)
+    end
+  end
+end
