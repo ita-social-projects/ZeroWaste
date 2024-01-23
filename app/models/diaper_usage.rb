c1da32b4a14b9d8d9bc2f1e77a127ac6184c890b
dafeys <dafeys@outlook.com>
2024-01-23 02:24:02 +0200

    Delete DiaperUsage and LifePeriod models and migrations

--- app/models/diaper_usage.rb
+++
@@ -1,6 +0,0 @@
-class DiaperUsage < ApplicationRecord
-  belongs_to :life_period
-  has_many :prices, as: :priceable, dependent: :destroy
-
-  validates :quantity_per_day, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-end
