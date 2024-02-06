3a98ec51c1d1ba4258d85d37ea093df1e1bf6bae
dafeys <dafeys@outlook.com>
2024-02-06 03:07:57 +0200

    Refactor diapers periods controller

--- app/controllers/account/diapers_periods_controller.rb
+++ app/controllers/account/diapers_periods_controller.rb
@@ -1,80 +1,84 @@
 class Account::DiapersPeriodsController < Account::BaseController
   # repond_with format: :turbo_stream, only: []
 
+  def index
+    set_category
+    @diapers_periods = @category.diapers_periods
+  end
+
+  def new
+    set_category
+    @diapers_period = DiapersPeriod.new(category_id: @category.id)
+  end
+
+  def edit
+    set_diapers_period
+  end
+
+  def create
+    set_category
+    @diapers_periods = @category.diapers_periods
+    @diapers_period  = @category.diapers_periods.build(diapers_period_params)
+
+    if @diapers_period.save
+      respond_to :turbo_stream, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    set_category
+    @diapers_periods = @category.diapers_periods
+    set_diapers_period
+
+    if @diapers_period.update(diapers_period_params)
+      respond_to :turbo_stream, status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    set_diapers_period
+
+    if @diapers_period.destroy
+      respond_to :turbo_stream
+    else
+      redirect_to account_site_setting_path, alert: "Can't delete diapers period."
+    end
+  end
+
   def available_categories
     @categories = Category.available_categories
   end
 
   def categories
-    @categories_with_periods   = Category.categories_with_periods
+    @categories_with_periods = Category.categories_with_periods
   end
 
-  def new
-    # @site_setting               = SiteSetting.current
-    @diapers_period             = DiapersPeriod.new(category_id: params[:category_id])
-    # render template: "account/site_settings/new_diapers_period"
+  def destroy_category
+    set_category
 
-    respond_to :turbo_stream
-  end
-
-  def edit
-    @diapers_period = DiapersPeriod.find(params[:id])
-
-    # render template: :edit_diapers_period
-  end
-
-  def create
-    @diapers_period = DiapersPeriod.new(diapers_period_params)
-
-    respond_to do |format|
-      if @diapers_period.save
-        format.turbo_stream { redirect_back(fallback_location: account_site_setting_path, notice: "New Diapers period was successfully created.", status: :see_other) }
-        format.html { redirect_back(fallback_location: account_site_setting_path, notice: "New Diapers period was successfully created.") }
-      else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@diapers_period, partial: "account/site_settings/form_diapers_period", locals: { diapers_period: @diapers_period }) }
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @category.diapers_periods.destroy_all
+      respond_to :turbo_stream
+    else
+      redirect_to account_site_setting_path, alert: "Can't delete diapers periods from category."
     end
   end
-
-  def update
-    @diapers_period = DiapersPeriod.find(params[:id])
-
-    respond_to do |format|
-      if @diapers_period.update!(diapers_period_params)
-        format.turbo_stream { redirect_back(fallback_location: account_site_setting_path, notice: "Diapers period was successfully updated.") }
-        format.html { redirect_back(fallback_location: account_site_setting_path, notice: "Diapers period was successfully updated.") }
-      else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@diapers_period, partial: "account/site_settings/form_diapers_period", locals: { diapers_period: @diapers_period }) }
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # Action to DESTROY ALL periods for category
-  def destroy
-    @category = Category.find(params[:category_id])
-
-    # notify if something went wrong
-    @category.diapers_periods.destroy_all
-
-    respond_to :turbo_stream
-  end
-
-  # Action to DESTROY ONE period for each category
-  # def destroy
-  #   @diapers_period = DiapersPeriod.find(params[:id])
-  #   @diapers_period.destroy
-
-  #   respond_to do |format|
-  #     format.turbo_stream { redirect_back(fallback_location: account_site_setting_path, notice: "Period was successfully destroyed.", status: :see_other) }
-  #     format.html { redirect_back(fallback_location: account_site_setting_path, notice: "Period was successfully destroyed.", status: :see_other) }
-  #   end
-  # end
 
   private
 
   def diapers_period_params
     params.require(:diapers_period).permit(:price, :period_start, :period_end, :usage_amount, :category_id)
+  end
+
+  def set_diapers_period
+    @diapers_period = DiapersPeriod.find(params[:id])
+  end
+
+  def set_category
+    category_id = params[:diapers_period].present? ? params[:diapers_period][:category_id] : params[:category_id]
+    @category   = Category.find(category_id)
   end
 end
