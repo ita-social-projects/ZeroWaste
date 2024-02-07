b58d4c8838f4f125889ab4e07f6c3dba4e6d55a7
dafeys <dafeys@outlook.com>
2024-02-07 21:38:07 +0200

    limit the maximum value for period_end

--- app/models/diapers_period.rb
+++ app/models/diapers_period.rb
@@ -1,8 +1,19 @@
 class DiapersPeriod < ApplicationRecord
   belongs_to :category
 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :period_start, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-  validates :period_end, presence: true, numericality: { only_integer: true, greater_than: :period_start }
-  validates :usage_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+
+  validates :period_start,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  validates :period_end,
+            presence: true,
+            numericality: { only_integer: true, greater_than: :period_start, less_than_or_equal_to: 30 }
+
+  validates :usage_amount,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
 end
