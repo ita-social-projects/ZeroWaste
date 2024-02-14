cbc6e36923a997a49b9faafeb04927711343c0a2
obniavko <vobniavko@gmail.com>
2024-02-14 12:21:54 +0200

    chore: update migration

--- db/migrate/20240207150024_add_index_to_categories_name.rb
+++ db/migrate/20240207150024_add_index_to_categories_name.rb
@@ -1,5 +1,5 @@
 class AddIndexToCategoriesName < ActiveRecord::Migration[7.1]
   def change
-    add_index :categories, :name, unique: true, name: "index_categories_on_name"
+    add_index :categories, :name, unique: true
   end
 end
