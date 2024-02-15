e43560fb8e7feabe7ee5cbf99f644be6f219cb7e
dafeys <dafeys@outlook.com>
2024-02-16 00:06:04 +0200

    fix: reduce the repetition of to_s

--- app/decorators/calculator_decorator.rb
+++ app/decorators/calculator_decorator.rb
@@ -18,15 +18,15 @@ class CalculatorDecorator
 
   def results
     {
-      money_spent: result.used_diapers_price.round(1).to_s,
-      money_will_be_spent: result.to_be_used_diapers_price.round(1).to_s,
-      used_diapers_amount: result.used_diapers_amount.to_s,
-      to_be_used_diapers_amount: result.to_be_used_diapers_amount.to_s,
+      money_spent: result.used_diapers_price.round(1),
+      money_will_be_spent: result.to_be_used_diapers_price.round(1),
+      used_diapers_amount: result.used_diapers_amount,
+      to_be_used_diapers_amount: result.to_be_used_diapers_amount,
       used_diapers_amount_pluralize: I18n.t("calculators.#{calculator_type}_calculator.bought_diapers",
                                             count: result.used_diapers_amount.ceil),
       to_be_diapers_amount_pluralize: I18n.t("calculators.#{calculator_type}_calculator.will_buy_diapers",
                                              count: result.to_be_used_diapers_amount.ceil)
-    }
+    }.transform_values(&:to_s)
   end
 
   def age
