e2274d06db310914e09d998647cc33533c753086
obniavko <vobniavko@gmail.com>
2024-02-20 12:28:32 +0200

    chore: update migration

--- db/migrate/20240207150024_add_index_to_categories_name.rb
+++ db/migrate/20240207150024_add_index_to_categories_name.rb
@@ -1,5 +1,5 @@
 class AddIndexToCategoriesName < ActiveRecord::Migration[7.1]
   def change
-    add_index :categories, :name, unique: true
+    add_index :categories, "LOWER(name)", name: "index_categories_on_name", unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_150024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "priority", default: 0, null: false
-    t.index ["name"], name: "index_categories_on_name", unique: true
+    t.index "lower((name)::text)", name: "index_categories_on_name", unique: true
   end
 
   create_table "category_categoryables", force: :cascade do |t|
