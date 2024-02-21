3a655715b7fb41e3e1bfeeb9a60616e78ac74545
obniavko <vobniavko@gmail.com>
2024-02-21 22:36:24 +0200

    feat: add ransackable associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -21,4 +21,8 @@ class Message < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "id", "message", "title", "updated_at"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
