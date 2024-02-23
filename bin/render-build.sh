c71cf1db787d6a8e44fed61aa2698a9b517a9842
Ivan Marynych <49816584+loqimean@users.noreply.github.com>
2024-02-23 11:35:41 +0200

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 bundle install
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
-bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
 bundle exec rake assets:precompile
