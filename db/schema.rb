3c80dc1c560c16e5ee88528476499b6edebc658e
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2024-03-03 14:49:24 +0200

    update after reveiwer comment

--- db/schema.rb
+++ db/schema.rb
@@ -118,7 +118,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_083954) do
   create_table "flipper_gates", force: :cascade do |t|
     t.string "feature_key", null: false
     t.string "key", null: false
-    t.text "value"
+    t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
