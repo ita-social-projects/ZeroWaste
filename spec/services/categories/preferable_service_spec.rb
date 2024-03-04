c732265d3cff96b06116822412204ca0a8a64110
kisiohlova <kisioglovan@gmail.com>
2024-03-04 13:44:54 +0200

    fix: tests after merge

--- spec/services/categories/preferable_service_spec.rb
+++ spec/services/categories/preferable_service_spec.rb
@@ -3,19 +3,17 @@ require "rails_helper"
 RSpec.describe Categories::PreferableService do
   describe "#call" do
     let!(:become_preferable_category) { create(:category, :medium) }
-    let!(:categories) { create_list(:category, 3) }
-    let!(:current_preferable_category) { create(:category, :medium) }
-    let(:service) { described_class.new(become_preferable_category) }
+    let!(:category) { create(:category, name: "category") }
+    let!(:current_preferable_category) { create(:category, name: "current preferable category", preferable: "1") }
+    let!(:service) { described_class.new(become_preferable_category) }
 
     context "when the category is preferable" do
       it "sets the category to preferable" do
         service.call
 
-        categories.each do |category|
-          category.reload
-          if category != become_preferable_category
-            expect(category.preferable?).to be(false)
-          end
+        category.reload
+        if category != become_preferable_category
+          expect(category.preferable?).to be(false)
         end
       end
     end
