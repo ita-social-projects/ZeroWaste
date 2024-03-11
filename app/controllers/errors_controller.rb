88eacec5377f1628c4ac044efb87a13e03434a8a
obniavko <vobniavko@gmail.com>
2024-03-11 17:25:57 +0200

    feat: add errors controller

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,9 @@
+class ErrorsController < ApplicationController
+  def not_found
+    if request.path.include?("/en/account/") || request.path.include?("/uk/account/")
+      render 'errors/admin_404', status: :not_found, layout: 'account'
+    else
+      render 'errors/public_404', status: :not_found, layout: 'application'
+    end
+  end
+end
