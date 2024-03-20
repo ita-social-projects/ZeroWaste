c7f88c960b9af07c1059cd556ae044ca359531a9
Natali Kotelnytska <nkotelnicka@gmail.com>
2024-03-20 09:43:16 +0200

    Refactor/products controller

--- app/controllers/account/products_controller.rb
+++ app/controllers/account/products_controller.rb
@@ -10,16 +10,19 @@ class Account::ProductsController < Account::BaseController
 
   def new
     @product = Product.new
+    @categories = category_collection
   end
 
   def edit
     @product = resource
+    @categories = category_collection
 
     @product.build_unsigned_categories
   end
 
   def create
     @product = Product.new(product_params)
+    @categories = category_collection
 
     if @product.save
       redirect_to account_products_path, notice: t(".created")
@@ -30,6 +33,7 @@ class Account::ProductsController < Account::BaseController
 
   def update
     @product = resource
+    @categories = category_collection
 
     if @product.update(product_params)
       redirect_to account_products_path, notice: t(".updated")
@@ -57,6 +61,10 @@ class Account::ProductsController < Account::BaseController
 
   def collection
     Product.ordered_by_title
+  end
+
+  def category_collection
+    Category.ordered_by_name
   end
 
   def product_params
