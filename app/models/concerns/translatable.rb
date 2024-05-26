5cf8fb44431daf5979342e3c03bcebaa5e64cf1d
dafeys <dafeys@outlook.com>
2024-05-26 23:33:16 +0300

    refactor: add respond_to? into traslalable

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -14,8 +14,13 @@ module Translatable
   private
 
   def translation_for(attribute)
-    public_send("#{I18n.locale}_#{attribute}") ||
-      public_send("#{I18n.default_locale}_#{attribute}") ||
-      public_send(attribute)
+    locale_attr         = "#{I18n.locale}_#{attribute}"
+    default_locale_attr = "#{I18n.default_locale}_#{attribute}"
+
+    if respond_to?(locale_attr)
+      public_send(locale_attr)
+    elsif respond_to?(default_locale_attr)
+      public_send(default_locale_attr)
+    end
   end
 end
