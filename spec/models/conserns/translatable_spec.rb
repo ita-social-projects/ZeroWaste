ccf1512f98b5f553781ba85bd2783c2b4fd153c2
dafeys <dafeys@outlook.com>
2024-05-26 23:34:51 +0300

    test: dummy model for translatable

--- spec/models/conserns/translatable_spec.rb
+++ spec/models/conserns/translatable_spec.rb
@@ -2,36 +2,30 @@ require "rails_helper"
 
 RSpec.describe Translatable do
   describe "translations" do
-    context "when en_name and uk_name are present" do
-      let(:category) { create(:category, en_name: "Books", uk_name: "Книги") }
+    context "when en_name and uk_name are exist in model" do
+      let(:dummy_translatable) { DummyTranslatable.new }
 
       it "returns the en_name when locale is set to :en" do
         allow(I18n).to receive(:locale).and_return(:en)
 
-        expect(category.name).to eq("Books")
+        expect(dummy_translatable.name).to eq("Diapers")
       end
 
       it "returns the uk_name when locale is set to :uk" do
         allow(I18n).to receive(:locale).and_return(:uk)
 
-        expect(category.name).to eq("Книги")
+        expect(dummy_translatable.name).to eq("Підгузки")
       end
     end
 
-    context "when en_name and uk_name are not present, but a default name exists" do
-      let(:category) { create(:category) }
+    context "when en_name is not exist and a default locale is uk" do
+      let(:dummy_translatable) { DummyTranslatable.new(en_name: nil) }
 
-      before do
-        allow(category).to receive(:uk_name).and_return(nil)
-        allow(category).to receive(:en_name).and_return(nil)
-        allow(category).to receive(:name).and_return("Default Name")
-      end
-
-      it "returns the name when locale is set to :en" do
+      it "returns the uk_name when locale is set to :en" do
         allow(I18n).to receive(:locale).and_return(:en)
+        allow(I18n).to receive(:default_locale).and_return(:uk)
 
-        expect(category).not_to receive(:en_name)
-        expect(category.name).to eq("Default Name")
+        expect(dummy_translatable.name).to eq("Підгузки")
       end
     end
   end
