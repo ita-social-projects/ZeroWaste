5dc88bfd334d9f55d877e3d5df6cd1bf1411b42c
aerea <la.aerea.1@gmail.com>
2024-08-17 02:34:52 +0300

    refactor service test

--- spec/services/calculators/calculate_spec.rb
+++ spec/services/calculators/calculate_spec.rb
@@ -3,25 +3,30 @@
 require "rails_helper"
 
 RSpec.describe Calculators::CalculateService do
-  DAYS = { # rubocop:disable Lint/ConstantDefinitionInBlock
-    "day" => 1,
-    "week" => 7,
-    "month" => 30.5,
-    "year" => 365
-  }.freeze
-
+  let(:days) do
+    {
+      day: 1,
+      week: 7,
+      month: 30.5,
+      year: 365
+    }
+  end
   let(:product) { create(:product, :diaper, default_usage_per_day: 3) }
   let(:category) { create(:category, :budgetary) }
   let(:price) { create(:price, :budgetary_price, priceable: product, category: category) }
   let(:valid_params) { { period: "week", price_id: price.id } }
 
-  let(:money_spent) { price.sum.to_i * product.default_usage_per_day * DAYS[valid_params[:period]] }
-  let(:items_used) { product.default_usage_per_day * DAYS[valid_params[:period]] }
+  describe "#calculate" do
+    context "when the period is 'week'" do
+      let(:money_spent) { price.sum.to_i * product.default_usage_per_day * days[:week] }
+      let(:items_used) { product.default_usage_per_day * days[:week] }
 
-  it "calculates correctly" do
-    result = Calculators::CalculateService.new(product, valid_params).calculate
+      subject(:result) { described_class.new(product, valid_params).calculate }
 
-    expect(result[:moneySpent]).to eq(money_spent)
-    expect(result[:itemsUsed]).to eq(items_used)
+      it "calculates correctly" do
+        expect(result[:moneySpent]).to eq(money_spent)
+        expect(result[:itemsUsed]).to eq(items_used)
+      end
+    end
   end
 end
