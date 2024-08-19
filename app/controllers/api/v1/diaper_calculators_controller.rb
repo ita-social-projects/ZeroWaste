64af5870807cdbc88eb8ebd9eee822607d44d2e8
aerea <la.aerea.1@gmail.com>
2024-08-19 14:36:09 +0300

    remove diaper calculator validator, add DiaperCalculatorForm

--- app/controllers/api/v1/diaper_calculators_controller.rb
+++ app/controllers/api/v1/diaper_calculators_controller.rb
@@ -2,16 +2,16 @@
 
 class Api::V1::DiaperCalculatorsController < ApplicationController
   def calculate
-    @validation = DiaperCalculatorValidator.new(params)
+    @form = DiaperCalculatorForm.new(calculator_params)
 
-    if @validation.valid?
+    if @form.valid?
       calc_service         = Calculators::DiaperUsageService.new(params[:childs_years], params[:childs_months], set_category_id)
       calc_service.calculate
       calculator_decorator = CalculatorDecorator.new(calc_service.result)
 
       render json: calculator_decorator.to_json, status: :ok
     else
-      render json: { error: @validation.error }, status: :unprocessable_entity
+      render json: { error: @form.errors.full_messages.join(", ") }, status: :unprocessable_entity
     end
   end
 
@@ -19,5 +19,9 @@ class Api::V1::DiaperCalculatorsController < ApplicationController
 
   def set_category_id
     params[:category_id].presence || Category.preferable.first.id
+  end
+
+  def calculator_params
+    params.permit(:childs_years, :childs_months)
   end
 end
