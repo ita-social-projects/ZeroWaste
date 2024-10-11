c8fdd94c2e69dfbcbb5a655d9da9d99ae0a3a112
iambrozjak <ivanochkavasyljuk@gmail.com>
2024-10-11 15:16:43 +0300

    Add test environment settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,6 +21,15 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
+
+  # Eager loading loads your entire application. When running a single test locally,
+  # this is usually not necessary, and can slow down your test suite. However, it's
+  # recommended that you enable it in continuous integration systems to ensure eager
+  # loading is working properly before deploying your code.
+  config.eager_load = ENV["CI"].present?
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
