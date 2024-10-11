455c7b6c2f0f7e3c951ffd000d3f614a22387c55
iambrozjak <ivanochkavasyljuk@gmail.com>
2024-10-11 13:57:40 +0300

    Update test environment configuration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,15 +21,6 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # While tests run files are not watched, reloading is not necessary.
-  config.enable_reloading = false
-
-  # Eager loading loads your entire application. When running a single test locally,
-  # this is usually not necessary, and can slow down your test suite. However, it's
-  # recommended that you enable it in continuous integration systems to ensure eager
-  # loading is working properly before deploying your code.
-  config.eager_load = ENV["CI"].present?
-
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
@@ -57,10 +48,6 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: "localhost" }
-
-  # Unlike controllers, the mailer instance doesn't have any context about the
-  # incoming request so you'll need to provide the :host parameter yourself.
-  config.action_mailer.default_url_options = { host: "www.example.com" }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
