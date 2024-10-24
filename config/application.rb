73d71ea3a5acedfcb36db1d1951fc1e0c285eedb
KseniaGovorun <ksena.govorun@gmail.com>
2024-10-24 22:45:18 +0300

    Changed the rails version

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 require "rails/all"
-require "csv"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,21 +9,18 @@ Bundler.require(*Rails.groups)
 module ZeroWaste
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: ["assets", "tasks"])
 
-    config.active_job.queue_adapter = :sidekiq
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Europe/Kyiv"
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.available_locales   = [:en, :uk]
-    config.i18n.default_locale      = :en
-
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
-
-    config.assets.css_compressor = nil
   end
 end
