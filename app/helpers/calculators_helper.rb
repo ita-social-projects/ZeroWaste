87bbee1d8616905c80d3464a9ca1370338ae2c6d
Bohdan <dennger228@gmail.com>
2024-10-27 13:38:09 +0200

    Fixed link and rubocop offences

--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -24,11 +24,11 @@ module CalculatorsHelper
 
   def mhc_calculator_items
     [{ image: "pads_bought.png", data_target: "padsUsed", unit: t(".pieces"), text: t(".bought_products") },
-     "arrow",
-     { image: "pads_to_buy.png", data_target: "padsToBeUsed", unit: t(".pieces"), text: t(".will_buy_products") },
-     { image: "money_spent_2.png", data_target: "moneySpent", unit: t(".unit"), text: t(".money_spent") },
-     "arrow",
-     { image: "money_to_spent_2.png", data_target: "moneyWillBeSpent", unit: t(".unit"), text: t(".money_will_be_spent") }]
+      "arrow",
+      { image: "pads_to_buy.png", data_target: "padsToBeUsed", unit: t(".pieces"), text: t(".will_buy_products") },
+      { image: "money_spent_2.png", data_target: "moneySpent", unit: t(".unit"), text: t(".money_spent") },
+      "arrow",
+      { image: "money_to_spent_2.png", data_target: "moneyWillBeSpent", unit: t(".unit"), text: t(".money_will_be_spent") }]
   end
 
   def new_calculator_items
