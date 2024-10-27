be3b8dcda9251771f3919ada34c7a9a1085f51d1
Bohdan <dennger228@gmail.com>
2024-10-27 13:11:51 +0200

    Created menstrual hygiene calculator. Created corresponding to it view, api controller, service and validator. Also added new corresponding action to render mhc calculator to calculators controller.

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -33,6 +33,15 @@ class CalculatorsController < ApplicationController
     end
   end
 
+  def mhc_calculator
+    @diaper_categories   = Category.ordered_by_diapers_periods_price
+    @preferable_category = Category.preferable.first
+    add_breadcrumb t("breadcrumbs.home"), root_path
+    add_breadcrumb t(".mhc_calculator.calculator_name")
+
+    render "calculators/mhc_calculator"
+  end
+
   def receive_recomendations
     current_user.toggle(:receive_recomendations)
     current_user.save
