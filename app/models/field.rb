51c8ac6385f8f82196425804f8fc29d5bc943638
KseniaGovorun <ksena.govorun@gmail.com>
2024-10-29 13:04:28 +0200

    Checked codecov

--- app/models/field.rb
+++ app/models/field.rb
@@ -27,9 +27,9 @@
 class Field < ApplicationRecord
   belongs_to :calculator
 
-  enum kind: { form: 0, parameter: 1, result: 2 }
-  enum unit: { day: 0, week: 1, month: 2, year: 3, date: 4, times: 5,
-               money: 6, items: 7 }
+  enum :kind, { form: 0, parameter: 1, result: 2 }
+  enum :unit, { day: 0, week: 1, month: 2, year: 3, date: 4, times: 5,
+                money: 6, items: 7 }
 
   validates :type, :kind, :label, presence: true
 
