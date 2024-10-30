c1986cae49bcf7a869a4a8169c5b743116a7beb2
KseniaGovorun <ksena.govorun@gmail.com>
2024-10-30 12:57:51 +0200

    Fixed tailwindcss

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module ZeroWaste
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: ["assets", "tasks"])
-
+    #
+    # Add to stop any css compressor
+    config.assets.css_compressor = nil
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
