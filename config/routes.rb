841c00853e69d773f6df0a3199c0f0a05f1cfd6c
NVMakarenko <taradator@ukr.net>
2024-11-02 20:34:15 +0200

    fix rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
 
   devise_for :users, only: :omniauth_callbacks,
-                    controllers: { omniauth_callbacks:
+                     controllers: { omniauth_callbacks:
                     "users/omniauth_callbacks" }
 
   get "/", to: "application#redirection", as: :root_redirection
