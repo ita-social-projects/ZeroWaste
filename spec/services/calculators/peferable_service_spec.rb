a5f508b01fe6f327633fa31d25d63fa2dc39bc4b
OlexanderVanzuriak <sashavanzuriak@gmail.com>
2024-11-05 16:53:18 +0200

    fixed formatting

--- spec/services/calculators/peferable_service_spec.rb
+++ spec/services/calculators/peferable_service_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Calculators::PreferableService do
-  let!(:calculator1) { Calculator.create!(name: "calculator1", slug: 'calculator1', preferable: true) }
-  let!(:calculator2) { Calculator.create!(name: "calculator2", slug: 'calculator2', preferable: true) }
-  let!(:calculator3) { Calculator.create!(name: "calculator3", slug: 'calculator3', preferable: false) }
+  let!(:calculator1) { Calculator.create!(name: "calculator1", slug: "calculator1", preferable: true) }
+  let!(:calculator2) { Calculator.create!(name: "calculator2", slug: "calculator2", preferable: true) }
+  let!(:calculator3) { Calculator.create!(name: "calculator3", slug: "calculator3", preferable: false) }
 
-  describe '#perform!' do
-    context 'when preferable is set to 1' do
-      it 'updates other calculators to have preferable set to false' do
+  describe "#perform!" do
+    context "when preferable is set to 1" do
+      it "updates other calculators to have preferable set to false" do
         service = Calculators::PreferableService.new(preferable: 1, slug: calculator1.slug)
         service.perform!
 
@@ -17,9 +17,9 @@ RSpec.describe Calculators::PreferableService do
       end
     end
 
-    context 'when preferable is not set to 1' do
-      it 'does not change any preferable values' do
-        service = Calculators::PreferableService.new(preferable: 0, slug: 'calculator1')
+    context "when preferable is not set to 1" do
+      it "does not change any preferable values" do
+        service = Calculators::PreferableService.new(preferable: 0, slug: "calculator1")
         service.perform!
 
         expect(calculator1.reload.preferable).to eq(true)
