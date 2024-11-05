24a8b2d2518ece1018732ac3fa17c10e8e6368c4
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-11-05 18:00:21 +0200

    Update 20230710212500_add_default_title_to_site_settings.rb

--- db/migrate/20230710212500_add_default_title_to_site_settings.rb
+++ db/migrate/20230710212500_add_default_title_to_site_settings.rb
@@ -1,7 +1,6 @@
 class AddDefaultTitleToSiteSettings < ActiveRecord::Migration[7.1]
   def up
     change_column_default :site_settings, :title, from: nil, to: "ZeroWaste"
-    # change_column_null :site_settings, :title, false
     change_table :site_settings, bulk: true do |t|
       t.change_null :title, false
     end
@@ -9,7 +8,6 @@ class AddDefaultTitleToSiteSettings < ActiveRecord::Migration[7.1]
 
   def down
     change_column_default :site_settings, :title, from: "ZeroWaste", to: nil
-    # change_column_null :site_settings, :title, true
     change_table :site_settings, bulk: true do |t|
       t.change_null :title, true
     end
