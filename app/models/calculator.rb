ee6bea108a9a75c93c5b23275b8c1de05e8611d6
Artem Stepanchenko <astepanchenko1@gmail.com>
2024-11-07 17:11:21 +0200

    added models; little fixes for new view

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -5,52 +5,20 @@
 # Table name: calculators
 #
 #  id         :bigint           not null, primary key
-#  name       :string
-#  preferable :boolean          default(FALSE)
+#  en_name    :string           not null
 #  slug       :string
-#  uuid       :uuid             not null
+#  uk_name    :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 # Indexes
 #
-#  index_calculators_on_name  (name) UNIQUE
 #  index_calculators_on_slug  (slug) UNIQUE
-#  index_calculators_on_uuid  (uuid) UNIQUE
 #
 
 class Calculator < ApplicationRecord
-  extend FriendlyId
-
-  friendly_id :uk_name, use: :sequentially_slugged
-
-  has_paper_trail
-
   has_many :fields, dependent: :destroy
+  has_many :formulas, dependent: :destroy
 
   accepts_nested_attributes_for :fields, allow_destroy: true
-
-  validates :uk_name, presence: true
-  validates :uk_name,
-            length: { in: 2..30 },
-            uniqueness: true,
-            format: { with: /\A[a-zA-Zа-яієїґ'А-ЯІЄЇҐ0-9\-\s]+\z/ },
-            allow_blank: true
-
-  scope :ordered_by_name, -> { order(:uk_name) }
-  scope :by_name_or_slug, lambda { |search|
-                            where(
-                              "uk_name ILIKE ? OR slug ILIKE ?",
-                              "%#{search&.strip}%",
-                              "%#{search&.strip}%"
-                            )
-                          }
-
-  def normalize_friendly_id(input)
-    input.to_slug.transliterate(:ukrainian).normalize.to_s
-  end
-
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "name", "preferable", "slug", "updated_at", "uuid"]
-  end
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -26,22 +26,5 @@
 #
 class Field < ApplicationRecord
   belongs_to :calculator
-
-  enum :kind, { form: 0, parameter: 1, result: 2 }
-  enum :unit, { day: 0, week: 1, month: 2, year: 3, date: 4, times: 5, money: 6, items: 7 }
-
-  validates :type, :kind, :label, presence: true
-
-  before_create :set_selector, if: -> { selector.blank? }
-
-  private
-
-  def set_selector
-    index = Field.order(selector: :desc)
-                 .find_by(calculator: calculator, kind: kind)
-                 &.selector
-                 &.gsub(/\D/, "").to_i.next
-
-    self.selector = "#{kind&.first&.upcase}#{index}"
-  end
+  has_many :categories, dependent: :destroy
 end
