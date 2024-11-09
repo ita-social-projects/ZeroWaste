a37b9c85a6e6790f4fc17fceb20ffb4f98cca63b
NVMakarenko <taradator@ukr.net>
2024-11-09 16:27:10 +0200

    fix rubocop

--- db/migrate/20241107101158_change_calculators.rb
+++
@@ -1,8 +0,0 @@
-class ChangeCalculators < ActiveRecord::Migration[7.2]
-  def change
-    remove_columns :calculators, :uuid, :name, :preferable
-
-    add_column :calculators, :uk_name, :string, null: false, default: ""
-    add_column :calculators, :en_name, :string, null: false, default: ""
-  end
-end
--- db/migrate/20241107135318_change_fields.rb
+++ db/migrate/20241107135318_change_fields.rb
@@ -1,10 +1,49 @@
 class ChangeFields < ActiveRecord::Migration[7.2]
-  def change
-    remove_columns :fields, :uuid, :selector, :type, :label, :name, :value, :from, :to, :kind, :unit
+  def up
+    change_table :fields, bulk: true do |t|
+      t.remove :uuid, type: :string
+      t.remove :selector, type: :string
+      t.remove :type, type: :string
+      t.remove :label, type: :string
+      t.remove :name, type: :string
+      t.remove :value, type: :string
+      t.remove :from, type: :integer
+      t.remove :to, type: :integer
+      t.remove :kind, type: :string
+      t.remove :unit, type: :string
+      t.remove :calculator_id, type: :integer
 
-    add_column :fields, :uk_label, :string, null: false, default: ""
-    add_column :fields, :en_label, :string, null: false, default: ""
-    add_column :fields, :var_name, :string, null: false, default: ""
-    add_column :fields, :field_type, :string, null: false, default: ""
+      t.string :uk_name, null: false, default: ""
+      t.string :en_name, null: false, default: ""
+      t.string :var_name, null: false, default: ""
+      t.string :field_type, null: false, default: ""
+
+      t.references :calculator, foreign_key: true, null: false, default: 0
+    end
+
+    change_column_default :fields, :calculator_id, from: 0, to: nil
+  end
+
+  def down
+    change_table :fields, bulk: true do |t|
+      t.string :uuid
+      t.string :selector
+      t.string :type
+      t.string :label
+      t.string :name
+      t.string :value
+      t.integer :from
+      t.integer :to
+      t.string :kind
+      t.string :unit
+      t.integer :calculator_id
+
+      t.remove :uk_name
+      t.remove :en_name
+      t.remove :var_name
+      t.remove :field_type
+
+      t.remove_references :calculator, foreign_key: true
+    end
   end
 end
