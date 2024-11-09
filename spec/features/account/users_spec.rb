bcac592667c1d21efca5652f9674dff1a055eaea
igorSimash <vigorochok@gmail.com>
2024-11-09 14:25:33 +0200

    fixed capybara test

--- spec/features/account/users_spec.rb
+++ spec/features/account/users_spec.rb
@@ -117,8 +117,10 @@ describe "visit admin page", js: true do
   describe "user info page" do
     context "viewing non-existing user" do
       it "renders the 404 page" do
-        visit account_user_path(id: 1355)
-        expect(page).to have_content("Oops! Page Not Found")
+        expect do
+          visit account_user_path(id: 1355)
+          expect(page).to have_content("Oops! Page Not Found")
+        end.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
