c3880a16f07fd62d1711cd9a2a3f2b49e17ee593
NVMakarenko <taradator@ukr.net>
2024-11-09 17:30:51 +0200

    add CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,7 +7,7 @@ Rails.application.config.content_security_policy do |policy|
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
 #   policy.object_src  :none
-  policy.script_src  :self, :https, :unsafe_inline
+  policy.script_src  :self, :https, :unsafe_inline, :blob
 #   policy.style_src   :self, :https
 #   # If you are using webpack-dev-server then specify webpack-dev-server host
 #   policy.connect_src :self, :https, "http://localhost:3035",
