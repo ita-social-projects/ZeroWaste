3962c7cb8b03a0531941fcadb6dbd5339a99ae65
NVMakarenko <taradator@ukr.net>
2024-11-09 13:44:10 +0200

    use inline script

--- app/javascript/controllers/redirect_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  connect() {
-    const url = this.element.dataset.url;
-    console.log("Redirecting to:", url);
-    Turbo.visit(url);
-  }
-}
