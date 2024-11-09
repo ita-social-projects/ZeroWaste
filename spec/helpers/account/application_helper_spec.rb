cf45a5494a410e48370bd28d058ba23ce67ab45b
KseniaGovorun <ksena.govorun@gmail.com>
2024-11-09 13:34:39 +0200

    Fixed in file application_helper

--- spec/helpers/account/application_helper_spec.rb
+++ spec/helpers/account/application_helper_spec.rb
@@ -4,15 +4,17 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#current_locale?" do
     context "when the current locale matches the given locale" do
       it "returns true" do
-        I18n.with_locale = :en
-        expect(helper.current_locale?(:en)).to be true
+        I18n.with_locale(:en) do
+          expect(helper.current_locale?(:en)).to be true
+        end
       end
     end
 
     context "when the current locale does not match the given locale" do
       it "returns false" do
-        I18n.with_locale = :en
-        expect(helper.current_locale?(:uk)).to be false
+        I18n.with_locale(:en) do
+          expect(helper.current_locale?(:uk)).to be false
+        end
       end
     end
   end
--- spec/models/authorization_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Authorization, type: :model do
-  context "associations" do
-    it { is_expected.to belong_to(:admin).optional }
-  end
-end
