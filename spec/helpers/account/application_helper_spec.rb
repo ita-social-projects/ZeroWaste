5fdaec5c1c71de31ebda0936f23f6c7dd5fa985e
KseniaGovorun <ksena.govorun@gmail.com>
2024-11-09 11:58:10 +0200

    Fixed rubocop

--- spec/helpers/account/application_helper_spec.rb
+++ spec/helpers/account/application_helper_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#current_locale?" do
     context "when the current locale matches the given locale" do
       it "returns true" do
-        I18n.locale = :en
+        I18n.with_locale = :en
         expect(helper.current_locale?(:en)).to be true
       end
     end
 
     context "when the current locale does not match the given locale" do
       it "returns false" do
-        I18n.locale = :en
+        I18n.with_locale = :en
         expect(helper.current_locale?(:uk)).to be false
       end
     end
