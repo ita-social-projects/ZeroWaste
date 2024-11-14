299d661098d4209c8655d635b6ebd20ee607ac05
KseniaGovorun <ksena.govorun@gmail.com>
2024-11-14 15:16:36 +0200

    Fixed rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_09_112721) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_14_131028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_09_112721) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uid", "provider"], name: "index_authorizations_on_uid_and_provider", unique: true
     t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
