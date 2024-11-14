08039a316ca1b9e8d7ebe7b642370009dd4ac845
KseniaGovorun <ksena.govorun@gmail.com>
2024-11-14 14:29:49 +0200

    Fixed rubocop

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -1,9 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Authorization, type: :model do
-  let(:authorization) { FactoryBot.build :authorization }
-
   describe "associations" do
-    it { is_expected.to belong_to(:admin).class_name("User").with_foreign_key("uid").optional }
+    it { is_expected.to belong_to(:admin).class_name("User").with_foreign_key("uid").optional.inverse_of(:authorizations) }
   end
 end
