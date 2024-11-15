d85a172728082e5c22cb04c1dba290e72dd7814d
Bohdan <dennger228@gmail.com>
2024-11-15 16:25:36 +0200

    Added before hook for blank param

--- spec/support/shared/presence_validation_example.rb
+++ spec/support/shared/presence_validation_example.rb
@@ -4,10 +4,14 @@ shared_examples "presence validation" do |attribute|
     expect(validator.errors[attribute]).to be_nil
   end
 
-  it "#{attribute} is invalid when blank" do
-    validator.params[attribute] = nil
+  context "#{attribute} is not present" do
+    before do
+      validator.params[attribute] = nil
+    end
 
-    expect(validator.send(:"validate_#{attribute}")).to be false
-    expect(validator.errors[attribute]).to eq(I18n.t("#{local_error_prefix}.presence_error_msg", field: I18n.t("#{local_field_prefix}.#{attribute}")))
+    it "#{attribute} is invalid when blank" do
+      expect(validator.send(:"validate_#{attribute}")).to be false
+      expect(validator.errors[attribute]).to eq(I18n.t("#{local_error_prefix}.presence_error_msg", field: I18n.t("#{local_field_prefix}.#{attribute}")))
+    end
   end
 end
