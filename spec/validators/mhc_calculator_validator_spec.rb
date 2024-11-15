55fbb52b75feb04ed6de12ca67ad8dc1bce54a7a
Bohdan <dennger228@gmail.com>
2024-11-15 14:04:51 +0200

    Added tests for each of the validate param method

--- spec/validators/mhc_calculator_validator_spec.rb
+++ spec/validators/mhc_calculator_validator_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe MhcCalculatorValidator do
       pad_category: "budget"
     }
   end
+  let(:local_error_prefix) { "calculators.errors" }
+  let(:local_field_prefix) { "calculators.mhc_calculator.form" }
 
   describe "#valid?" do
     context "when all parameters are valid" do
@@ -25,14 +27,57 @@ RSpec.describe MhcCalculatorValidator do
     end
 
     context "when some parameter is missing" do
+      let(:invalid_params) { valid_params.except(:user_age, :pads_per_cycle) }
+
       it "returns false" do
-        invalid_params = valid_params.except(:user_age)
-        validator      = described_class.new(invalid_params)
+        validator = described_class.new(invalid_params)
 
         expect(validator.valid?).to be false
         expect(validator.errors).to include(:user_age)
-        expect(validator.errors[:user_age]).to eq(I18n.t("calculators.errors.presence_error_msg", field: I18n.t("calculators.mhc_calculator.form.user_age")))
+        expect(validator.errors[:user_age]).to eq(I18n.t("#{local_error_prefix}.presence_error_msg", field: I18n.t("#{local_field_prefix}.user_age")))
       end
+    end
+  end
+
+  describe "individual validations" do
+    shared_examples "a presence validation" do |attribute|
+      it "#{attribute} is valid when present" do
+        expect(validator.send(:"validate_#{attribute}")).to be true
+        expect(validator.errors[attribute]).to be_nil
+      end
+
+      it "#{attribute} is invalid when blank" do
+        validator.params[attribute] = nil
+
+        expect(validator.send(:"validate_#{attribute}")).to be false
+        expect(validator.errors[attribute]).to eq(I18n.t("#{local_error_prefix}.presence_error_msg", field: I18n.t("#{local_field_prefix}.#{attribute}")))
+      end
+    end
+
+    let!(:validator) { described_class.new(valid_params) }
+
+    context "user age validation" do
+      include_examples "a presence validation", :user_age
+    end
+
+    context "menstruation age validation" do
+      include_examples "a presence validation", :menstruation_age
+    end
+
+    context "menopause age validation" do
+      include_examples "a presence validation", :menopause_age
+    end
+
+    context "average menstruation cycle duration validation" do
+      include_examples "a presence validation", :average_menstruation_cycle_duration
+    end
+
+    context "pads per cycle validation" do
+      include_examples "a presence validation", :pads_per_cycle
+    end
+
+    context "pad category validation" do
+      include_examples "a presence validation", :pad_category
     end
   end
 end
