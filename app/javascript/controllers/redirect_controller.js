970e074e99d8323076003357bbf51f4604f1a31b
NVMakarenko <taradator@ukr.net>
2024-11-19 12:43:07 +0200

    remove redundant code

--- app/javascript/controllers/redirect_controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-export default class extends Controller {
-  connect() {
-    const url = this.element.dataset.url;
-    window.location.assign(url);
-  }
-}
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,20 +2,20 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.content_security_policy do |policy|
-  #   policy.default_src :self, :https
-  #   policy.font_src    :self, :https, :data
-  #   policy.img_src     :self, :https, :data
-  #   policy.object_src  :none
-  policy.script_src :self, :https, :unsafe_inline, :blob
-  #   policy.style_src   :self, :https
-  #   # If you are using webpack-dev-server then specify webpack-dev-server host
-  #   policy.connect_src :self, :https, "http://localhost:3035",
-  #   "ws://localhost:3035" if Rails.env.development?
+# Rails.application.config.content_security_policy do |policy|
+#   policy.default_src :self, :https
+#   policy.font_src    :self, :https, :data
+#   policy.img_src     :self, :https, :data
+#   policy.object_src  :none
+#   policy.script_src  :self, :https
+#   policy.style_src   :self, :https
+#   # If you are using webpack-dev-server then specify webpack-dev-server host
+#   policy.connect_src :self, :https, "http://localhost:3035",
+#   "ws://localhost:3035" if Rails.env.development?
 
-  #   # Specify URI for violation reports
-  #   # policy.report_uri "/csp-violation-report-endpoint"
-end
+#   # Specify URI for violation reports
+#   # policy.report_uri "/csp-violation-report-endpoint"
+# end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config
