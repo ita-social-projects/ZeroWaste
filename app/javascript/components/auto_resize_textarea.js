12c49a31284dc73b8e2865bc93e7618cc7fbfe06
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-11-26 17:29:45 +0200

    script delete

--- app/javascript/components/auto_resize_textarea.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('input', function (event) {
-  if (event.target && event.target.classList.contains('resize-auto') && event.target.tagName === 'TEXTAREA') {
-    const textarea = event.target;
-    textarea.style.overflow = 'hidden';
-    textarea.style.resize = 'none';
-    textarea.style.height = 'auto';
-    textarea.style.height = (textarea.scrollHeight) + 'px';
-  }
-});
