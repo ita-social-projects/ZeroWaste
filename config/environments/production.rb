9ce562fbe9abb1da2e573a17809389a23e293003
KseniaGovorun <ksena.govorun@gmail.com>
2024-11-26 15:29:51 +0200

    Deleted logger by default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,6 @@ Rails.application.configure do
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   config.log_level = :info
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new($stdout)
-                                       .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-                                       .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
