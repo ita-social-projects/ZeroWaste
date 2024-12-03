9e63cbb4bce054a25ac29f4832c89f41ea73f1b9
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-12-03 20:37:57 +0200

    formula field styling

--- app/javascript/controllers/textarea_controller.js
+++ app/javascript/controllers/textarea_controller.js
@@ -8,8 +8,6 @@ export default class extends Controller {
   }
 
   resize(textarea) {
-    textarea.style.overflow = "hidden";
-    textarea.style.resize = "none";
     textarea.style.height = "auto";
     textarea.style.height = `${textarea.scrollHeight}px`; // Adjust height based on scrollHeight
   }
