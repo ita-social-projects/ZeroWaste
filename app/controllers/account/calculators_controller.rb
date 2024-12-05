1df17d568d61fa8d9c7f391cb59260605e0bf86f
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-12-05 13:32:16 +0200

    #988 Add color to Calculator (#997)
    * color picker
    
    * cop
    
    * minor
    
    * default color
    
    * schema update
    
    * Update _form.html.erb
    
    * test+style
    
    * cop
    
    * Update app/assets/stylesheets/components/form.css
    
    Co-authored-by: Ivan Marynych <49816584+loqimean@users.noreply.github.com>
    
    * style
    
    * fix
    
    ---------
    
    Co-authored-by: Ivan Marynych <49816584+loqimean@users.noreply.github.com>

--- app/controllers/account/calculators_controller.rb
+++ app/controllers/account/calculators_controller.rb
@@ -81,7 +81,7 @@ class Account::CalculatorsController < Account::BaseController
 
   def calculator_params
     params.require(:calculator).permit(
-      :id, :en_name, :uk_name,
+      :id, :en_name, :uk_name, :color,
       formulas_attributes: [:id, :expression, :en_label, :uk_label, :calculator_id, :en_unit, :uk_unit, :_destroy],
       fields_attributes: [:id, :en_label, :uk_label, :var_name, :kind, :_destroy,
         categories_attributes: [:id, :en_name, :uk_name, :price, :_destroy]]
