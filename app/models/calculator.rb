add0bfa782178c17428625f921f3cec5d1e93bf6
OlexanderVanzuriak <sashavanzuriak@gmail.com>
2024-12-11 19:00:49 +0200

    Added relation validation to calculator

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -28,6 +28,8 @@ class Calculator < ApplicationRecord
   has_many :fields, dependent: :destroy
   has_many :formulas, dependent: :destroy
 
+  validates_with RelationValidator
+
   accepts_nested_attributes_for :fields, allow_destroy: true
   accepts_nested_attributes_for :formulas, allow_destroy: true
 
---
+++ app/validators/relation_validator.rb
@@ -0,0 +1,32 @@
+class RelationValidator < ActiveModel::Validator
+    def validate(record)
+      relation_is_correct(record)
+    end
+  
+    private
+  
+    def relation_is_correct(record)
+      return if record.formulas.blank?
+
+      if record.formulas.first.relation == "Previous"
+        record.formulas.first.errors.add(:relation, "The first formula cannot have a relation of 'Previous' because there is no previous formula.")
+      end
+
+      if record.formulas.last.relation == "Next"
+        record.formulas.last.errors.add(:relation, "The last formula cannot have a relation of 'Next' because there is no next formula.")
+      end
+
+      last_relation = nil
+      record.formulas.each do |formula|
+        if last_relation == "Next" && formula.relation == "Previous"
+            formula.errors.add(:relation, "cannot have the same relation as the previous formula.")
+        end
+        last_relation = formula.relation
+      end
+
+      if record.formulas.any? { |formula| formula.errors.present? }
+        record.errors.add(:formulas, "contain invalid relations.")
+      end
+    end
+  end
+  
