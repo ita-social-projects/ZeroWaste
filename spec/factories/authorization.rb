1296e1c9fe1c51e1e5d1718f1d3708788cbcd521
OlexanderVanzuriak <sashavanzuriak@gmail.com>
2024-12-17 09:53:29 +0200

    rubocop

--- spec/factories/authorization.rb
+++ spec/factories/authorization.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :authorization do
     uid { "unique_uid" }
     provider { "provider_name" }
-    association :admin, factory: :user
+    admin factory: [:user]
   end
 end
