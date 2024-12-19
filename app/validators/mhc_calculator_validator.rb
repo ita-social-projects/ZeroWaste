a58c3fc5cc86ded11e229806aefb87253e3b6228
Bohdan <dennger228@gmail.com>
2024-12-19 21:41:14 +0200

    Added length validations to mhc calculator

--- app/validators/mhc_calculator_validator.rb
+++ app/validators/mhc_calculator_validator.rb
@@ -22,23 +22,28 @@ class MhcCalculatorValidator
   private
 
   def validate_user_age
-    presence_valid?(:user_age)
+    return unless presence_valid?(:user_age)
+    length_valid?(:user_age, 0, 100)
   end
 
   def validate_menstruation_age
-    presence_valid?(:menstruation_age)
+    return unless presence_valid?(:menstruation_age)
+    length_valid?(:menstruation_age, 0, 100)
   end
 
   def validate_menopause_age
-    presence_valid?(:menopause_age)
+    return unless presence_valid?(:menopause_age)
+    length_valid?(:menopause_age, 0, 100)
   end
 
   def validate_average_menstruation_cycle_duration
-    presence_valid?(:average_menstruation_cycle_duration)
+    return unless presence_valid?(:average_menstruation_cycle_duration)
+    length_valid?(:average_menstruation_cycle_duration, 0, 40)
   end
 
   def validate_pads_per_cycle
-    presence_valid?(:pads_per_cycle)
+    return unless presence_valid?(:pads_per_cycle)
+    length_valid?(:pads_per_cycle, 0, 100)
   end
 
   def validate_pad_category
@@ -49,6 +54,14 @@ class MhcCalculatorValidator
     return true if @params[param].present?
 
     @errors[param] = I18n.t("calculators.errors.presence_error_msg", field: I18n.t("calculators.mhc_calculator.form.#{param}"))
+
+    false
+  end
+
+  def length_valid?(param, from, to)
+    return true if (from..to).include?(@params[param])
+
+    @errors[param] = I18n.t("calculators.errors.length_error_msg", field: I18n.t("calculators.mhc_calculator.form.#{param}"), from: from, to: to)
 
     false
   end
