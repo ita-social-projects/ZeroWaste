1511258f5ba80a4f65ab7c8320b4e06a7c4eb248
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-12-26 12:15:21 +0200

    cop

--- app/controllers/account/calculators_controller.rb
+++ app/controllers/account/calculators_controller.rb
@@ -81,10 +81,8 @@ class Account::CalculatorsController < Account::BaseController
 
   def calculator_params
     params.require(:calculator).permit(
-
       :id, :en_name, :uk_name, :uk_notes,
       :en_notes, :color,
-
       formulas_attributes: [:id, :expression, :en_label, :uk_label, :calculator_id, :en_unit, :uk_unit, :_destroy],
       fields_attributes: [:id, :en_label, :uk_label, :var_name, :kind, :_destroy,
         categories_attributes: [:id, :en_name, :uk_name, :price, :_destroy]]
