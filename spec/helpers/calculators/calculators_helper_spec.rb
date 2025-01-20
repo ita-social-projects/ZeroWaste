8c7b500898f061e8cfdbbd60885fd76b5747db1e
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2025-01-20 09:54:54 +0200

    Update calculators_helper_spec.rb

--- spec/helpers/calculators/calculators_helper_spec.rb
+++ spec/helpers/calculators/calculators_helper_spec.rb
@@ -16,27 +16,25 @@ RSpec.describe ApplicationHelper, type: :helper do
       '<img src="image.png" alt="example image" style="width:100px;">'
     end
 
-    let(:result) { |html| helper.sanitized_notes(html) }
-
     it "allows specific tags and attributes" do
-      expect(result(valid_html)).to include('<p class="text-bold">')
-      expect(result(valid_html)).to include("<strong>bold</strong>")
-      expect(result(valid_html)).to include("<em>italic</em>")
-      expect(result(valid_html)).to include('<a href="http://example.com" target="_blank">Link</a>')
+      expect(helper.sanitized_notes(valid_html)).to include('<p class="text-bold">')
+      expect(helper.sanitized_notes(valid_html)).to include("<strong>bold</strong>")
+      expect(helper.sanitized_notes(valid_html)).to include("<em>italic</em>")
+      expect(helper.sanitized_notes(valid_html)).to include('<a href="http://example.com" target="_blank">Link</a>')
     end
 
     it "removes disallowed tags" do
-      expect(result(invalid_html)).not_to include("<script>")
+      expect(helper.sanitized_notes(invalid_html)).not_to include("<script>")
     end
 
     it "removes disallowed tags but keeps allowed tags and attributes" do
-      expect(result(mixed_html)).to include("<p>This is <strong>bold</strong>")
-      expect(result(mixed_html)).not_to include("<script>")
-      expect(result(mixed_html)).to include('<a href="http://example.com">Link</a>')
+      expect(helper.sanitized_notes(mixed_html)).to include("<p>This is <strong>bold</strong>")
+      expect(helper.sanitized_notes(mixed_html)).not_to include("<script>")
+      expect(helper.sanitized_notes(mixed_html)).to include('<a href="http://example.com">Link</a>')
     end
 
     it "does not remove allowed attributes from tags" do
-      expect(result(html_with_attributes)).to eq('<img src="image.png" alt="example image" style="width:100px;">')
+      expect(helper.sanitized_notes(html_with_attributes)).to eq('<img src="image.png" alt="example image" style="width:100px;">')
     end
   end
 end
