b7944bce841e38c4ba18d756e3c4678edf8eb4c8
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2025-02-14 15:04:22 +0200

    removed unused script

--- app/javascript/controllers/info_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ["text"];
-
-  toggle() {
-    this.textTarget.classList.toggle("hidden");
-  }
-}
