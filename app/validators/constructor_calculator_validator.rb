e1197ff75719053c9304158b35129a84ca2290ed
oasis-dot <maksimrudik28113@gmail.com>
2025-05-14 16:12:50 +0300

    refactor: update private methods in calc service

--- app/validators/constructor_calculator_validator.rb
+++ app/validators/constructor_calculator_validator.rb
@@ -5,14 +5,14 @@ class ConstructorCalculatorValidator
     @params     = params
     @calculator = calculator
     @errors     = {}
-    @fields     = @calculator.fields
+    @fields     = calculator.fields
   end
 
   def valid?
     @fields.each do |field|
       next unless presence_valid?(field)
 
-      category_valid?(field) if category_field_names.include?(field.var_name)
+      category_valid?(field) if category_fields.include?(field.var_name)
     end
 
     errors.empty?
@@ -26,29 +26,28 @@ class ConstructorCalculatorValidator
     field.en_label
   end
 
-  def category_field_names
-    @calculator.fields.where(kind: "category").map(&:var_name)
+  def category_fields
+    @fields.where(kind: "category").map(&:var_name)
   end
 
-  def valid_categories(field)
-    @calculator.fields.find_by(var_name: field).categories.map(&:en_name)
+  def valid_categories(var_name)
+    @fields.find_by(var_name: var_name).categories.map(&:en_name)
   end
 
   def presence_valid?(field)
-    return true if @params[field.var_name].present?
+    name = field.var_name
+    return true if @params[name].present?
 
-    @errors[field.var_name] = I18n.t("calculators.errors.presence_error_msg", field: localized_field_label(field))
-
+    @errors[name] = I18n.t("calculators.errors.presence_error_msg", field: localized_label(field))
     false
   end
 
   def category_valid?(field)
-    valid_categories = valid_categories(field.var_name)
+    name       = field.var_name
+    categories = valid_categories(name)
+    return true if categories.include?(@params[name])
 
-    return true if valid_categories.include?(@params[field.var_name])
-
-    @errors[field.var_name] = I18n.t("calculators.errors.wrong_category_error_msg")
-
+    @errors[name] = I18n.t("calculators.errors.wrong_category_error_msg")
     false
   end
 end
