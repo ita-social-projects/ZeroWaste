4564a3c0d3a0e561c49ccbe39a27321abd3cf196
Anna Lobova <riabkova.as.87@gmail.com>
2025-05-24 22:19:47 +0300

    Add tests for localized_column_for method

--- spec/models/conserns/translatable_spec.rb
+++ spec/models/conserns/translatable_spec.rb
@@ -29,4 +29,36 @@ RSpec.describe Translatable do
       end
     end
   end
+
+  describe "localized_column_for" do
+    let(:attribute_with_locales) { "name" }
+    let(:attribute_without_locale) { "color" }
+
+    context "when current locale column exists" do
+      it "returns the column name for the current locale" do
+        I18n.with_locale(:uk) do
+          expect(Calculator.localized_column_for(attribute_with_locales)).to eq("uk_name")
+        end
+
+        I18n.with_locale(:en) do
+          expect(Calculator.localized_column_for(attribute_with_locales)).to eq("en_name")
+        end
+      end
+    end
+
+    context "when current locale column does not exist, but default locale column exists" do
+      it "returns the column name for the default locale" do
+        I18n.with_locale(:uk) do
+          allow(Calculator).to receive(:column_names).and_return(["en_name", "another_column"])
+          expect(Calculator.localized_column_for(attribute_with_locales)).to eq("en_name")
+        end
+      end
+    end
+
+    context "when the locale column does not exist and the default locale column does not exist" do
+      it "returns the column name without locale" do
+        expect(Calculator.localized_column_for(attribute_without_locale)).to eq("color")
+      end
+    end
+  end
 end
