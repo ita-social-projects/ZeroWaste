b91507183fb856ac3ec18f2238f0b9a117eab53f
oasis-dot <maksimrudik28113@gmail.com>
2025-06-02 13:32:08 +0300

    fix; change api to new version of pads calc

--- app/validators/mhc_calculator_validator.rb
+++ app/validators/mhc_calculator_validator.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class MhcCalculatorValidator
+  PARAM_TO_PRODUCT_MAPPING = {
+    pad_category: :pads,
+    product_type: :tampons
+  }.freeze
+
   attr_reader :params, :errors
 
   def initialize(params)
@@ -13,7 +18,9 @@ class MhcCalculatorValidator
     validate_menstruation_age
     validate_menopause_age
     validate_average_menstruation_cycle_duration
-    validate_pads_per_cycle
+    validate_duration_of_menstruation
+    validate_disposable_products_per_day
+    validate_product_type
     validate_pad_category
 
     errors.empty?
@@ -45,10 +52,20 @@ class MhcCalculatorValidator
     length_valid?(:average_menstruation_cycle_duration, 1, 100)
   end
 
-  def validate_pads_per_cycle
-    return unless presence_valid?(:pads_per_cycle)
+  def validate_duration_of_menstruation
+    presence_valid?(:duration_of_menstruation)
+  end
 
-    length_valid?(:pads_per_cycle, 1, 100)
+  def validate_disposable_products_per_day
+    return unless presence_valid?(:disposable_products_per_day)
+
+    length_valid?(:disposable_products_per_day, 1, 100)
+  end
+
+  def validate_product_type
+    return unless presence_valid?(:product_type)
+
+    type_valid?(:product_type)
   end
 
   def validate_pad_category
@@ -74,11 +91,22 @@ class MhcCalculatorValidator
   end
 
   def category_valid?(param)
-    valid_categories = Calculators::PadUsageService::PAD_PRICES.keys.map(&:to_s)
+    product_key      = PARAM_TO_PRODUCT_MAPPING[param]
+    valid_categories = Calculators::PadUsageService::PRODUCT_PRICES[product_key].keys.map(&:to_s)
 
     return true if valid_categories.include?(@params[param])
 
     @errors[param] = I18n.t("calculators.errors.wrong_category_error_msg")
+
+    false
+  end
+
+  def type_valid?(param)
+    valid_types = Calculators::PadUsageService::PRODUCT_PRICES.keys.map(&:to_s)
+
+    return true if valid_types.include?(@params[param])
+
+    @errors[param] = I18n.t("calculators.errors.wrong_type_error_msg")
 
     false
   end
