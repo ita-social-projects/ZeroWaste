44d9b89f62af4893e34dd60ea01c84eb9641d712
Anna Lobova <riabkova.as.87@gmail.com>
2025-06-07 20:05:07 +0300

    Add reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_23_135823) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_05_172551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_135823) do
     t.text "uk_notes"
     t.text "en_notes"
     t.string "color", default: "#8fba3b"
+    t.bigint "original_calculator_id"
+    t.index ["original_calculator_id"], name: "index_calculators_on_original_calculator_id"
     t.index ["slug"], name: "index_calculators_on_slug", unique: true
   end
 
@@ -242,6 +244,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_135823) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "authorizations", "users"
+  add_foreign_key "calculators", "calculators", column: "original_calculator_id"
   add_foreign_key "categories", "fields"
   add_foreign_key "category_categoryables", "categories"
   add_foreign_key "diapers_periods", "categories"
