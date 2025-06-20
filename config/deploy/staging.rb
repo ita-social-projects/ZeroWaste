44ea221f2bd18d3a0677fb21ddeaac0c986f4bad
Andrii <andrii.khomik@gmail.com>
2025-06-20 17:47:34 +0300

    update staging configuration

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,11 @@
-set :user, "ubuntu"
+require "capistrano/yarn"
 
-server "51.44.25.104", user: fetch(:user), roles: ["app", "db", "web"], primary: true, port: 22
+set :user, "deploy"
+
+server "185.233.37.137", user: fetch(:user), roles: ["app", "db", "web"], primary: true, port: 22
 
 set :passenger_restart_with_touch, true
-set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}-dev"
 set :ssh_options, {
   keys: ["~/.ssh/id_rsa"],
   forward_agent: false,
@@ -15,4 +17,10 @@ set :rails_env, "staging"
 
 set :branch, :develop
 set :rvm_ruby, "3.3.5"
-set :rvm_custom_path, "/usr/share/rvm"
+set :rbenv_ruby, "2.7.2"
+# set :rvm_custom_path, "/usr/share/rvm"
+
+set :default_env, {
+  "ZW_DATABASE_USERNAME" => "",
+  "ZW_DATABASE_PASSWORD" => ""
+}
